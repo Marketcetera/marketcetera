ffb088311fee7044433130214b4a42f146f98d65
Graham Miller <gmiller@marketcetera.com>
2006-06-07 19:02:11 +0000

    Action to allow JMS reconnect.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@5516 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ source/trunk/src/main/java/org/marketcetera/photon/actions/ReconnectJMSAction.java
@@ -0,0 +1,30 @@
+package org.marketcetera.photon.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.marketcetera.photon.Application;
+import org.marketcetera.photon.IImageKeys;
+import org.marketcetera.photon.PhotonPlugin;
+
+public class ReconnectJMSAction extends Action implements IWorkbenchAction {
+
+	public static final String ID = "org.marketcetera.photon.actions.ReconnectJMSAction";
+	private IWorkbenchWindow window;
+	
+	public ReconnectJMSAction(IWorkbenchWindow window){
+		this.window = window;
+		setId(ID);
+		setText("&Reconnect Message Connection");
+		setToolTipText("Reconnect to the message server");
+		setImageDescriptor(PhotonPlugin.getImageDescriptor(IImageKeys.RECONNECT_JMS_HISTORY));
+	}
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+	public void run() {
+		Application.initJMSConnector();
+	}
+
+}
