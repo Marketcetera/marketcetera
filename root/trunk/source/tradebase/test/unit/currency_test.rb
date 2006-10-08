21abe24d41d91dad9947ee6135a21fcd7444dff5
Toli Kuznets <toli@marketcetera.com>
2006-10-08 22:43:50 +0000

    currency and dividends basic tests
    git-svn-id: file:///space/source.marketcetera.org@7765 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/test/unit/currency_test.rb
+++ root/trunk/source/tradebase/test/unit/currency_test.rb
@@ -3,8 +3,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class CurrencyTest < Test::Unit::TestCase
   fixtures :currencies
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_lookup
+    assert_equal 2, Currency.count
+    c = Currency.find(1)
+    assert c
+    assert_equal currencies(:usd).alpha_code, c.alpha_code
   end
 end
--- root/trunk/source/tradebase/test/unit/dividend_test.rb
+++ root/trunk/source/tradebase/test/unit/dividend_test.rb
@@ -1,10 +1,69 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DividendTest < Test::Unit::TestCase
-  fixtures :dividends
+  fixtures :currencies, :dividends
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    @USD = currencies(:usd)
+  end 
+  
+  def test_modification_existing
+    div = Dividend.find(1)
+    oldDesc = div.description
+    div.description = "new desc"
+    div.save
+    div.reload
+    assert_not_equal oldDesc, div.description
+    assert_equal "new desc", div.description
+  end
+  
+  def test_negative_amount_validation
+    div = Dividend.new
+    div.amount = -20
+    div.description = "negative"
+    div.currency = @USD
+    
+    assert_equal 0, div.errors.length
+    div.save  
+    assert_equal 1, div.errors.length, "accepted a negative amount"
+  end
+  
+  def test_numericality_validation
+    div = Dividend.new
+    div.amount = "abcd"
+    div.description = "non-numeric"
+    div.currency = @USD
+    
+    div.save
+    assert_equal 1, div.errors.length, "accepted non-numeric"
+  end
+  
+  def test_numericality_validation_string_number
+    div = Dividend.new
+    div.amount = "20"
+    div.description = "string"
+    div.currency = @USD
+    
+    div.save
+    assert_equal 0, div.errors.length, "didn't accepted string number"
+  end
+  
+  def test_zero_amount
+    div = Dividend.new
+    div.amount = 0
+    div.description = "zero"
+    div.currency = @USD
+    
+    div.save
+    assert_equal 0, div.errors.length, "didn't accepted zero"
+  end
+  def test_non_integer
+    div = Dividend.new
+    div.amount = 23.42
+    div.description = "non-integer"
+    div.currency = @USD
+    
+    div.save
+    assert_equal 0, div.errors.length, "didn't accepted non-integer"
   end
 end
