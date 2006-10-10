ff5d40ca41f02fe2bf39a1a9f809880727499ab5
Toli Kuznets <toli@marketcetera.com>
2006-10-10 21:36:29 +0000

    fixed wrong use of constants
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7770 501a4a96-914b-0410-8454-f5988a4ca596

--- source/tradebase/db/migrate/015_insert_sub_account_types.rb
+++ source/tradebase/db/migrate/015_insert_sub_account_types.rb
@@ -2,13 +2,13 @@ class InsertSubAccountTypes < ActiveRecord::Migration
   include SubAccountsHelper
   
   def self.up
-    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:ShortTermInvestmentDescription]).save()
-    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:CashDescription]).save()
-    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:DividendRevenueDescription]).save()
-    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:UnrealizedGainLossDescription]).save()
-    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:ChangeonCloseOfInvestmentDescription]).save()
-    SubAccountType.new(:accounting_account_type => "E", :description => SubAccountType::DESCRIPTIONS[:CommissionsDescription]).save()
-    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:InterestRevenueDescription]).save()
+    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:sti]).save()
+    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:cash]).save()
+    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:dividendRevenue]).save()
+    SubAccountType.new(:accounting_account_type => "A", :description => SubAccountType::DESCRIPTIONS[:unrealizedGainLoss]).save()
+    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:changeOnCloseOfInv]).save()
+    SubAccountType.new(:accounting_account_type => "E", :description => SubAccountType::DESCRIPTIONS[:commissions]).save()
+    SubAccountType.new(:accounting_account_type => "R", :description => SubAccountType::DESCRIPTIONS[:interestRevenue]).save()
   end
 
   def self.down
