45683fd665829151d7c93ece409118931563134b
Toli Kuznets <toli@marketcetera.com>
2006-10-10 19:42:02 +0000

    preliminary checkin with tests working manually but not as part of rake
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7768 501a4a96-914b-0410-8454-f5988a4ca596

--- source/tradebase/test/unit/account_test.rb
+++ source/tradebase/test/unit/account_test.rb
@@ -1,10 +1,32 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/marketcetera_test_base'
 
-class AccountTest < Test::Unit::TestCase
+class AccountTest < MarketceteraTestBase
   fixtures :accounts
-
+  
   # Replace this with your real tests.
-  def test_truth
+  def test_sub_account_creation
+    a = Account.new(:nickname => "new account")
+    assert_equal 0, a.sub_accounts.length
+    a.save
+    assert_equal SubAccountType::DESCRIPTIONS.length, SubAccountType::PRELOADED.length
+    assert_equal SubAccountType::DESCRIPTIONS.length, a.sub_accounts.length
+    assert_equal SubAccountType::PRELOADED.length, a.sub_accounts.length
+    assert a.sub_accounts.length > 0
     assert true
   end
+
+  def test_find_by_type
+    a = Account.create(:nickname => "find-by-type")
+    sa = a.find_sub_account_by_sat(SubAccountType::TYPES[:sti])
+    assert_not_nil sa
+    assert_equal SubAccountType::DESCRIPTIONS[:ShortTermInvestmentDescription], sa.description
+    
+    # now do it for all
+    SubAccountType::TYPES.each { |t| 
+      p "looking up " + t[0].to_s
+      assert_not_nil a.find_sub_account_by_sat(t[1]), t[1].description
+    }
+  end
+  
 end
--- source/tradebase/test/unit/journal_test.rb
+++ source/tradebase/test/unit/journal_test.rb
@@ -1,10 +1,46 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/marketcetera_test_base'
 
-class JournalTest < Test::Unit::TestCase
+class JournalTest < MarketceteraTestBase
   fixtures :journals
+  include TradesHelper
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  # Populate journal with some sub-account types and find them by desc
+  def test_find_posting_by_sat
+    # create an account
+    acct = Account.create(:nickname => "test-acct")
+    journal = Journal.create(:description => "random journal")
+    
+    sti = acct.find_sub_account_by_sat(SubAccountType::TYPES[:sti])
+    assert_not_nil sti, "didn't find STI sub_account"
+    cash = acct.find_sub_account_by_sat(SubAccountType::TYPES[:cash])
+    assert_not_nil cash, "didn't find cash sub_account"
+    commissions = acct.find_sub_account_by_sat(SubAccountType::TYPES[:commissions])
+    assert_not_nil commissions, "didn't find commission sub_account"
+    
+    # create some postings: sti, cash, commissions, cash
+    journal.postings << Posting.create(:journal=>journal, :currency=>currencies(:usd), 
+        :quantity=>100, :sub_account=>sti, :pair_id => 1)
+    journal.postings << Posting.create(:journal=>journal, :currency=>currencies(:usd), 
+        :quantity=>-100, :sub_account=>cash, :pair_id => 1)
+    journal.postings << Posting.create(:journal=>journal, :currency=>currencies(:usd), 
+        :quantity=>17, :sub_account=>commissions, :pair_id => 2)
+    journal.postings << Posting.create(:journal=>journal, :currency=>currencies(:usd), 
+        :quantity=>-17, :sub_account=>cash, :pair_id => 2)
+    journal.save
+    
+    # now start checking that all postings are found
+    assert_equal sti, journal.find_posting_by_sat(SubAccountType::TYPES[:sti]).sub_account
+    assert_equal cash, journal.find_posting_by_sat(SubAccountType::TYPES[:cash]).sub_account
+    assert_equal commissions, journal.find_posting_by_sat(SubAccountType::TYPES[:commissions]).sub_account
+    
+    # now find the 2nd cash posting with pair-id
+    commissionCash = journal.find_posting_by_sat_and_pair_id(SubAccountType::TYPES[:cash], 2)
+    assert_not_nil commissionCash
+    assert_equal -17, commissionCash.quantity
+    
+    # and now empty
+    assert_nil journal.find_posting_by_sat_and_pair_id(SubAccountType::TYPES[:cash], 37)
+    assert_nil journal.find_posting_by_sat(SubAccountType::TYPES[:dividendRevenue])
   end
 end
