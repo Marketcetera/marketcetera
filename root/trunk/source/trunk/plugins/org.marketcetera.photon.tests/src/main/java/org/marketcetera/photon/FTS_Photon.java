f4a98e87ab987c524c29bdc30a17e46b4e15820a
Graham Miller <gmiller@marketcetera.com>
2006-10-12 00:27:48 +0000

    some tests
    git-svn-id: file:///space/source.marketcetera.org@5748 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/trunk/plugins/org.marketcetera.photon.tests/src/main/java/org/marketcetera/photon/FTS_Photon.java
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon.tests/src/main/java/org/marketcetera/photon/FTS_Photon.java
@@ -1,4 +1,4 @@
-package org.marketcetera.photon.tests;
+package org.marketcetera.photon;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -6,7 +6,7 @@ import junit.framework.TestSuite;
 public class FTS_Photon {
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		suite.addTestSuite(FTC_TestTest.class);
+		//suite.addTestSuite(FTC_TestTest.class);
 		return suite;
 	}
 }
