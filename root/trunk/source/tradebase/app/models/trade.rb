838967c9ef01cd8346636317da6ca3b4d7254d71
Toli Kuznets <toli@marketcetera.com>
2006-10-12 19:31:11 +0000

    added more tests for Trade object
    git-svn-id: file:///space/source.marketcetera.org@7773 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/app/models/trade.rb
+++ root/trunk/source/tradebase/app/models/trade.rb
@@ -6,18 +6,18 @@ class Trade < ActiveRecord::Base
   belongs_to :tradeable, :polymorphic => true
   
   def journal_post_date
-    (self.id.nil?) ? nil : self.journal.post_date
+    (self.journal.nil?) ? nil : self.journal.post_date
   end
   
   def journal_post_date=(inDate)
       logger.debug("setting the journal post date: "+inDate.to_s)
-    if(!self.id.nil?) 
+    if(!self.journal.nil?) 
       self.journal.post_date = inDate
     end
   end
 
   def tradeable_m_symbol_root
-    (self.id.nil?) ? nil : self.tradeable.m_symbol.root
+    (self.tradeable.nil?) ? nil : self.tradeable.m_symbol.root
   end
   
   def tradeable_m_symbol_root=(inSymbol)
@@ -25,17 +25,22 @@ class Trade < ActiveRecord::Base
   end
   
   def account_nickname
-    (self.id.nil?) ? nil : self.account.nickname
+    (self.account.nil?) ? nil : self.account.nickname
   end
   
+  # todo: should this create a new account if it DNE?
   def account_nickname=(inNick)
-    if(!self.id.nil?)
-      self.account.nickname = inNick
+    newAcct = Account.find_by_nickname(inNick)
+    if(!newAcct.nil?) 
+      self.account = newAcct
+    else
+      self.account = Account.create(:nickname => inNick)
+      logger.debug("created new acct")
     end
   end
   
   def total_commission
-    if(self.id.nil?) 
+    if(self.journal.nil?) 
       return nil
     else 
       commissions = self.journal.find_posting_by_sat(SubAccountType::DESCRIPTIONS[:commissions])
@@ -44,18 +49,13 @@ class Trade < ActiveRecord::Base
   end
     
   def total_commission=(inCommission)
-    if(self.id.nil?)
+    if(self.journal.nil?)
       logger.debug("we are not initialized yet, so jumping out early")
       return
     end
 
-    commissions = self.journal.find_posting_by_sat(SubAccountType::DESCRIPTIONS[:commissions])
-    cash = self.journal.find_posting_by_sat_and_pair_id(SubAccountType::DESCRIPTIONS[:cash], commissions.pair_id)
-    commissions.quantity = inCommission
-    cash.quantity = -Float(inCommission)
-    logger.debug("updated commissions for "+self.tradeable_m_symbol_root + " to "+commissions.quantity.to_s)
-    commissions.save
-    cash.save
+    update_underlying_posting_pairs(SubAccountType::DESCRIPTIONS[:commissions], SubAccountType::DESCRIPTIONS[:cash], 
+                                    inCommission)
   end
     
   def total_price
@@ -66,32 +66,36 @@ class Trade < ActiveRecord::Base
   # Need to update all the underlying journals/postings when we update the price_per_share
   def price_per_share=(new_price)
     super(new_price)
-    if(self.id.nil? || self.journal.nil?)
+    if(self.journal.nil?)
       logger.debug("we are not initialized yet, so jumping out early, internal pps set to "+self.price_per_share.to_s)
       return
     end
     
-    logger.debug("updating the posting transactions when updating price")
-    sti = self.journal.find_posting_by_sat(SubAccountType::DESCRIPTIONS[:sti])
-    cash = self.journal.find_posting_by_sat_and_pair_id(SubAccountType::DESCRIPTIONS[:cash], sti.pair_id)
-    sti.quantity = self.quantity * Float(new_price)
-    cash.quantity = -Float(sti.quantity)
-    logger.debug("updated total price for "+self.tradeable_m_symbol_root + " to "+sti.quantity.to_s)
-    sti.save
-    cash.save
+    update_underlying_posting_pairs(SubAccountType::DESCRIPTIONS[:sti], SubAccountType::DESCRIPTIONS[:cash], 
+                                    self.quantity * Float(new_price))
   end
+  
+  # Need to update the underlying journals/postings when we update the price_per_share
+  def quantity=(new_qty)
+    super(new_qty)
+    if(self.journal.nil?)
+      logger.debug("we are not initialized yet, so jumping out early, internal qty set to "+self.quantity.to_s)
+      return
+    end
+    update_underlying_posting_pairs(SubAccountType::DESCRIPTIONS[:sti], SubAccountType::DESCRIPTIONS[:cash], 
+                                    self.quantity * self.price_per_share)
+  end    
+  
+  
     
  def create_equity_trade(quantity, symbol, price_per_share, 
                           total_commission, currency_alpha_code, account_nickname, trade_date)
     self.price_per_share = Float(price_per_share)
     self.quantity = quantity
-    logger.debug("incoming pps: "+price_per_share.to_s)
-    logger.debug("*** pps: "+self.price_per_share.to_s)
     notional = self.quantity * self.price_per_share
     total_commission = total_commission
     self.tradeable = Equity.get_equity(symbol)
     logger.debug("creating a trade for "+symbol + " for "+notional.to_s + "/("+total_commission.to_s + ")")
-    logger.debug("*** pps: "+self.price_per_share.to_s)
     self.account = Account.find_by_nickname(account_nickname)
     if(self.account == nil)
       self.account = Account.create(:nickname => account_nickname)
@@ -103,9 +107,6 @@ class Trade < ActiveRecord::Base
     cash_sub_account = self.account.find_sub_account_by_sat(SubAccountType::DESCRIPTIONS[:cash])
     commission_sub_account = self.account.find_sub_account_by_sat(SubAccountType::DESCRIPTIONS[:commissions])
 
-    logger.debug("*** pps: "+self.price_per_share.to_s)
-
-    
     self.journal = Journal.create( :post_date => trade_date )
     base_currency = Currency.get_currency(currency_alpha_code)
     self.journal.postings << Posting.create(:journal=>self.journal, :currency=>base_currency, :quantity=>notional, :sub_account=>short_term_investment_sub_account, :pair_id => 1)
@@ -115,8 +116,18 @@ class Trade < ActiveRecord::Base
     
     self.journal.save
     logger.debug("created and saved all related postings")
-    logger.debug("*** pps: "+self.price_per_share.to_s)
   end
   
-    
+  
+  def update_underlying_posting_pairs(debitName, creditName, qty)
+    logger.debug("updating the posting transactions when updating trade for postings: "+debitName+"/"+creditName + " for total: "+qty.to_s)
+    debitP = self.journal.find_posting_by_sat(debitName)
+    creditP = self.journal.find_posting_by_sat_and_pair_id(creditName, debitP.pair_id)
+    debitP.quantity = qty
+    creditP.quantity = -Float(qty)
+    logger.debug("updated total price for "+self.tradeable_m_symbol_root + "["+debitName+"] to "+debitP.quantity.to_s)
+    debitP.save
+    creditP.save
+  
+  end
 end
--- root/trunk/source/tradebase/test/unit/trade_test.rb
+++ root/trunk/source/tradebase/test/unit/trade_test.rb
@@ -21,6 +21,82 @@ class TradeTest < MarketceteraTestBase
     assert_equal 420.23, theTrade.price_per_share
     assert_equal 19.99, theTrade.total_commission
     assert_equal 420.23 * theTrade.quantity, theTrade.total_price
+  end
+  
+  def test_update_price_per_share
+    t = Trade.new
+    t.create_equity_trade(10, "TOLI", 4.99, 7.50, "USD", "some-account", Date.civil(2006, 10,10))
+    assert_equal 4.99, t.price_per_share
+    assert_equal Float(4.99*10).to_s, t.total_price.to_s
+    
+    # now update the pps - we expect the underlying total price to update too
+    t.price_per_share = 37
+    assert_equal 37, t.price_per_share
+    assert_equal Float(37*10), t.total_price
+    # lookup the posting too
+    verify_trade_prices(t, Float(37*10), 7.50)
+  end 
+  
+  def test_update_qty
+    t = Trade.new
+    t.create_equity_trade(10, "TOLI", 4.99, 7.50, "USD", "some-account", Date.civil(2006, 10,10))
+    assert_equal 10, t.quantity
+    assert_equal Float(4.99*10).to_s, t.total_price.to_s
+    
+    # now update the qty - we expect the underlying total price to update too
+    t.quantity = 37
+    assert_equal 37, t.quantity
+    assert_equal Float(37*4.99), t.total_price
+    # lookup the posting too
+    verify_trade_prices(t, Float(37*4.99), 7.50)
+  end 
+  
+  def test_update_commission
+    t = Trade.new
+    t.create_equity_trade(11, "TOLI", 4.99, 7.50, "USD", "some-account", Date.civil(2006, 10,10))
+    assert_equal 11, t.quantity
+    assert_equal Float(4.99*11), t.total_price
+    
+    # now update the qty - we expect the underlying total price to update too
+    t.total_commission = 14.99
+    assert_equal 14.99, t.total_commission
+    # lookup the posting too
+    verify_trade_prices(t, Float(11*4.99), 14.99)
+  end 
+  
+  def test_update_price_and_qty_and_commission
+    t = Trade.new
+    t.create_equity_trade(10, "TOLI", 4.99, 7.50, "USD", "some-account", Date.civil(2006, 10,10))
+    assert_equal 10, t.quantity
+    assert_equal Float(4.99*10).to_s, t.total_price.to_s
+    
+    # now update the qty - we expect the underlying total price to update too
+    t.total_commission = 14.99
+    t.price_per_share = 25
+    t.quantity = 200
+    assert_equal 14.99, t.total_commission
+    assert_equal 200, t.quantity
+    assert_equal 200*25, t.total_price
+    assert_equal 25, t.price_per_share
+    # lookup the posting too
+    verify_trade_prices(t, Float(25*200), 14.99)
+  end 
+  
+   
+  ##### Helpers ######
 
+  # verifies trade has the right total price + commissions
+  def verify_trade_prices(trade, total_price, total_commission)
+    sti = trade.journal.find_posting_by_sat(SubAccountType::DESCRIPTIONS[:sti])
+    assert_equal total_price, sti.quantity
+    assert_equal -sti.quantity, 
+        trade.journal.find_posting_by_sat_and_pair_id(SubAccountType::DESCRIPTIONS[:cash], sti.pair_id).quantity, 
+        "cash portion of STI is incorrect"
+    
+    comm = trade.journal.find_posting_by_sat(SubAccountType::DESCRIPTIONS[:commissions])
+    assert_equal total_commission, comm.quantity
+    assert_equal -comm.quantity, 
+        trade.journal.find_posting_by_sat_and_pair_id(SubAccountType::DESCRIPTIONS[:cash], comm.pair_id).quantity, 
+        "cash portion of commission is incorrect"   
   end
 end
