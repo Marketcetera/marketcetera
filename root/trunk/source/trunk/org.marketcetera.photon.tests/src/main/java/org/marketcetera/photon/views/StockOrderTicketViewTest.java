3bc19c55679fe294fd7265e385500a9e7e70e924
Graham Miller <gmiller@marketcetera.com>
2006-10-12 00:19:43 +0000

    plugin tests
    git-svn-id: file:///space/source.marketcetera.org@5746 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/trunk/org.marketcetera.photon.tests/src/main/java/org/marketcetera/photon/views/StockOrderTicketViewTest.java
@@ -0,0 +1,22 @@
+package org.marketcetera.photon.views;
+
+import org.marketcetera.photon.views.StockOrderTicket;
+
+public class StockOrderTicketViewTest extends ViewTestBase {
+
+
+	public StockOrderTicketViewTest(String name) {
+		super(name);
+	}
+
+	public void testView() {
+	}
+
+	@Override
+	protected String getViewID() {
+		return StockOrderTicket.ID;
+	}
+
+	
+
+}
