faef252767b6be5811cf8cb72b322e7c1f510db7
Graham Miller <gmiller@marketcetera.com>
2006-10-14 00:07:47 +0000

    Now with working tests!
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@5753 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.photon.tests/src/main/java/org/marketcetera/photon/TS_Photon.java
+++ source/trunk/plugins/org.marketcetera.photon.tests/src/main/java/org/marketcetera/photon/TS_Photon.java
@@ -1,16 +1,20 @@
 package org.marketcetera.photon;
 
-import org.marketcetera.photon.views.FIXMessagesViewTest;
-import org.marketcetera.photon.views.StockOrderTicketViewTest;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.marketcetera.photon.views.AveragePricesViewTest;
+import org.marketcetera.photon.views.FIXMessagesViewTest;
+import org.marketcetera.photon.views.FillsViewTest;
+import org.marketcetera.photon.views.StockOrderTicketViewTest;
 
 public class TS_Photon {
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
 		suite.addTestSuite(StockOrderTicketViewTest.class);
 		suite.addTestSuite(FIXMessagesViewTest.class);
+		suite.addTestSuite(FillsViewTest.class);
+		suite.addTestSuite(AveragePricesViewTest.class);
 		return suite;
 	}
 
