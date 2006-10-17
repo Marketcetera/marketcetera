b0f9242111a90498404824c418b5caf9136642bf
Toli Kuznets <toli@marketcetera.com>
2006-10-17 21:03:39 +0000

    updated id_repository stuff
    git-svn-id: file:///space/source.marketcetera.org@7782 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/test/functional/id_repository_controller_test.rb
+++ root/trunk/source/tradebase/test/functional/id_repository_controller_test.rb
@@ -1,10 +1,13 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../unit/marketcetera_test_base'
 require 'id_repository_controller'
 
 # Re-raise errors caught by the controller.
 class IdRepositoryController; def rescue_action(e) raise e end; end
 
-class IdRepositoryControllerTest < Test::Unit::TestCase
+class IdRepositoryControllerTest < MarketceteraTestBase
+include IdRepositoryHelper
+
   def setup
     @controller = IdRepositoryController.new
     @request    = ActionController::TestRequest.new
@@ -12,7 +15,27 @@ class IdRepositoryControllerTest < Test::Unit::TestCase
   end
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_get_next_batch
+    post :get_next_batch
+    
+    assert_response :success
+    assert_template 'id_repository/get_next_batch'
+    
+    assert_not_nil assigns(:current)
+    assert_not_nil assigns(:num_allowed)
+    assert_equal NumAllowed, assigns(:num_allowed)
+    assert assigns(:current) > 0
+    firstRun = assigns(:current)
+    
+    # get again
+    get :get_next_batch
+    assert firstRun + NumAllowed, assigns(:current)
+    
+    assert_tag :tag => "id", :child => { :tag => "next"}
+    assert_tag :tag => "id", :child => { :tag => "num"}
+    
+    assert_tag :tag => "next", :content => "#{assigns(:current)}"
+    assert_tag :tag => "num", :content => "#{assigns(:num_allowed)}"
+    
   end
 end
