1f6e68db50eef11f1985610943ff74bf08b241d8
Toli Kuznets <toli@marketcetera.com>
2006-10-24 01:30:02 +0000

    brought pessimistic locking plugin back into our SVN
    git-svn-id: file:///space/source.marketcetera.org@7799 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/tradebase/vendor/plugins/pessimistic_locking/lib/pessimistic_locking.rb
@@ -0,0 +1,33 @@
+module ActiveRecord
+  module PessimisticLocking
+    def self.append_features(base)
+      super(base)
+      base.extend(ClassMethods)
+      class << base
+        VALID_FIND_OPTIONS.push(:lock)
+        alias_method(:add_limit_without_pessimistic_lock!, :add_limit!)
+        alias_method(:add_limit!, :add_limit_with_pessimistic_lock!)
+      end
+    end
+
+    def lock
+      clear_aggregation_cache
+      clear_association_cache
+      record = self.class.find(self.id, :lock => true)
+      @attributes.update(record.instance_variable_get('@attributes'))
+    end
+
+    module ClassMethods
+      def add_limit_with_pessimistic_lock!(sql, options, scope=nil)
+        if (scope == nil)
+          add_limit_without_pessimistic_lock!(sql, options)
+        else
+          add_limit_without_pessimistic_lock!(sql, options, scope)
+        end
+        if options[:lock]
+          sql << " FOR UPDATE"
+        end
+      end
+    end
+  end
+end
