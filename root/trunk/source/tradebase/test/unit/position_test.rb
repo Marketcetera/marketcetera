53fbcae5b347fe844c5a543f1623f4004d418c98
Toli Kuznets <toli@marketcetera.com>
2006-10-26 23:44:34 +0000

    Removed the unused postings and positions fixtures removed references to positions fixtures from posting test
    git-svn-id: file:///space/source.marketcetera.org@7811 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/test/unit/position_test.rb
+++ root/trunk/source/tradebase/test/unit/position_test.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PositionTest < Test::Unit::TestCase
-  fixtures :positions
 
   # Replace this with your real tests.
   def test_truth
--- root/trunk/source/tradebase/test/unit/posting_test.rb
+++ root/trunk/source/tradebase/test/unit/posting_test.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PostingTest < Test::Unit::TestCase
-  fixtures :postings
 
   # Replace this with your real tests.
   def test_truth
