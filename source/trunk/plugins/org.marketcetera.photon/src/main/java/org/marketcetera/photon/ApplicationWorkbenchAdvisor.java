0801d0aa235f0e46f29d897272f68d1686688adf
alissovski <alissovski@marketcetera.com>
2006-11-17 14:47:04 +0000

    Fix for #73 (An error dialog when saving a Ruby file with a syntax error).
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@5927 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ApplicationWorkbenchAdvisor.java
+++ source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ApplicationWorkbenchAdvisor.java
@@ -1,12 +1,20 @@
 package org.marketcetera.photon;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.internal.ide.IDEInternalWorkbenchImages;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 import org.eclipse.ui.internal.ide.model.WorkbenchAdapterBuilder;
+import org.osgi.framework.Bundle;
 
 /**
  * Class required by the RCP to initialize the workbench.
@@ -41,11 +49,58 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	public void initialize(IWorkbenchConfigurer configurer) {
 		configurer.setSaveAndRestore(true);
 		
-		// explicitly register ide- and resource-related adapters that rdt relies on
-		WorkbenchAdapterBuilder.registerAdapters();
+		registerIdeAdapters();
+		declareIdeWorkbenchImages();
 		
 		ConsolePlugin.getDefault().getConsoleManager().addConsoles(
 				new IConsole[] { new MainConsole() });
+	}
+
+	/**
+	 * Explicitly registers IDE- and resource-related adapters that RDT relies on.
+	 */
+	private void registerIdeAdapters() {
+		WorkbenchAdapterBuilder.registerAdapters();
+	}
+
+	/**
+	 * Declares shared images that RDT relies on. 
+	 * <p>
+	 * In the context of the IDE, this is done by the <code>IDEWorkbenchAdvisor.declareWorkbenchImages()</code>. 
+	 * In the context of an RCP app, however, <code>IDEWorkbenchAdvisor</code> is not used. In addition, we cannot 
+	 * directly invoke the method since it has private access. We thus have to explicitly declare the few shared 
+	 * IDE images that RDT needs here.   
+	 */
+	private void declareIdeWorkbenchImages() {
+
+		final String ICONS_PATH = "$nl$/icons/full/";//$NON-NLS-1$
+		final String PATH_ETOOL = ICONS_PATH + "etool16/"; // Enabled toolbar icons.//$NON-NLS-1$
+
+		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+
+		declareIdeWorkbenchImage(ideBundle,
+				IDEInternalWorkbenchImages.IMG_ETOOL_PROBLEM_CATEGORY,
+				PATH_ETOOL + "problem_category.gif", true); //$NON-NLS-1$
+	}
+	
+	/**
+	 * Declares an IDE-specific workbench image.
+	 * 
+	 * @param symbolicName
+	 *            the symbolic name of the image
+	 * @param path
+	 *            the path of the image file; this path is relative to the base
+	 *            of the IDE plug-in
+	 * @param shared
+	 *            <code>true</code> if this is a shared image, and
+	 *            <code>false</code> if this is not a shared image
+	 * @see IWorkbenchConfigurer#declareImage
+	 */
+	private void declareIdeWorkbenchImage(Bundle ideBundle, String symbolicName,
+			String path, boolean shared) {
+		URL url = Platform.find(ideBundle, new Path(path));
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		getWorkbenchConfigurer().declareImage(symbolicName, desc, shared);
 	}
 
 }
