72eb66c29315145ec91a40c107ee23fd7b9b78f4
Toli Kuznets <toli@marketcetera.com>
2006-11-20 23:16:06 +0000

    improved navigation handling - the right navigation section now opens when you go to each link
    git-svn-id: file:///space/source.marketcetera.org@7870 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/tradebase/app/helpers/navigation_helper.rb
@@ -0,0 +1,18 @@
+# Works in conjunction with navigation.js to manage the 
+# left-side navigation sidebar.
+
+module NavigationHelper
+  
+  NAVIGATION_MAPPING = {
+    'positions' => 'position', 
+    'trades' => 'trades', 
+    'welcome' => 'trades',
+    'message_logs' => 'trades',
+    'queries' => 'reports',
+    'accounts' => 'db_maintenance',
+    'currencies' => 'db_maintenance',
+    'equities' => 'db_maintenance',
+    'dividends' => 'db_maintenance',
+    'm_symbols' => 'db_maintenance',
+  }
+end
--- root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
+++ root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
@@ -4,15 +4,20 @@
  */
 
 /* Toggle the show/hide of the date range selection div in the import trade page */
+var open = false;
+
 var subset = {
   '#subset' : function(element) {
     if(element.checked == false) new Effect.BlindUp('subset_content', {duration: 0.0});
+    if(element.checked == true) open = true;
   },
   '#subset:click': function(element) {
-    new Effect.BlindDown('subset_content', {duration: 0.5});
+    if (open == false) new Effect.BlindDown('subset_content', {duration: 0.5});
+    open = true;
   },
   '#all:click': function(element) {
     new Effect.BlindUp('subset_content', {duration: 0.5});
+    open = false;
   }
 }
 
