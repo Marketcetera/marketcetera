f3d4b18c100f6dd6fe93916f3f98f4c04608ad7c
Toli Kuznets <toli@marketcetera.com>
2006-11-21 02:59:29 +0000

    Fixed bug in marketcetera.js where we were overriding a JS var open which broke the debug popup window operation Fixed all the blank forms (which we used for layout/formatting) to have empty actions to conform to HTML spec fixed breadcrumbs to have right top-controller links for navigation (had the entire controller object instead of just name)
    git-svn-id: file:///space/source.marketcetera.org@7876 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
+++ root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
@@ -4,20 +4,20 @@
  */
 
 /* Toggle the show/hide of the date range selection div in the import trade page */
-var open = false;
+var alreadyOpen = false;
 
 var subset = {
   '#subset' : function(element) {
     if(element.checked == false) new Effect.BlindUp('subset_content', {duration: 0.0});
-    if(element.checked == true) open = true;
+    if(element.checked == true) alreadyOpen = true;
   },
   '#subset:click': function(element) {
-    if (open == false) new Effect.BlindDown('subset_content', {duration: 0.5});
-    open = true;
+    if (alreadyOpen == false) new Effect.BlindDown('subset_content', {duration: 0.5});
+    alreadyOpen = true;
   },
   '#all:click': function(element) {
     new Effect.BlindUp('subset_content', {duration: 0.5});
-    open = false;
+    alreadyOpen = false;
   }
 }
 
