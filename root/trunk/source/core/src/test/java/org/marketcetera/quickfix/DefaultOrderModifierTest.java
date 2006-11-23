f186c8d1d167dcd92984baf32654d7c29873f4ca
Toli Kuznets <toli@marketcetera.com>
2006-11-23 01:19:38 +0000

    Merged tests from oms.DefaultOrderModiferTest into core.DefaultOrderModifierTest throw an exception in DefaultOrderModifier if we can't parse the modifier field correctly
    git-svn-id: file:///space/source.marketcetera.org@569 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/core/src/test/java/org/marketcetera/quickfix/DefaultOrderModifierTest.java
+++ root/trunk/source/core/src/test/java/org/marketcetera/quickfix/DefaultOrderModifierTest.java
@@ -2,12 +2,15 @@ package org.marketcetera.quickfix;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
-import org.marketcetera.core.ExpectedTestFailure;
-import org.marketcetera.core.MarketceteraTestSuite;
-import org.marketcetera.core.ClassVersion;
+import org.marketcetera.core.*;
 import quickfix.FieldNotFound;
 import quickfix.Message;
 import quickfix.StringField;
+import quickfix.field.MsgType;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.prefs.BackingStoreException;
 
 /**
  * @author Graham Miller
@@ -15,6 +18,11 @@ import quickfix.StringField;
  */
 @ClassVersion("$Id$")
 public class DefaultOrderModifierTest extends TestCase {
+    private static final String HEADER_57_VAL = "asdf";
+    private static final String FIELD_21_VAL = "qwer";
+    private static final String TRAILER_28_VAL = "ppp";
+    private static final String FIELD_42_VAL = "123456789101112";
+
     public DefaultOrderModifierTest(String inName) {
         super(inName);
     }
@@ -41,6 +49,18 @@ public class DefaultOrderModifierTest extends TestCase {
                 }
             }.run();
     }
+
+    public void testIncorrectModiferListingFormat()
+    {
+        final DefaultOrderModifier mod = new DefaultOrderModifier();
+        new ExpectedTestFailure(MarketceteraException.class,
+                                MessageKey.ORDER_MODIFIER_WRONG_FIELD_FORMAT.getLocalizedMessage("27(app")) {
+            protected void execute() throws Throwable {
+                mod.setMsgFields(createFieldsMap(new String[][]{{"27(app", "malformed"}}));
+            }
+        }.run();
+
+    }
     
     public void testModifyOrderValueExists() throws Exception {
         String replacementValue = "Replacement value";
@@ -56,4 +76,74 @@ public class DefaultOrderModifierTest extends TestCase {
         assertEquals(originalValue, aMessage.getField(outField).getValue());
 
     }
+
+    public void testModifyOrderWithPredicate() throws BackingStoreException, FieldNotFound, MarketceteraException {
+        DefaultOrderModifier mod = new DefaultOrderModifier();
+        mod.setHeaderFields(createFieldsMap(new String[][]{{"57(*)", HEADER_57_VAL}}));
+        mod.setMsgFields(createFieldsMap(new String[][]{{"21(d)", FIELD_21_VAL}, {"42(admin)", FIELD_42_VAL}}));
+        mod.setTrailerFields(createFieldsMap(new String[][]{{"28(app)", TRAILER_28_VAL}}));
+
+        Message heartbeat = FIXMessageUtil.createNewMessage();
+        heartbeat.getHeader().setField(new MsgType(MsgType.HEARTBEAT));
+
+        Message newOrderSingle = FIXMessageUtil.createNewMessage();
+        newOrderSingle.getHeader().setField(new MsgType(MsgType.ORDER_SINGLE));
+
+        Message logon = FIXMessageUtil.createNewMessage();
+        logon.getHeader().setField(new MsgType(MsgType.LOGON));
+
+        assertTrue(mod.modifyOrder(heartbeat));
+        assertTrue(mod.modifyOrder(newOrderSingle));
+        assertTrue(mod.modifyOrder(logon));
+
+        assertEquals(HEADER_57_VAL, heartbeat.getHeader().getString(57));
+        assertFalse(heartbeat.isSetField(21));
+        assertFalse(heartbeat.getTrailer().isSetField(28));
+        assertEquals(FIELD_42_VAL, heartbeat.getString(42));
+
+        assertEquals(HEADER_57_VAL, newOrderSingle.getHeader().getString(57));
+        assertEquals(FIELD_21_VAL, newOrderSingle.getString(21));
+        assertEquals(TRAILER_28_VAL, newOrderSingle.getTrailer().getString(28));
+        assertFalse(newOrderSingle.isSetField(42));
+
+        assertEquals(HEADER_57_VAL, logon.getHeader().getString(57));
+        assertFalse(logon.isSetField(21));
+        assertFalse(logon.getTrailer().isSetField(28));
+        assertEquals(FIELD_42_VAL, logon.getString(42));
+    }
+
+    /** Put one extra field in the config and make sure that it appears
+     * in the vanilla message after initializing the modifier and running the message
+     * through it
+     * @throws Exception
+     */
+    public void testModifyOrderWithAllMessageTypeModifiers() throws Exception
+    {
+        DefaultOrderModifier mod = new DefaultOrderModifier();
+        mod.setHeaderFields(createFieldsMap(new String[][]{{"57", HEADER_57_VAL}}));
+        mod.setMsgFields(createFieldsMap(new String[][]{{"21", FIELD_21_VAL}}));
+        mod.setTrailerFields(createFieldsMap(new String[][]{{"28", TRAILER_28_VAL}}));
+
+        Message msg = FIXMessageUtil.createNewMessage();
+        assertTrue(mod.modifyOrder(msg));
+        assertEquals(HEADER_57_VAL, msg.getHeader().getString(57));
+        assertEquals(FIELD_21_VAL, msg.getString(21));
+        assertEquals(TRAILER_28_VAL, msg.getTrailer().getString(28));
+    }
+
+
+
+    /** Given an array of key->value mappings, createsa a map out of them
+     * Example:
+     *  createFieldsMap(new String[][]{{"21(d)", FIELD_21_VAL}, {"42(admin)", FIELD_42_VAL}});
+     */
+    public static Map<String, String> createFieldsMap(String[][] mappings)
+    {
+        HashMap<String, String> result = new HashMap<String, String>();
+        for (String[] mapping : mappings) {
+            result.put(mapping[0], mapping[1]);
+        }
+        return result;
+    }
+
 }
