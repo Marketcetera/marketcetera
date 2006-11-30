7412a92cce6861f06c5dba868ec6305d3030b0a5
Graham Miller <gmiller@marketcetera.com>
2006-11-30 01:14:52 +0000

    changed JmsTemplate to JmsOperations
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@2399 501a4a96-914b-0410-8454-f5988a4ca596

--- source/oms/src/main/java/org/marketcetera/oms/QuickFIXApplication.java
+++ source/oms/src/main/java/org/marketcetera/oms/QuickFIXApplication.java
@@ -1,6 +1,6 @@
 package org.marketcetera.oms;
 
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.JmsOperations;
 
 import quickfix.Application;
 import quickfix.DoNotSend;
@@ -14,20 +14,20 @@ import quickfix.UnsupportedMessageType;
 
 public class QuickFIXApplication implements Application {
 
-    private JmsTemplate jmsTemplate;
+    private JmsOperations jmsOperations;
 
 	public QuickFIXApplication() {
 	}
 	
 	public void fromAdmin(Message message, SessionID session) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
-		if (jmsTemplate != null){
-			jmsTemplate.convertAndSend(message);
+		if (jmsOperations != null){
+			jmsOperations.convertAndSend(message);
 		}
 	}
 
 	public void fromApp(Message message, SessionID session) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
-		if (jmsTemplate != null){
-			jmsTemplate.convertAndSend(message);
+		if (jmsOperations != null){
+			jmsOperations.convertAndSend(message);
 		}
 	}
 
@@ -46,12 +46,12 @@ public class QuickFIXApplication implements Application {
 	public void toApp(Message message, SessionID session) throws DoNotSend {
 	}
 
-	public JmsTemplate getJmsTemplate() {
-		return jmsTemplate;
+	public JmsOperations getJmsOperations() {
+		return jmsOperations;
 	}
 
-	public void setJmsTemplate(JmsTemplate jmsTemplate) {
-		this.jmsTemplate = jmsTemplate;
+	public void setJmsOperations(JmsOperations jmsOperations) {
+		this.jmsOperations = jmsOperations;
 	}
 
 }
