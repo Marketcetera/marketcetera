77ea40f05e46d131e9900d5393b6d86686b75b02
Toli Kuznets <toli@marketcetera.com>
2006-12-01 04:36:37 +0000

    added some icons to front page, created a partial for all show/edit/delete links
    git-svn-id: file:///space/source.marketcetera.org@7942 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
+++ root/trunk/source/tradebase/public/javascripts/marketcetera/marketcetera.js
@@ -24,3 +24,19 @@ var subset = {
 EventSelectors.addLoadEvent(function() {
   EventSelectors.start(subset);
 });
+
+/* Toggles the checkboxes in the form on/off */ 
+function toggleCheckboxes(formName, value){
+        var boxes = Form.getInputs(formName, 'checkbox')
+        options = $A(boxes);
+        options.each( function(oneBox){
+                oneBox.checked = value;
+        });
+}
+
+/*  Toggles the date selection fields on/off when date selection radio buttons are switched */
+function toggleDivSelection(divName, value) {
+    var divs = $(divName)
+    if(value)   divs.style.display = "block";
+    else divs.style.display = "none";
+}
---
+++ root/trunk/source/tradebase/test/unit/positions_helper_test.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/marketcetera_test_base'
+
+class PositionssHelperTest < MarketceteraTestBase
+  fixtures :currencies
+  include PositionsHelper
+
+  def test_get_top_positioned_accounts
+    create_trades_in_account(3, "TOLI")
+    create_trades_in_account(5, "RAMA")
+    create_trades_in_account(2, "LENA")
+    create_trades_in_account(3, "MOLI")
+
+    accounts = get_top_positioned_accounts(3)
+    assert_equal 3, accounts.length
+    assert_equal ["RAMA", 5], [accounts[0][0].nickname, accounts[0][1]]
+    assert_equal ["TOLI", 3], [accounts[1][0].nickname, accounts[1][1]]
+    assert_equal ["MOLI", 3], [accounts[2][0].nickname, accounts[2][1]]
+  end
+  
+  private
+  def create_trades_in_account(num_trades, account)
+    for i in 1..num_trades
+      create_test_trade(100, 20.11, Side::QF_SIDE_CODE[:buy], account, Date.today, "IFLI-"+i.to_s, 4.99, "USD")
+    end
+  end  
+end
