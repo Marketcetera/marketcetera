0a14225a14c3fc554d7898f57aa70b743cefd2f1
Graham Miller <gmiller@marketcetera.com>
2007-01-18 05:30:20 +0000

    Now ensures the existence of an ActiveScripts project.  This is now the only project in which scripts execute.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6089 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/EclipseUtils.java
+++ source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/EclipseUtils.java
@@ -3,7 +3,9 @@ package org.marketcetera.photon;
 import java.io.IOException;
 import java.net.URL;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
@@ -11,11 +13,17 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Plugin;
 
 public class EclipseUtils {
+	
 	public static IPath getWorkspacePath()
 	{
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IPath fullPath = workspace.getRoot().getRawLocation();
 		return fullPath;
+	}
+
+	public static IProject[] getProjects(){
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		return root.getProjects();
 	}
 	
 	public static IPath getPluginPath(Plugin plugin){
