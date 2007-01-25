f009d5e8f4b5f4f7fb52dc36df30594ab3572c50
Graham Miller <gmiller@marketcetera.com>
2007-01-25 01:11:09 +0000

    fixed bug #20, Stock order ticket fields don't show up properly on linux-gtk
    git-svn-id: file:///space/source.marketcetera.org@6101 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/EclipseUtils.java
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/EclipseUtils.java
@@ -11,6 +11,10 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Control;
 
 public class EclipseUtils {
 	
@@ -35,6 +39,34 @@ public class EclipseUtils {
 			return null;
 		}
 		return Path.fromOSString(fileURL.getFile());
+	}
+
+	/**
+	 * Determines the size of a text area based on possible hints.  If there is a 
+	 * charWidthHint greater than 0, that will be used to calcualate the width,
+	 * otherwise the defaultString will be used.
+	 * 
+	 * @param aComposite 
+	 * @param defaultString
+	 * @param charWidthHint
+	 * @param heightFactorHint
+	 * @return
+	 */
+	public static Point getTextAreaSize(Control aComposite, String defaultString, int charWidthHint, double heightFactorHint) {
+		Point sizeHint = new Point(0,0);
+		GC gc = new GC(aComposite);
+		gc.setFont(aComposite.getFont());
+		FontMetrics fm = gc.getFontMetrics();
+		if (charWidthHint > 0) {
+			int averageCharWidth = fm.getAverageCharWidth();
+			sizeHint.x = averageCharWidth * charWidthHint;
+		} else {
+			Point extent = gc.textExtent(defaultString);
+			sizeHint.x = extent.x;
+		}
+		sizeHint.y = (int) (fm.getHeight() * heightFactorHint);
+		gc.dispose();
+		return sizeHint;
 	}
 
 }
