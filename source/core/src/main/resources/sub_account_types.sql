1219a7107909fe23a23c6fe3fa4c3e65fc24b7b2
Graham Miller <gmiller@marketcetera.com>
2007-01-30 00:17:02 +0000

    removed obsolete files...
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@721 501a4a96-914b-0410-8454-f5988a4ca596

--- source/core/src/main/resources/sub_account_types.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO sub_account_types (accounting_account_type, description, created_on, updated_on) VALUES
-("A", "Short Term Investment", NOW(), NOW() ),
-("A", "Cash", NOW(), NOW() ),
-("R", "Dividend Revenue", NOW(), NOW() ),
-("A", "Unrealized Gain/Loss", NOW(), NOW() ),
-("R", "Change on Close of Investment", NOW(), NOW() ),
-("E", "Commissions", NOW(), NOW() ),
-("R", "Interest Revenue", NOW(), NOW() );
