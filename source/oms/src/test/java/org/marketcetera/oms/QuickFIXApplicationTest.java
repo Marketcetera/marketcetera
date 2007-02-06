ff3bbe53ccef7f79b3946b65f2100c749c3dd00c
Toli Kuznets <toli@marketcetera.com>
2007-02-06 20:51:57 +0000

    Added the test for ticket #120 that should've gone into changest [727] but was left out
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@2534 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ source/oms/src/test/java/org/marketcetera/oms/QuickFIXApplicationTest.java
@@ -0,0 +1,49 @@
+package org.marketcetera.oms;
+
+import org.marketcetera.core.MarketceteraTestSuite;
+import org.marketcetera.core.ClassVersion;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.UncategorizedJmsException;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import quickfix.Message;
+import quickfix.SessionID;
+
+/**
+ * Verifies that we don't error out of the sending functions
+ * even if the underlying JMS connection goes stale
+ * Otherwise we end up logging out of the FIX connection as well
+ * This is in reference to ticket 120
+ * 
+ * @author toli
+ * @version $Id$
+ */
+
+@ClassVersion("$Id$")
+public class QuickFIXApplicationTest extends TestCase {
+    public QuickFIXApplicationTest(String inName) {
+        super(inName);
+    }
+
+    public static Test suite() {
+        return new MarketceteraTestSuite(QuickFIXApplicationTest.class, OrderManagementSystem.OMS_MESSAGE_BUNDLE_INFO);
+    }
+
+
+    public void testMessageSendWhenJMSBarfs() throws Exception {
+        QuickFIXApplication qfApp = new QuickFIXApplication();
+        JmsOperations ops = new JmsTemplate() {
+
+            public void convertAndSend(Object message) throws JmsException {
+                throw new UncategorizedJmsException("we always throw an exception");
+            }
+        };
+        qfApp.setJmsOperations(ops);
+
+        // these should not fail
+        qfApp.fromAdmin(new Message(), new SessionID());
+        qfApp.fromApp(new Message(), new SessionID());
+    }
+}
