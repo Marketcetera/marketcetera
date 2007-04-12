fec2eef9ece209918db5052858ef62a301f2be76
Toli Kuznets <toli@marketcetera.com>
2007-04-12 23:42:24 +0000

    Fix for bug #175 the Message_log controller now shows (as an error) the # of messages that failed to be parsed correctly. They are also showing up above all valid importable messages, and have their own view too.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@8279 501a4a96-914b-0410-8454-f5988a4ca596

--- source/tradebase/app/controllers/message_logs_controller.rb
+++ source/tradebase/app/controllers/message_logs_controller.rb
@@ -20,27 +20,37 @@ class MessageLogsController < ApplicationController
       @endDate = parse_date_from_params(params, :dates, "end_date")
     end
     all_exec_reports = []
+    @failed_msg = []
     msgTypeField = Quickfix::MsgType.new
     ordStatusField = Quickfix::OrdStatus.new
     sendingTimeField = Quickfix::SendingTime.new
     all_messages = MessageLog.find(:all, :conditions => [ 'processed = false' ])
     all_messages.each { |msg| 
-      if(msg.executed_trade?)
-        if(fSubsetSearch) 
-           logger.error("checking "+msg.sending_time.to_s + " against [" + @startDate.to_s + " to "+@endDate.to_s+"]")
-           if((msg.sending_time >= @startDate) && (msg.sending_time <= @endDate))
+      begin 
+        if(msg.executed_trade?)
+          if(fSubsetSearch) 
+             logger.error("checking "+msg.sending_time.to_s + " against [" + @startDate.to_s + " to "+@endDate.to_s+"]")
+             if((msg.sending_time >= @startDate) && (msg.sending_time <= @endDate))
+               all_exec_reports << msg
+             else 
+              logger.error("discarding date "+msg.sending_time.to_s)
+             end
+          else 
              all_exec_reports << msg
-           else 
-            logger.error("discarding date "+msg.sending_time.to_s)
-           end
-        else 
-           all_exec_reports << msg
+          end
         end
+      rescue => ex
+         logger.debug("encountered error parsing QF message["+msg.id.to_s+"]: "+ex.message)
+         logger.debug("malformed message: "+msg.text)
+         @failed_msg << msg
       end
     }
   
     if(all_exec_reports.empty?)
       flash.now[:error] = "No matching messages found"
+    end
+    if(!@failed_msg.empty?) 
+      flash.now[:error] = "Failed to parse " + @failed_msg.size.to_s + " message(s)."
     end
     # paginate manually
     @paginator = Paginator.new(self, all_exec_reports.length, MaxPerPage, params[:page])
@@ -54,5 +64,9 @@ class MessageLogsController < ApplicationController
         flash[:notice] = 'Specified message is not a valid executed trade.'
         redirect_to :action => 'list'      
       end
+  end
+  
+  def show_unparseable_msg
+    @message = MessageLog.find(params[:id])
   end
 end
