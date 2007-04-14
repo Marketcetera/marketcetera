d0f4650188507cf76cad477db66db4cfb91a5424
cleung <cleung>
2007-04-14 04:36:35 +0000

    Replaced custom error-highlighting control with Eclipse ControlDecoration control in Stock Order Ticket for error reporting. - Backported ControlDecoration class into ui.validation package from Eclipse 3.3 (will need to remove once upgrade to 3.3) - Used Eclipse standard message_error image as the ControlDecoration image for now, added to left upper corner of the control - Set background color of the control to red when error is detected - Made the specific error message a hover text on both the ControlDecoration image and also the control - Left the error message at the bottom of the ticket view for now (seems more user-friendly - same place for any errors, no need to move mouse to show errors) - Modified horizontalSpacing and marginWidth in the StockOrderTicket form so the entire error image can be shown without being clipped
    Things to decide
    - Display custom image instead of the Eclipse message_error image?  
    - Image position, size, etc. 
    - Keep the red background highlighting of the actual control when an erroneous input occurs?
    - Where to display the error message?  As a hover text on image and control, print directly under the control, (and/or) at the bottom of the view (as it is currently)?
    
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6340 501a4a96-914b-0410-8454-f5988a4ca596

--- source/branches/0.4-options/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/validation/ParentColorHighlighter.java
+++ source/branches/0.4-options/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/validation/ParentColorHighlighter.java
@@ -1,32 +1,37 @@
 package org.marketcetera.photon.ui.validation;
 
+import org.eclipse.jface.fieldassist.FieldDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 public class ParentColorHighlighter implements IControlHighlighter {
 
-	Color initialColor;
-	Color errorColor;
-	Control referenceControl;
-	Composite parentComposite;
-	
+	ControlDecoration cd;
+
 	public ParentColorHighlighter(Control control) {
-		this.referenceControl = control;
-		this.parentComposite = control.getParent();
-		initialColor = parentComposite.getBackground();
-		errorColor = Display.getCurrent().getSystemColor(SWT.COLOR_RED);
 		control.setData(IControlValidator.CONTROL_HIGHLIGHTER_KEY, this);
+		cd = new ControlDecoration(control, SWT.LEFT | SWT.UP);
+		cd.setMarginWidth(0);
+		FieldDecoration deco = FieldDecorationRegistry.getDefault()
+				.getFieldDecoration(FieldDecorationRegistry.DEC_ERROR);
+		cd.setImage(deco.getImage());
+		cd.hide();
 	}
 
 	public void clearHighlight() {
-		parentComposite.setBackground(initialColor);
+		cd.hide();
+		cd.getControl().setBackground(null);
+		cd.getControl().setToolTipText("");
 	}
 
-	public void highlightError() {
-		parentComposite.setBackground(errorColor);
+	public void highlightError(String errorMessage) {
+		cd.setDescriptionText(errorMessage);
+		cd.getControl().setBackground(
+				Display.getCurrent().getSystemColor(SWT.COLOR_RED));
+		cd.getControl().setToolTipText(errorMessage);
+		cd.show();
 	}
 
 }
