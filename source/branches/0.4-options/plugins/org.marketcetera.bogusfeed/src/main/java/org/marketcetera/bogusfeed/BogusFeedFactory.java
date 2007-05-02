b56d4968e9d006c5257c6b93d4a33a5b97750f4b
Graham Miller <gmiller@marketcetera.com>
2007-05-02 00:54:21 +0000

    Merged revisions 957-958,961,963-967,970,972-974,976-1033,1036-1047 via svnmerge from http://svn.marketcetera.org/client/photon/trunk
    ........
      r1040 | gmiller | 2007-05-01 17:09:03 -0700 (Tue, 01 May 2007) | 1 line
      
      Added required backport-util-concurrent-2.1.jar
    ........
      r1041 | gmiller | 2007-05-01 17:15:41 -0700 (Tue, 01 May 2007) | 1 line
      
      Adding new marketdata feed implementation, org.marketcetera.marketdata (modified market data API)
    ........
      r1043 | gmiller | 2007-05-01 17:16:23 -0700 (Tue, 01 May 2007) | 1 line
      
      Adding new marketdata feed implementation, org.marketcetera.marketdata (modified market data API)
    ........
      r1044 | gmiller | 2007-05-01 17:16:40 -0700 (Tue, 01 May 2007) | 1 line
      
      Adding new marketdata feed implementation, org.marketcetera.marketdata (modified market data API)
    ........
      r1045 | gmiller | 2007-05-01 17:19:57 -0700 (Tue, 01 May 2007) | 1 line
      
      Adding new marketdata feed implementation, org.marketcetera.marketdata (modified market data API)
    ........
      r1046 | gmiller | 2007-05-01 17:33:30 -0700 (Tue, 01 May 2007) | 1 line
      
      added new marketdata plugin
    ........
    
    
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6440 501a4a96-914b-0410-8454-f5988a4ca596

--- source/branches/0.4-options/plugins/org.marketcetera.bogusfeed/src/main/java/org/marketcetera/bogusfeed/BogusFeedFactory.java
+++ source/branches/0.4-options/plugins/org.marketcetera.bogusfeed/src/main/java/org/marketcetera/bogusfeed/BogusFeedFactory.java
@@ -1,12 +1,18 @@
 package org.marketcetera.bogusfeed;
 
+import java.util.Map;
+
 import org.marketcetera.marketdata.IMarketDataFeed;
 import org.marketcetera.marketdata.IMarketDataFeedFactory;
 
 public class BogusFeedFactory implements IMarketDataFeedFactory {
 
-	public IMarketDataFeed getInstance(String url, String userName, String password) {
+	public IMarketDataFeed getInstance(String url, String userName, String passwordString, Map<String,Object> properties) {
 		return new BogusFeed();
+	}
+
+	public String[] getAllowedPropertyKeys() {
+		return new String[0];
 	}
 
 }
