8c0e4b28172df7fa17040928e043d7364c3f5361
Toli Kuznets <toli@marketcetera.com>
2007-05-17 21:14:44 +0000

    modified IMarketDataListener interface to have a onMessage command instead of the old onTrade/onQuote/onLevel2
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@1215 501a4a96-914b-0410-8454-f5988a4ca596

--- source/core/src/main/java/org/marketcetera/marketdata/MarketDataListener.java
+++ source/core/src/main/java/org/marketcetera/marketdata/MarketDataListener.java
@@ -1,47 +1,14 @@
 package org.marketcetera.marketdata;
 
 import quickfix.Message;
-import quickfix.field.MsgType;
-import quickfix.field.NoMDEntries;
 
 public abstract class MarketDataListener implements IMarketDataListener {
 
-	public void onMessage(Message aMessage) {
-		try {
-			String msgTypeString = aMessage.getHeader().getString(MsgType.FIELD);
-			if (MsgType.EXECUTION_REPORT.equals(msgTypeString)) {
-				onTrade(aMessage);
-			}
-			if (MsgType.MARKET_DATA_SNAPSHOT_FULL_REFRESH.equals(msgTypeString)) {
-				onQuote(aMessage);
-			}
-		} catch (Exception e) {
-			System.out.println(""+e);
-		}
-	}
+	public abstract void onMessage(Message aMessage);
 
 	public void onMessages(Message[] messages) {
         for (Message message : messages) {
             onMessage(message);
         }
     }
-
-	public void onQuotes(Message[] messages) {
-        for (Message message : messages) {
-            onQuote(message);
-        }
-    }
-
-	public void onTrades(Message[] trades) {
-        for (Message trade : trades) {
-            onTrade(trade);
-        }
-    }
-
-
-    public abstract void onQuote(Message aQuote);
-
-    public abstract void onTrade(Message aTrade);
-    
-
 }
