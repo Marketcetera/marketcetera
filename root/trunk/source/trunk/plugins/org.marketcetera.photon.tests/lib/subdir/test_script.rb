6c809c2920da5cf1a2e568f1152a3ba8215e2ed0
Graham Miller <gmiller@marketcetera.com>
2007-05-23 22:36:32 +0000

    Changed the ScriptRegistry to use a Java hashtable of registrations rather than a JRuby hash
    git-svn-id: file:///space/source.marketcetera.org@6618 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/trunk/plugins/org.marketcetera.photon.tests/lib/subdir/test_script.rb
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon.tests/lib/subdir/test_script.rb
@@ -1,5 +1,7 @@
+include_class "org.marketcetera.photon.scripting.Strategy"
+
 module Subdir
-	class TestScript
+	class TestScript < Strategy
 	  def on_market_data_message(message)
 	    $quote_count = $quote_count + 81
 	  end
