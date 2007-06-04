87ddd270adb2cc72afa8076ef59b3bd99a134ad3
Graham Miller <gmiller@marketcetera.com>
2007-06-04 18:26:04 +0000

    Added logging for data feeds.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6672 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.bogusfeed/src/main/java/org/marketcetera/bogusfeed/BogusFeedFactory.java
+++ source/trunk/plugins/org.marketcetera.bogusfeed/src/main/java/org/marketcetera/bogusfeed/BogusFeedFactory.java
@@ -2,12 +2,17 @@ package org.marketcetera.bogusfeed;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.marketcetera.core.NoOpLogger;
 import org.marketcetera.marketdata.IMarketDataFeed;
 import org.marketcetera.marketdata.IMarketDataFeedFactory;
 
 public class BogusFeedFactory implements IMarketDataFeedFactory {
 
 	public IMarketDataFeed getInstance(String url, String userName, String passwordString, Map<String,Object> properties) {
+		return getInstance(url, userName, passwordString, properties, new NoOpLogger("BogusFeed"));
+	}
+	public IMarketDataFeed getInstance(String url, String userName, String passwordString, Map<String,Object> properties, Logger logger) {
 		return new BogusFeed();
 	}
 
