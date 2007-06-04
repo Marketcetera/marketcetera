ae8d627981713391e9a6b544fd21723d9478de28
Graham Miller <gmiller@marketcetera.com>
2007-06-04 18:26:20 +0000

    AAdded logging for data feeds.
    git-svn-id: file:///space/source.marketcetera.org@1290 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/core/src/main/java/org/marketcetera/marketdata/IMarketDataFeedFactory.java
+++ root/trunk/source/core/src/main/java/org/marketcetera/marketdata/IMarketDataFeedFactory.java
@@ -2,10 +2,12 @@ package org.marketcetera.marketdata;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.marketcetera.core.MarketceteraException;
 
 public interface IMarketDataFeedFactory {
 	public IMarketDataFeed getInstance(String url, String userName, String password, Map<String, Object> properties) throws MarketceteraException;
+	public IMarketDataFeed getInstance(String url, String userName, String password, Map<String, Object> properties, Logger logger) throws MarketceteraException;
 
 	public String [] getAllowedPropertyKeys();
 }
