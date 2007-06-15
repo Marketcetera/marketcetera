7dc280f9dcd480b702220476c83f55858b8c893a
Toli Kuznets <toli@marketcetera.com>
2007-06-15 02:17:44 +0000

    Fix for bug #292
    Modified the workaround written for #292 to instead "repairing" the data bindings to have a separate
    constructor that doesn't use DataDictionary to retrieve field type and field name
    
    The main problem was that both PutOrCall and OrderCapacity are present in some FIX versions and not in others, so the data dictionary was not finding them.
    
    now we just manually specify the field type/name instead of relying on data dictionary to look it up.
    
    
    
    
    git-svn-id: file:///space/source.marketcetera.org@6714 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/validation/fix/FIXObservables.java
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/validation/fix/FIXObservables.java
@@ -1,13 +1,13 @@
 package org.marketcetera.photon.ui.validation.fix;
 
 
-import java.util.Calendar;
-
 import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
-
 import quickfix.DataDictionary;
+import quickfix.FieldType;
 import quickfix.Message;
+
+import java.util.Calendar;
 
 public class FIXObservables {
 
@@ -15,6 +15,11 @@ public class FIXObservables {
 			int fieldNumber, DataDictionary dict) {
 		return new FIXObservableValue(realm, message, fieldNumber, dict);
 	}
+	public static IObservableValue observeValue(Realm realm, Message message,
+			int fieldNumber, DataDictionary dict, String fieldName, FieldType fieldType) {
+		return new FIXObservableValue(realm, message, fieldNumber, dict, fieldName, fieldType);
+	}
+
 	public static IObservableValue observePriceValue(Realm realm, Message message,
 			int fieldNumber, DataDictionary dict) {
 		return new PriceObservableValue(realm, message, fieldNumber, dict);
