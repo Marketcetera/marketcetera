0a4b23e985d6645b78f0c809c98fd764ba7ec244
Graham Miller <gmiller@marketcetera.com>
2007-07-16 20:05:07 +0000

    Fixed one failing unit test by changing option symbol control back to a "Text" because "Label" didn't support the correct events via the databinding API.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6791 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IOptionOrderTicket.java
+++ source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IOptionOrderTicket.java
@@ -2,13 +2,14 @@ package org.marketcetera.photon.views;
 
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public interface IOptionOrderTicket extends IOrderTicket {
 
 	/**
 	 * @return symbol for the specific option contract (e.g. "MSQ+GE")
 	 */
-	Label getOptionSymbolControl();
+	Text getOptionSymbolControl();
 
 	Combo getExpireMonthCombo();
 
