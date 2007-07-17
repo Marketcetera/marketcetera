dcf6388bfe0d1070509e1eaf5d652196015edf90
Graham Miller <gmiller@marketcetera.com>
2007-07-17 00:04:26 +0000

    Changed format of expiration month/year in OptionMessageListTableFormat.java
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@6796 501a4a96-914b-0410-8454-f5988a4ca596

--- source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/OptionMessageListTableFormat.java
+++ source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/OptionMessageListTableFormat.java
@@ -2,8 +2,12 @@ package org.marketcetera.photon.ui;
 
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IWorkbenchPartSite;
+import org.marketcetera.core.Pair;
 import org.marketcetera.photon.marketdata.OptionInfoComponent;
+import org.marketcetera.photon.marketdata.OptionMarketDataUtils;
 import org.marketcetera.photon.marketdata.OptionMessageHolder;
+import org.marketcetera.photon.views.OptionDateHelper;
+import org.marketcetera.photon.views.OptionMarketDataView;
 import org.marketcetera.photon.views.OptionMessagesComposite;
 import org.marketcetera.photon.views.OptionMessagesComposite.OptionDataColumns;
 
@@ -14,8 +18,11 @@ import quickfix.FieldMap;
 
 public class OptionMessageListTableFormat extends MessageListTableFormatBase<OptionMessageHolder> {
 
+	private OptionDateHelper helper;
+
 	public OptionMessageListTableFormat(Table table, Enum[] columns, IWorkbenchPartSite site, DataDictionary dataDictionary) {
 		super(table, columns, site, dataDictionary);
+		helper = new OptionDateHelper();
 	}
 
 	@Override
@@ -29,6 +36,15 @@ public class OptionMessageListTableFormat extends MessageListTableFormatBase<Opt
 	public String getColumnText(Object element, int index) {
 		if (index == 0) {
 			return ""; //$NON-NLS-1$
+		}
+		if (index == OptionMessagesComposite.EXP_DATE_INDEX){
+			try{
+				Pair<Integer, Integer> pair = OptionMarketDataUtils.getMaturityMonthYear(((OptionMessageHolder) element).get(OptionInfoComponent.STRIKE_INFO));
+				Integer year = pair.getSecondMember();
+				Integer month = pair.getFirstMember();
+				return helper.getMonthAbbreviation(month)+(helper.formatYear(year).substring(2));
+			} catch (Exception ex){
+			}
 		}
 		return super.getColumnText(element, index);
 	}
