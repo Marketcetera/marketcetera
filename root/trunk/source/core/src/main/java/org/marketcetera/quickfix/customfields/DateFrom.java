385c4d4646b65066b006a2780787fa2dd1e4421e
Toli Kuznets <toli@marketcetera.com>
2007-08-16 02:36:21 +0000

    Added custom FIX44-marketcetera.xml dictionary file with new DateTo and DateFrom fields adeed custom DateTo and DateFrom messages, and a class with custom constants that appear in the XML file
    git-svn-id: file:///space/source.marketcetera.org@1497 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/core/src/main/java/org/marketcetera/quickfix/customfields/DateFrom.java
@@ -0,0 +1,24 @@
+package org.marketcetera.quickfix.customfields;
+
+import org.marketcetera.core.ClassVersion;
+import quickfix.UtcTimeStampField;
+
+import java.util.Date;
+
+/**
+ * Implementation of a custom field 9883: DateFrom
+ * @author toli
+ * @version $Id$
+ */
+
+@ClassVersion("$Id$")
+public class DateFrom extends UtcTimeStampField {
+    public static final int FIELD = 9883;
+    public DateFrom() {
+        super(FIELD, new Date());
+    }
+
+    protected DateFrom(Date date) {
+        super(FIELD, date);
+    }
+}
