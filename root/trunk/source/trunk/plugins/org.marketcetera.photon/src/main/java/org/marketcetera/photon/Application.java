06ad84eddd77e78e81aaa720eaff7b78e5823430
Graham Miller <gmiller@marketcetera.com>
2007-09-12 02:01:30 +0000

    IPlatformRunnable is deprecated, and the data-binding realm stuff is taken care of by the platform now.
    git-svn-id: file:///space/source.marketcetera.org@6897 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/Application.java
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/Application.java
@@ -1,8 +1,8 @@
 package org.marketcetera.photon;
 
-import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.core.runtime.IPlatformRunnable;
-import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.widgets.Display;
@@ -19,49 +19,42 @@ import org.marketcetera.photon.preferences.PhotonPage;
  *
  */
 @ClassVersion("$Id$")
-public class Application implements IPlatformRunnable, IPropertyChangeListener {
+public class Application implements IApplication, IPropertyChangeListener {
 
 
-	
-	/**
-	 * This method is called by the Eclipse RCP, and therefore is the main 
-	 * entry point to the Application.  This method initializes the FIX version
-	 * for the application (to FIX 4.2), the FIXMessageHistory, the JMSConnector
-	 * the OrderManager.  Finally it creates the display, and creates and runs
-	 * the workbench.
-	 * 
-	 * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
-	 * @see PlatformUI#createDisplay()
-	 * @see PlatformUI#createAndRunWorkbench(Display, org.eclipse.ui.application.WorkbenchAdvisor)
-	 */
-	public Object run(Object args) throws Exception {
-		PhotonPlugin.getDefault().getPreferenceStore().addPropertyChangeListener(this);
-
-		final Display display = PlatformUI.createDisplay();
-		try {
-			final int [] returnCode = new int[1];
-			Realm.runWithDefault(SWTObservables.getRealm(display), new Runnable() {
-				public void run() {
-					returnCode[0] = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-				}
-			});
-
-			if (returnCode[0] == PlatformUI.RETURN_RESTART) {
-				return IPlatformRunnable.EXIT_RESTART;
-			}
-			return IPlatformRunnable.EXIT_OK;
-		} finally {
-			display.dispose();
-		}
-
-	}
-
-	
 
 	public void propertyChange(PropertyChangeEvent event) {
 		if (event.getProperty().equals(PhotonPage.LOG_LEVEL_KEY)){
 			PhotonPlugin.getDefault().changeLogLevel(""+event.getNewValue());
 		}
+	}
+
+
+	/**
+	 * @see IApplication#start(IApplicationContext)
+ 	 * @see PlatformUI#createDisplay()
+	 * @see PlatformUI#createAndRunWorkbench(Display, org.eclipse.ui.application.WorkbenchAdvisor)
+	 */
+	public Object start(IApplicationContext context) throws Exception {
+		PhotonPlugin.getDefault().getPreferenceStore().addPropertyChangeListener(this);
+
+		Display display = PlatformUI.createDisplay();
+		try {
+			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
+			if (returnCode == PlatformUI.RETURN_RESTART) {
+				return IApplication.EXIT_RESTART;
+			}
+			return IApplication.EXIT_OK;
+		} finally {
+			display.dispose();
+		}
+	}
+
+
+
+	public void stop() {
+		// Do nothing
+		
 	}
 
 }
