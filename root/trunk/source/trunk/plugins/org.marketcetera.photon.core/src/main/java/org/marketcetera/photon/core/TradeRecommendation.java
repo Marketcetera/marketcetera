eb45b10fcc374adb9bff98c4c6a0bd53bb965adf
Graham Miller <gmiller@marketcetera.com>
2007-11-05 22:08:17 +0000

    TradeRecommendation model object.
    git-svn-id: file:///space/source.marketcetera.org@6964 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/trunk/plugins/org.marketcetera.photon.core/src/main/java/org/marketcetera/photon/core/TradeRecommendation.java
@@ -0,0 +1,50 @@
+package org.marketcetera.photon.core;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Date;
+
+import quickfix.Message;
+
+public class TradeRecommendation extends OutgoingMessageHolder {
+
+	private Date createdAt;
+	private Double score;
+	
+	public TradeRecommendation(Message message, Double score) {
+		super(message);
+		createdAt = new Date();
+		this.score = score;
+	}
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public Date getCreationDate(){
+		return createdAt;
+	}
+	
+	public String getStringRepresentation(){
+		return toString();
+	}
+	
+	@Override
+	public String toString() {
+		return "Trade: " + getMessage();
+	}
+	
+	private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+	public void addPropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		changeSupport.addPropertyChangeListener(propertyName, listener);
+	}
+	public void removePropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		changeSupport.removePropertyChangeListener(propertyName, listener);
+	}
+
+}
