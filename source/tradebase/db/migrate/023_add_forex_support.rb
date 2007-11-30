8144701b587d6ff502659a202134d15235fb4a44
Graham Miller <gmiller@marketcetera.com>
2007-11-30 23:18:35 +0000

    beginning of forex support in tradebase
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@8922 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ source/tradebase/db/migrate/023_add_forex_support.rb
@@ -0,0 +1,22 @@
+require "migrate_plus"
+
+class AddForexSupport < ActiveRecord::Migration
+  include MigratePlus
+
+  def self.up
+    add_column :trades, :type, :string
+    add_column :trades, :strategy, :string, :limit=>255
+
+    usd = Currency.find_by_alpha_code("USD")
+    add_column :marks, :currency_id, :integer, {:default=>usd.id, :null =>false} 
+
+    add_fkey :marks, :currency_id, :currencies
+  end
+
+  def self.down
+    remove_fkey :marks, :currency_id, :currencies
+    remove_column :trades, :type
+    remove_column :trades, :strategy
+    remove_column :marks, :currency_id
+  end
+end
