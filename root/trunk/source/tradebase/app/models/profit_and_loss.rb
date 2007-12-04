ba386e340028410e8628cd18eef7f12703be88ff
Graham Miller <gmiller@marketcetera.com>
2007-12-04 01:44:17 +0000

    fixed test_aggregate_across_all_accts
    git-svn-id: file:///space/source.marketcetera.org@8931 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/app/models/profit_and_loss.rb
+++ root/trunk/source/tradebase/app/models/profit_and_loss.rb
@@ -37,7 +37,6 @@ class ProfitAndLoss
     '   ) AS PositionTable ON PositionTable.tradeable_id = marks.tradeable_id AND mark_date = position_date AND mark_type="C" AND tradeable_type="Equity" '
     
   @missing_equity_marks_query = 
-    '/* phantom cash flows for start date */ '+
     'SELECT PositionTable.tradeable_id, mark_value, position_date as mark_date, null as mark_type, null as created_on, null as updated_on, "Equity" as tradeable_type '+
     'FROM marks RIGHT JOIN '+
     '( '+
@@ -105,6 +104,18 @@ class ProfitAndLoss
     ') AS PnL '+
     'GROUP BY PnL.account_id, PnL.currency_id, PnL.strategy ';
 
+  @missing_forex_marks_query = 
+    'SELECT PositionTable.tradeable_id, mark_value, position_date as mark_date, null as mark_type, null as created_on, null as updated_on, "CurrencyPair" as tradeable_type '+
+    'FROM marks RIGHT JOIN '+
+    '( '+
+    ' SELECT sum(position_qty) AS position, tradeable_id, account_id, strategy, ? as position_date '+
+    ' FROM trades JOIN journals ON journals.id=trades.journal_id '+
+    ' WHERE journals.post_date < ? AND tradeable_type = "CurrencyPair" '+
+    ' GROUP BY tradeable_id, account_id, strategy '+
+    ' HAVING position != 0 '+
+    ') AS PositionTable ON marks.tradeable_id = PositionTable.tradeable_id AND mark_date = position_date AND mark_type="C" AND tradeable_type="CurrencyPair" '+
+    'WHERE mark_value IS NULL '
+
   # Profitandloss is a BigDecimal, account and symbols are strings
   def initialize(profit_and_loss, currency_id, account_id, strategy, tradeable_id = nil)
     @profit_and_loss = profit_and_loss
@@ -160,8 +171,14 @@ class ProfitAndLoss
   end 
 
   def ProfitAndLoss.get_missing_equity_marks(the_date)
-      sane = ActiveRecord::Base.sanitize_sql_accessor([@missing_equity_marks_query, the_date, the_date])
-      
-      Mark.find_by_sql(sane)
+    sane = ActiveRecord::Base.sanitize_sql_accessor([@missing_equity_marks_query, the_date, the_date])
+    
+    Mark.find_by_sql(sane)
+  end
+  
+  def ProfitAndLoss.get_missing_forex_marks(the_date)
+    sane = ActiveRecord::Base.sanitize_sql_accessor([@missing_forex_marks_query, the_date, the_date])
+    
+    ForexMark.find_by_sql(sane)
   end
 end
