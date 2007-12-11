41151c3142fee4d9fe0fc2462a563adde02ab3fb
Graham Miller <gmiller@marketcetera.com>
2007-12-11 22:30:47 +0000

    moved message history code into core
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@1626 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ source/core/src/main/java/org/marketcetera/messagehistory/MessageHolder.java
@@ -0,0 +1,44 @@
+package org.marketcetera.messagehistory;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.marketcetera.core.ClassVersion;
+
+import quickfix.Message;
+
+@ClassVersion("$Id$")
+public class MessageHolder implements Comparable<MessageHolder> {
+	private Message message;
+	private long messageReference;
+	private static AtomicLong counter = new AtomicLong();
+	private String groupID = null;
+	
+	public MessageHolder(Message message) {
+		this.message = message;
+		this.messageReference = counter.incrementAndGet();
+	}
+
+	public MessageHolder(Message message, String groupID){
+		this(message);
+		this.groupID = groupID;
+	}
+	
+	public Message getMessage() {
+		return message;
+	}
+	
+	public long getMessageReference()
+	{
+		return messageReference;
+	}
+
+	public int compareTo(MessageHolder mh) {  
+		return (int)(messageReference - mh.messageReference);
+	}
+
+	public String getGroupID() {
+		return groupID;
+	}
+	
+	
+}
