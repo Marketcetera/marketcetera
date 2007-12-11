bd3efdbdd858d7dfc0a607c4f5221ed28a179ac1
Graham Miller <gmiller@marketcetera.com>
2007-12-11 22:30:47 +0000

    moved message history code into core
    git-svn-id: file:///space/source.marketcetera.org@1626 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/core/src/test/java/org/marketcetera/messagehistory/MessageHolderTest.java
@@ -0,0 +1,41 @@
+package org.marketcetera.messagehistory;
+
+import java.math.BigDecimal;
+
+import junit.framework.TestCase;
+
+import org.marketcetera.core.MSymbol;
+import org.marketcetera.messagehistory.IncomingMessageHolder;
+import org.marketcetera.messagehistory.MessageHolder;
+import org.marketcetera.messagehistory.OutgoingMessageHolder;
+import org.marketcetera.quickfix.FIXMessageFactory;
+import org.marketcetera.quickfix.FIXVersion;
+
+import quickfix.Message;
+import quickfix.field.Side;
+
+public class MessageHolderTest extends TestCase 
+{
+    private FIXMessageFactory msgFactory = FIXVersion.FIX42.getMessageFactory();
+	public void testIncomingMessageHolder()
+	{
+		Message message = msgFactory.newLimitOrder("asdf", Side.BUY, BigDecimal.TEN, new MSymbol("123"), BigDecimal.ONE, '\0', "asd");
+		IncomingMessageHolder holder = new IncomingMessageHolder(message);
+		assertEquals(message, holder.getMessage());
+	}
+
+	public void testOutgoingMessageHolder()
+	{
+		Message message = msgFactory.newLimitOrder("asdf", Side.BUY, BigDecimal.TEN, new MSymbol("123"), BigDecimal.ONE, '\0', "asd");
+		OutgoingMessageHolder holder = new OutgoingMessageHolder(message);
+		assertEquals(message, holder.getMessage());
+	}
+
+	public void testMessageHolder()
+	{
+		Message message = msgFactory.newLimitOrder("asdf", Side.BUY, BigDecimal.TEN, new MSymbol("123"), BigDecimal.ONE, '\0', "asd");
+		MessageHolder holder = new MessageHolder(message);
+		assertEquals(message, holder.getMessage());
+	}
+
+}
