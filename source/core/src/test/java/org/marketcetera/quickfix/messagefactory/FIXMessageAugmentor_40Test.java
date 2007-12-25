98be8f1a41edf439bf46ed28ed4dd8b42c3744d1
Toli Kuznets <toli@marketcetera.com>
2007-12-25 01:28:12 +0000

    fix for #449 - added handling of LOC and MOC to cancel-replace messages going out. not sure we need to handle plain cancels as there's no !MarketOnClose or !LimitOnClose infomration there.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@1646 501a4a96-914b-0410-8454-f5988a4ca596

--- source/core/src/test/java/org/marketcetera/quickfix/messagefactory/FIXMessageAugmentor_40Test.java
+++ source/core/src/test/java/org/marketcetera/quickfix/messagefactory/FIXMessageAugmentor_40Test.java
@@ -1,9 +1,9 @@
 package org.marketcetera.quickfix.messagefactory;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
 import org.marketcetera.core.ClassVersion;
-import org.marketcetera.core.MarketceteraTestSuite;
+import org.marketcetera.core.FIXVersionTestSuite;
+import org.marketcetera.core.FIXVersionedTestCase;
 import org.marketcetera.quickfix.FIXMessageFactory;
 import org.marketcetera.quickfix.FIXMessageUtilTest;
 import org.marketcetera.quickfix.FIXVersion;
@@ -18,13 +18,14 @@ import quickfix.field.TimeInForce;
  */
 
 @ClassVersion("$Id$")
-public class FIXMessageAugmentor_40Test extends TestCase {
-    public FIXMessageAugmentor_40Test(String inName) {
-        super(inName);
+public class FIXMessageAugmentor_40Test extends FIXVersionedTestCase {
+    public FIXMessageAugmentor_40Test(String inName, FIXVersion version) {
+        super(inName, version);
     }
 
     public static Test suite() {
-        return new MarketceteraTestSuite(FIXMessageAugmentor_40Test.class);
+        // we dont' really want to setup multi-versioned tests, but it's the easiest way to create the factories
+        return new FIXVersionTestSuite(FIXMessageAugmentor_40Test.class, new FIXVersion[] {FIXVersion.FIX40});
     }
 
     public void testCountTT_applicableTypes() throws Exception {
@@ -57,7 +58,56 @@ public class FIXMessageAugmentor_40Test extends TestCase {
         assertEquals(TimeInForce.DAY, buy.getChar(TimeInForce.FIELD));
     }
 
+    // same but for cancel/replace orders
+    public void testMarketOnClose_cxr() throws Exception
+    {
+        FIXMessageFactory factory = FIXVersion.FIX40.getMessageFactory();
+
+        Message buy = FIXMessageUtilTest.createMarketNOS("TOLI", 123, Side.BUY, factory);
+        Message cancelReplace = factory.newCancelReplaceFromMessage(buy);
+
+        assertEquals(OrdType.MARKET, cancelReplace.getChar(OrdType.FIELD));
+        cancelReplace.setField(new TimeInForce(TimeInForce.AT_THE_CLOSE));
+
+        FIXMessageAugmentor_40 augmentor = new FIXMessageAugmentor_40();
+        cancelReplace = augmentor.cancelReplaceRequestAugment(cancelReplace);
+
+        assertEquals(OrdType.MARKET_ON_CLOSE, cancelReplace.getChar(OrdType.FIELD));
+        assertEquals(TimeInForce.DAY, cancelReplace.getChar(TimeInForce.FIELD));
+
+        // now send a non-MoC order make sure no changes are made
+        buy = FIXMessageUtilTest.createMarketNOS("TOLI", 213, Side.BUY, factory);
+        buy.setField(new TimeInForce(TimeInForce.DAY));
+        cancelReplace = factory.newCancelReplaceFromMessage(buy);
+        buy = augmentor.cancelReplaceRequestAugment(cancelReplace);
+        assertEquals(OrdType.MARKET, cancelReplace.getChar(OrdType.FIELD));
+        assertEquals(TimeInForce.DAY, cancelReplace.getChar(TimeInForce.FIELD));
+    }
+
     public void testLimitOnClose() throws Exception
+    {
+        FIXMessageFactory factory = FIXVersion.FIX40.getMessageFactory();
+        Message buy = FIXMessageUtilTest.createNOS("TOLI", 123, 100, Side.BUY, factory);
+        assertEquals(OrdType.LIMIT, buy.getChar(OrdType.FIELD));
+        buy.setField(new TimeInForce(TimeInForce.AT_THE_CLOSE));
+        Message cancelReplace = factory.newCancelReplaceFromMessage(buy);
+
+        FIXMessageAugmentor_40 augmentor = new FIXMessageAugmentor_40();
+        cancelReplace = augmentor.cancelReplaceRequestAugment(buy);
+
+        assertEquals(OrdType.LIMIT_ON_CLOSE, cancelReplace.getChar(OrdType.FIELD));
+        assertEquals(TimeInForce.DAY, cancelReplace.getChar(TimeInForce.FIELD));
+
+        // now send a non-LoC order make sure no changes are made
+        buy = FIXMessageUtilTest.createNOS("TOLI", 213, 100, Side.BUY, factory);
+        buy.setField(new TimeInForce(TimeInForce.DAY));
+        cancelReplace = factory.newCancelReplaceFromMessage(buy);
+        cancelReplace = augmentor.newOrderSingleAugment(cancelReplace);
+        assertEquals(OrdType.LIMIT, cancelReplace.getChar(OrdType.FIELD));
+        assertEquals(TimeInForce.DAY, cancelReplace.getChar(TimeInForce.FIELD));
+    }
+
+    public void testLimitOnClose_cxr() throws Exception
     {
         FIXMessageFactory factory = FIXVersion.FIX40.getMessageFactory();
         Message buy = FIXMessageUtilTest.createNOS("TOLI", 123, 100, Side.BUY, factory);
