2a726a6d104e204f5ffa730711329b88a5058a28
Toli Lerios <tlerios@marketcetera.com>
2008-05-01 18:18:57 +0000

    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7182 501a4a96-914b-0410-8454-f5988a4ca596

--- source/branches/0.4-options/plugins/org.marketcetera.photon.tests/lib/test_script.rb
+++
@@ -1,5 +0,0 @@
-class TestScript
-  def on_message(message)
-    $quote_count = $quote_count + 1
-  end
-end
--- source/branches/0.4-options/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IMSymbolListener.java
+++
@@ -1,7 +0,0 @@
-package org.marketcetera.photon.views;
-
-import org.marketcetera.core.MSymbol;
-
-public interface IMSymbolListener {
-	public void onAssertSymbol(MSymbol symbol);
-}
