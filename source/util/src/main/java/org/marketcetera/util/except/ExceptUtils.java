232cd066e4483ea89970118a1c1b839e88ee1b23
Toli Lerios <tlerios@marketcetera.com>
2008-05-14 07:15:34 +0000

    Added support for InterruptedException wrappers
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7223 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/main/java/org/marketcetera/util/except/ExceptUtils.java
+++ source/util/src/main/java/org/marketcetera/util/except/ExceptUtils.java
@@ -21,9 +21,9 @@ public final class ExceptUtils
 
     /**
      * Checks whether the calling thread has been interrupted, and, if
-     * so, throws an exception built using the {@link
-     * InterruptedException#InterruptedException()} constructor. The
-     * interrupted status of the thread is cleared.
+     * so, throws an {@link InterruptedException} with the default
+     * interruption message and no underlying cause. The interrupted
+     * status of the thread is cleared.
      *
      * @throws InterruptedException Thrown if the calling thread
      * was interrupted.
@@ -33,15 +33,16 @@ public final class ExceptUtils
         throws InterruptedException
     {
         if (Thread.interrupted()) {
-            throw new InterruptedException();
+            throw new InterruptedException
+                (Messages.PROVIDER.getText(Messages.THREAD_INTERRUPTED));
         }
     }
 
     /**
      * Checks whether the calling thread has been interrupted, and, if
-     * so, throws an exception built using the {@link
-     * InterruptedException#InterruptedException(String)}
-     * constructor. The interrupted status of the thread is cleared.
+     * so, throws an {@link InterruptedException} with the given
+     * interruption message, but without an underlying cause. The
+     * interrupted status of the thread is cleared.
      *
      * @param message The message.
      *
@@ -60,9 +61,9 @@ public final class ExceptUtils
 
     /**
      * Checks whether the calling thread has been interrupted, and, if
-     * so, throws an exception built using the {@link
-     * InterruptedException#InterruptedException()} constructor. The
-     * given underlying cause is set on the thrown exception. The
+     * so, throws an {@link InterruptedException} with the default
+     * interruption message and the given underlying cause. The given
+     * underlying cause is set on the thrown exception. The
      * interrupted status of the thread is cleared.
      *
      * @param cause The cause.
@@ -76,7 +77,8 @@ public final class ExceptUtils
         throws InterruptedException
     {
         if (Thread.interrupted()) {
-            InterruptedException ex=new InterruptedException();
+            InterruptedException ex=new InterruptedException
+                (Messages.PROVIDER.getText(Messages.THREAD_INTERRUPTED));
             ex.initCause(cause);
             throw ex;
         }
@@ -84,10 +86,10 @@ public final class ExceptUtils
 
     /**
      * Checks whether the calling thread has been interrupted, and, if
-     * so, throws an exception built using the {@link
-     * InterruptedException#InterruptedException(String)}
-     * constructor. The given underlying cause is set on the thrown
-     * exception. The interrupted status of the thread is cleared.
+     * so, throws an {@link InterruptedException} with the given
+     * interruption message and the given underlying cause. The given
+     * underlying cause is set on the thrown exception. The
+     * interrupted status of the thread is cleared.
      *
      * @param cause The cause.
      * @param message The message.
@@ -112,17 +114,16 @@ public final class ExceptUtils
      * Swallows the given throwable. It logs the given parameterized
      * message and throwable under the given logging category at the
      * warning level via the given logger proxy. Also, if the given
-     * throwable is an instance of {@link InterruptedException}, then
-     * the calling thread is interrupted.
+     * throwable is an instance of {@link InterruptedException},
+     * {@link I18NInterruptedException}, or {@link
+     * I18NInterruptedRuntimeException}, then the calling thread is
+     * interrupted.
      * 
      * @param throwable The throwable.
      * @param logger The logger proxy.
      * @param category The category.
      * @param message The message.
      * @param params The message parameters.
-     *
-     * @throws InterruptedException Thrown if the calling thread
-     * was interrupted.
      */
 
     public static void swallow
@@ -133,7 +134,9 @@ public final class ExceptUtils
          Object... params)
     {
         logger.warn(category,throwable,message,params);
-        if (throwable instanceof InterruptedException) {
+        if ((throwable instanceof InterruptedException) ||
+            (throwable instanceof I18NInterruptedException) ||
+            (throwable instanceof I18NInterruptedRuntimeException)) {
             Thread.currentThread().interrupt();
         }
     }
@@ -142,12 +145,11 @@ public final class ExceptUtils
      * Swallows the given throwable. It logs a standard message
      * alongside the throwable at the warning level. Also, if the
      * given throwable is an instance of {@link InterruptedException},
-     * then the calling thread is interrupted.
+     * {@link I18NInterruptedException}, or {@link
+     * I18NInterruptedRuntimeException}, then the calling thread is
+     * interrupted.
      * 
      * @param throwable The throwable.
-     *
-     * @throws InterruptedException Thrown if the calling thread
-     * was interrupted.
      */
 
     public static void swallow
