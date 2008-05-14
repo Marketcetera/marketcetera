830fceb3f56f14986531e9dc225a25b98d08057b
Toli Lerios <tlerios@marketcetera.com>
2008-05-14 09:25:19 +0000

    Augmented expected exception tests to detect unthrown exceptions
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7227 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util-test/src/test/java/org/marketcetera/util/test/CollectionAssertTest.java
+++ source/util-test/src/test/java/org/marketcetera/util/test/CollectionAssertTest.java
@@ -29,11 +29,12 @@ public class CollectionAssertTest
     {
         try {
             assertArrayPermutation(null,new String[0]);
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("expected array is null but actual is not",ex.getMessage());
+            return;
         }
+        fail();
     }
 
     @Test
@@ -41,11 +42,12 @@ public class CollectionAssertTest
     {
         try {
             assertArrayPermutation(new String[0],null);
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("actual array is null but expected is not",ex.getMessage());
+            return;
         }
+        fail();
     }
 
     @Test
@@ -54,11 +56,12 @@ public class CollectionAssertTest
         try {
             assertArrayPermutation
                 (new Integer[] {null},new Integer[] {2});
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("actual is missing 'null'",ex.getMessage());
+            return;
         }
+        fail();
     }
 
     @Test
@@ -67,11 +70,12 @@ public class CollectionAssertTest
         try {
             assertArrayPermutation
                 (new Integer[] {1},new Integer[] {1,1});
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("actual contains extra elements such as 1",ex.getMessage());
+            return;
         }
+        fail();
     }
 
     @Test
@@ -80,11 +84,12 @@ public class CollectionAssertTest
         try {
             assertArrayPermutation
                 (new Integer[] {null,null},new Integer[] {null});
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("actual is missing 'null'",ex.getMessage());
+            return;
         }
+        fail();
     }
 
     @Test
@@ -93,10 +98,11 @@ public class CollectionAssertTest
         try {
             assertArrayPermutation
                 ("Right now,",new Integer[] {1},new Integer[] {2});
-            fail();
         } catch (AssertionError ex) {
             assertEquals
                 ("Right now, actual is missing '1'",ex.getMessage());
+            return;
         }
+        fail();
     }
 }
