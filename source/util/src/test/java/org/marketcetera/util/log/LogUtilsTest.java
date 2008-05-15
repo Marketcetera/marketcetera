91e344532bebdf6c5e862cfe4c3f7383af018944
Toli Lerios <tlerios@marketcetera.com>
2008-05-15 11:28:20 +0000

    Simplified signature of exceptions
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7276 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/test/java/org/marketcetera/util/log/LogUtilsTest.java
+++ source/util/src/test/java/org/marketcetera/util/log/LogUtilsTest.java
@@ -19,20 +19,31 @@ public class LogUtilsTest
     @Test
     public void simpleMessage()
     {
+        I18NMessageProvider provider=new I18NMessageProvider("nonexistent_prv");
+        assertEquals
+            ("provider 'nonexistent_prv'; id 'log'; entry 'msg'; "+
+             "parameters ()",
+             LogUtils.getSimpleMessage(provider,TestMessages.LOG_MSG));
+        assertEquals
+            ("provider 'nonexistent_prv'; id 'log'; entry 'msg'; "+
+             "parameters ([null])",
+             LogUtils.getSimpleMessage(provider,TestMessages.LOG_MSG,null));
+        assertEquals
+            ("provider 'nonexistent_prv'; id 'log'; entry 'msg'; "+
+             "parameters ('a',[null])",
+             LogUtils.getSimpleMessage(provider,TestMessages.LOG_MSG,"a",null));
+
         assertEquals
             ("provider 'log_test'; id 'log'; entry 'msg'; "+
              "parameters ()",
-             LogUtils.getSimpleMessage
-             (TestMessages.PROVIDER,TestMessages.LOG_MSG));
+             LogUtils.getSimpleMessage(TestMessages.LOG_MSG));
         assertEquals
             ("provider 'log_test'; id 'log'; entry 'msg'; "+
              "parameters ([null])",
-             LogUtils.getSimpleMessage
-             (TestMessages.PROVIDER,TestMessages.LOG_MSG,null));
+             LogUtils.getSimpleMessage(TestMessages.LOG_MSG,null));
         assertEquals
             ("provider 'log_test'; id 'log'; entry 'msg'; "+
              "parameters ('a',[null])",
-             LogUtils.getSimpleMessage
-             (TestMessages.PROVIDER,TestMessages.LOG_MSG,"a",null));
+             LogUtils.getSimpleMessage(TestMessages.LOG_MSG,"a",null));
     }
 }
