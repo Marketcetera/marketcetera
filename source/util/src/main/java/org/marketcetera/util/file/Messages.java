ee651cdcbae6349da304362dd3f38531d036ab8b
Toli Lerios <tlerios@marketcetera.com>
2008-05-18 07:26:42 +0000

    Added bound messages
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@7300 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/main/java/org/marketcetera/util/file/Messages.java
+++ source/util/src/main/java/org/marketcetera/util/file/Messages.java
@@ -2,7 +2,9 @@ package org.marketcetera.util.file;
 
 import org.marketcetera.core.ClassVersion;
 import org.marketcetera.util.log.I18NLoggerProxy;
-import org.marketcetera.util.log.I18NMessage;
+import org.marketcetera.util.log.I18NMessage0P;
+import org.marketcetera.util.log.I18NMessage1P;
+import org.marketcetera.util.log.I18NMessage2P;
 import org.marketcetera.util.log.I18NMessageProvider;
 
 /**
@@ -36,24 +38,24 @@ public interface Messages
      * The messages.
      */
 
-    static final I18NMessage CLOSING_FAILED=
-        new I18NMessage(LOGGER,"closing_failed");
-    static final I18NMessage CANNOT_GET_TYPE=
-        new I18NMessage(LOGGER,"cannot_get_type");
-    static final I18NMessage CANNOT_DELETE=
-        new I18NMessage(LOGGER,"cannot_delete");
-    static final I18NMessage CANNOT_COPY_FILES=
-        new I18NMessage(LOGGER,"cannot_copy_files");
-    static final I18NMessage CANNOT_COPY_ISTREAM=
-        new I18NMessage(LOGGER,"cannot_copy_istream");
-    static final I18NMessage CANNOT_COPY_READER=
-        new I18NMessage(LOGGER,"cannot_copy_reader");
-    static final I18NMessage CANNOT_COPY_OSTREAM=
-        new I18NMessage(LOGGER,"cannot_copy_ostream");
-    static final I18NMessage CANNOT_COPY_WRITER=
-        new I18NMessage(LOGGER,"cannot_copy_writer");
-    static final I18NMessage CANNOT_COPY_MEMORY_SRC=
-        new I18NMessage(LOGGER,"cannot_copy_memory_src");
-    static final I18NMessage CANNOT_COPY_MEMORY_DST=
-        new I18NMessage(LOGGER,"cannot_copy_memory_dst");
+    static final I18NMessage0P CLOSING_FAILED=
+        new I18NMessage0P(LOGGER,"closing_failed");
+    static final I18NMessage1P CANNOT_GET_TYPE=
+        new I18NMessage1P(LOGGER,"cannot_get_type");
+    static final I18NMessage1P CANNOT_DELETE=
+        new I18NMessage1P(LOGGER,"cannot_delete");
+    static final I18NMessage2P CANNOT_COPY_FILES=
+        new I18NMessage2P(LOGGER,"cannot_copy_files");
+    static final I18NMessage1P CANNOT_COPY_ISTREAM=
+        new I18NMessage1P(LOGGER,"cannot_copy_istream");
+    static final I18NMessage1P CANNOT_COPY_READER=
+        new I18NMessage1P(LOGGER,"cannot_copy_reader");
+    static final I18NMessage1P CANNOT_COPY_OSTREAM=
+        new I18NMessage1P(LOGGER,"cannot_copy_ostream");
+    static final I18NMessage1P CANNOT_COPY_WRITER=
+        new I18NMessage1P(LOGGER,"cannot_copy_writer");
+    static final I18NMessage1P CANNOT_COPY_MEMORY_SRC=
+        new I18NMessage1P(LOGGER,"cannot_copy_memory_src");
+    static final I18NMessage1P CANNOT_COPY_MEMORY_DST=
+        new I18NMessage1P(LOGGER,"cannot_copy_memory_dst");
 }
