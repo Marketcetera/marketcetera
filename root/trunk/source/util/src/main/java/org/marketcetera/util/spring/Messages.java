4b14db099bd59cf205dcc03bdd9d1c0aedc43f12
Toli Lerios <tlerios@marketcetera.com>
2008-05-18 09:37:48 +0000

    Added bound messages
    git-svn-id: file:///space/source.marketcetera.org@7310 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/util/src/main/java/org/marketcetera/util/spring/Messages.java
+++ root/trunk/source/util/src/main/java/org/marketcetera/util/spring/Messages.java
@@ -2,19 +2,18 @@ package org.marketcetera.util.spring;
 
 import org.marketcetera.core.ClassVersion;
 import org.marketcetera.util.log.I18NLoggerProxy;
-import org.marketcetera.util.log.I18NMessage;
 import org.marketcetera.util.log.I18NMessageProvider;
 
 /**
  * The internationalization constants used by this package.
  *
  * @author tlerios
- * @version $Id: Messages.java 1797 2008-04-24 01:34:08Z tlerios $
+ * @version $Id$
  */
 
 /* $License$ */
 
-@ClassVersion("$Id: Messages.java 1797 2008-04-24 01:34:08Z tlerios $")
+@ClassVersion("$Id$")
 public interface Messages
 {
 
