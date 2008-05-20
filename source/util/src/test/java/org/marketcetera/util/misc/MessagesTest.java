13c4aa5109b657876486342e7ded27553d64b952
Toli Lerios <tlerios@marketcetera.com>
2008-05-20 08:55:10 +0000

    Remove TerminalCredentials
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@9129 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/test/java/org/marketcetera/util/misc/MessagesTest.java
+++ source/util/src/test/java/org/marketcetera/util/misc/MessagesTest.java
@@ -13,26 +13,5 @@ public class MessagesTest
     public void messagesExist()
     {
         Messages.PROVIDER.setLocale(Locale.US);
-        assertEquals
-            ("User: ",
-             Messages.USER_PROMPT.getText());
-        assertEquals
-            ("user name",
-             Messages.USER_DESCRIPTION.getText());
-        assertEquals
-            ("Password: ",
-             Messages.PASSWORD_PROMPT.getText());
-        assertEquals
-            ("password",
-             Messages.PASSWORD_DESCRIPTION.getText());
-        assertEquals
-            ("Parsing of the command line has failed",
-             Messages.PARSING_FAILED.getText());
-        assertEquals
-            ("No user was specified",
-             Messages.NO_USER.getText());
-        assertEquals
-            ("No password was specified",
-             Messages.NO_PASSWORD.getText());
     }
 }
