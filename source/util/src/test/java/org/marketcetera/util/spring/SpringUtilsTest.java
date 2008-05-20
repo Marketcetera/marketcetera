75507d5ee79947c651f072af85617501517723e0
Toli Lerios <tlerios@marketcetera.com>
2008-05-20 11:24:41 +0000

    Added spring property configuration
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@9146 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
+++ source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
@@ -2,6 +2,7 @@ package org.marketcetera.util.spring;
 
 import org.junit.Test;
 import org.marketcetera.util.test.TestCaseBase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 
 import static org.junit.Assert.*;
@@ -11,39 +12,57 @@ public class SpringUtilsTest
 {
     private static final String TEST_NAME_BEAN=
         "testNameBean";
-    private static final String TEST_VALUE_BEAN=
-        "testValueBean";
+    private static final String TEST_VALUE=
+        "testValue";
     private static final String TEST_NAME_PROP=
         "testNameProp";
     private static final String TEST_VALUE_PROP=
         "testValueProp";
-    private static final String TEST_NAME_CONFIGURER=
+    private static final String TEST_VALUE_PROP_OVERRIDE=
+        "testValuePropOverride";
+    private static final String TEST_CONFIGURER_BEAN=
         "testConfigurer";
-    private static final String TEST_NAME_RESOURCE_BEAN=
-        "testResource";
-    private static final String TEST_RESOURCES_FILE=
-        "spring.properties";
+    private static final String TEST_SPRING_FILE=
+        "spring.xml";
+    private static final String TEST_PROPERTIES_FILE_BEAN=
+        "propertiesFile";
+    private static final String TEST_PROPERTIES_FILES_BEAN=
+        "propertiesFiles";
 
 
     @Test
     public void stringBean()
     {
 		GenericApplicationContext context=new GenericApplicationContext();
-        SpringUtils.addStringBean(context,TEST_NAME_BEAN,TEST_VALUE_BEAN);
-        assertEquals(TEST_VALUE_BEAN,context.getBean(TEST_NAME_BEAN));
+        SpringUtils.addStringBean(context,TEST_NAME_BEAN,TEST_VALUE);
+        assertEquals(TEST_VALUE,context.getBean(TEST_NAME_BEAN));
     }
 
     @Test
-    public void propertiesConfigurer()
+    public void propertiesConfigurerString()
     {
-		GenericApplicationContext context=new GenericApplicationContext();
-        SpringUtils.addStringBean
-            (context,TEST_NAME_RESOURCE_BEAN,"classpath:"+TEST_RESOURCES_FILE);
+		GenericApplicationContext context=
+            new GenericApplicationContext
+            (new ClassPathXmlApplicationContext(TEST_SPRING_FILE));
         SpringUtils.addPropertiesConfigurer
-            (context,TEST_NAME_CONFIGURER,TEST_NAME_RESOURCE_BEAN);
+            (context,TEST_CONFIGURER_BEAN,TEST_PROPERTIES_FILE_BEAN);
         SpringUtils.addStringBean
             (context,TEST_NAME_BEAN,"${"+TEST_NAME_PROP+"}");
         context.refresh();
         assertEquals(TEST_VALUE_PROP,context.getBean(TEST_NAME_BEAN));
+    }
+
+    @Test
+    public void propertiesConfigurerList()
+    {
+		GenericApplicationContext context=
+            new GenericApplicationContext
+            (new ClassPathXmlApplicationContext(TEST_SPRING_FILE));
+        SpringUtils.addPropertiesConfigurer
+            (context,TEST_CONFIGURER_BEAN,TEST_PROPERTIES_FILES_BEAN);
+        SpringUtils.addStringBean
+            (context,TEST_NAME_BEAN,"${"+TEST_NAME_PROP+"}");
+        context.refresh();
+        assertEquals(TEST_VALUE_PROP_OVERRIDE,context.getBean(TEST_NAME_BEAN));
     }
 }
