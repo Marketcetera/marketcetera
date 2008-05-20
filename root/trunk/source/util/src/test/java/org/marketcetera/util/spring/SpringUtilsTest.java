a30213cbed7e095790be5ec0cb7115fb609539d9
Toli Lerios <tlerios@marketcetera.com>
2008-05-20 11:07:24 +0000

    Added spring property configuration
    git-svn-id: file:///space/source.marketcetera.org@9142 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
+++ root/trunk/source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
@@ -2,24 +2,48 @@ package org.marketcetera.util.spring;
 
 import org.junit.Test;
 import org.marketcetera.util.test.TestCaseBase;
-import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import static org.junit.Assert.*;
 
 public class SpringUtilsTest
 	extends TestCaseBase
 {
-    private static final String TEST_NAME=
-        "testName";
-    private static final String TEST_VALUE=
-        "testValue";
+    private static final String TEST_NAME_BEAN=
+        "testNameBean";
+    private static final String TEST_VALUE_BEAN=
+        "testValueBean";
+    private static final String TEST_NAME_PROP=
+        "testNameProp";
+    private static final String TEST_VALUE_PROP=
+        "testValueProp";
+    private static final String TEST_NAME_CONFIGURER=
+        "testConfigurer";
+    private static final String TEST_NAME_RESOURCE_BEAN=
+        "testResource";
+    private static final String TEST_RESOURCES_FILE=
+        "spring.properties";
 
 
     @Test
-    public void setValue()
+    public void stringBean()
     {
-		StaticApplicationContext context=new StaticApplicationContext();
-        SpringUtils.addStringBean(context,TEST_NAME,TEST_VALUE);
-        assertEquals(TEST_VALUE,context.getBean(TEST_NAME));
+		GenericApplicationContext context=new GenericApplicationContext();
+        SpringUtils.addStringBean(context,TEST_NAME_BEAN,TEST_VALUE_BEAN);
+        assertEquals(TEST_VALUE_BEAN,context.getBean(TEST_NAME_BEAN));
+    }
+
+    @Test
+    public void propertiesConfigurer()
+    {
+		GenericApplicationContext context=new GenericApplicationContext();
+        SpringUtils.addStringBean
+            (context,TEST_NAME_RESOURCE_BEAN,"classpath:"+TEST_RESOURCES_FILE);
+        SpringUtils.addPropertiesConfigurer
+            (context,TEST_NAME_CONFIGURER,TEST_NAME_RESOURCE_BEAN);
+        SpringUtils.addStringBean
+            (context,TEST_NAME_BEAN,"${"+TEST_NAME_PROP+"}");
+        context.refresh();
+        assertEquals(TEST_VALUE_PROP,context.getBean(TEST_NAME_BEAN));
     }
 }
