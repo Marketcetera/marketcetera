dd9b7fbad10c9c83fab01e3aaa5113d0da73907b
Toli Lerios <tlerios@marketcetera.com>
2008-05-20 11:07:00 +0000

    Added spring property configuration
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@9141 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/main/java/org/marketcetera/util/spring/SpringUtils.java
+++ source/util/src/main/java/org/marketcetera/util/spring/SpringUtils.java
@@ -1,7 +1,10 @@
 package org.marketcetera.util.spring;
 
 import org.marketcetera.core.ClassVersion;
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.GenericApplicationContext;
 
@@ -27,7 +30,7 @@ public final class SpringUtils
      * @param context The context.
      * @param name The bean name.
      * @param value The text value.
-     **/
+     */
 
     public static void addStringBean
         (GenericApplicationContext context,
@@ -35,10 +38,37 @@ public final class SpringUtils
          String value)
     {
         RootBeanDefinition bean=new RootBeanDefinition(String.class);
+        bean.setResourceDescription(SpringUtils.class.getName());
         ConstructorArgumentValues values=new ConstructorArgumentValues();
         values.addGenericArgumentValue(value);
         bean.setConstructorArgumentValues(values);
         context.registerBeanDefinition(name,bean);
+    }
+
+    /**
+     * Creates a preferences configurer bean in the given application
+     * context. The new bean has the given name and refers to another
+     * bean with another given name for the properties' locations.
+     *
+     * @param context The context.
+     * @param configurerName The bean name.
+     * @param locationRef The name of the bean whose value is the
+     * properties' locations.
+     */
+
+    public static void addPropertiesConfigurer
+        (GenericApplicationContext context,
+         String configurerName,
+         String locationRef)
+    {
+        RootBeanDefinition bean=
+            new RootBeanDefinition(PropertyPlaceholderConfigurer.class);
+        bean.setResourceDescription(SpringUtils.class.getName());
+        MutablePropertyValues values=new MutablePropertyValues();
+        values.addPropertyValue
+            ("locations",new RuntimeBeanReference(locationRef));
+        bean.setPropertyValues(values);
+        context.registerBeanDefinition(configurerName,bean);
     }
 
 
