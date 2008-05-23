b9462e72b73714a37b54a6a8421b1dde1dbbee2b
Toli Lerios <tlerios@marketcetera.com>
2008-05-23 08:59:29 +0000

    Moved comments for javadoc
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@9213 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/main/java/org/marketcetera/util/file/SmartLinksDirectoryWalker.java
+++ source/util/src/main/java/org/marketcetera/util/file/SmartLinksDirectoryWalker.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Collection;
 import org.apache.commons.io.DirectoryWalker;
 import org.apache.commons.io.filefilter.IOFileFilter;
+import org.marketcetera.core.ClassVersion;
 
 /**
  * A {@link DirectoryWalker} which provides convenience methods to
