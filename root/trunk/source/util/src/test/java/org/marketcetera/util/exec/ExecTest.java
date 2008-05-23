bf637ab49a607461b0f9397da401225be0479987
Toli Lerios <tlerios@marketcetera.com>
2008-05-23 08:55:12 +0000

    Moved comments for javadoc
    git-svn-id: file:///space/source.marketcetera.org@9208 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/util/src/test/java/org/marketcetera/util/exec/ExecTest.java
+++ root/trunk/source/util/src/test/java/org/marketcetera/util/exec/ExecTest.java
@@ -1,13 +1,5 @@
 package org.marketcetera.util.exec;
 
-/**
- * @author tlerios@marketcetera.com
- * @since 0.5.0
- * @version $Id$
- */
-
-/* $License$ */
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
@@ -26,6 +18,14 @@ import org.marketcetera.util.test.TestCaseBase;
 
 import static org.junit.Assert.*;
 import static org.marketcetera.util.test.RegExAssert.*;
+
+/**
+ * @author tlerios@marketcetera.com
+ * @since 0.5.0
+ * @version $Id$
+ */
+
+/* $License$ */
 
 public class ExecTest
     extends TestCaseBase
