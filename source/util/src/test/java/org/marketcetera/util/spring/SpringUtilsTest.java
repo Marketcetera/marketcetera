a886d33dc0fd0bf1047589ef73c48f735016f459
Toli Lerios <tlerios@marketcetera.com>
2008-05-29 08:31:57 +0000

    Migrated from resources to files for spring configuration files
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@9237 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
+++ source/util/src/test/java/org/marketcetera/util/spring/SpringUtilsTest.java
@@ -1,8 +1,10 @@
 package org.marketcetera.util.spring;
 
+import java.io.File;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.marketcetera.util.test.TestCaseBase;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 
 import static org.junit.Assert.*;
@@ -18,12 +20,16 @@ import static org.junit.Assert.*;
 public class SpringUtilsTest
     extends TestCaseBase
 {
+    private static final String TEST_ROOT=
+        DIR_ROOT+File.separator+"spring"+File.separator;
     private static final String TEST_NAME_BEAN=
         "testNameBean";
     private static final String TEST_VALUE=
         "testValue";
     private static final String TEST_NAME_PROP=
         "testNameProp";
+    private static final String TEST_NAME_PROP_BLANK=
+        "testNamePropBlank";
     private static final String TEST_VALUE_PROP=
         "testValueProp";
     private static final String TEST_VALUE_PROP_OVERRIDE=
@@ -31,7 +37,7 @@ public class SpringUtilsTest
     private static final String TEST_CONFIGURER_BEAN=
         "testConfigurer";
     private static final String TEST_SPRING_FILE=
-        "spring.xml";
+        TEST_ROOT+"spring.xml";
     private static final String TEST_PROPERTIES_FILE_BEAN=
         "propertiesFile";
     private static final String TEST_PROPERTIES_FILES_BEAN=
@@ -47,11 +53,11 @@ public class SpringUtilsTest
     }
 
     @Test
-    public void propertiesConfigurerString()
+    public void propertiesConfigurerStringSet()
     {
         GenericApplicationContext context=
             new GenericApplicationContext
-            (new ClassPathXmlApplicationContext(TEST_SPRING_FILE));
+            (new FileSystemXmlApplicationContext(TEST_SPRING_FILE));
         SpringUtils.addPropertiesConfigurer
             (context,TEST_CONFIGURER_BEAN,TEST_PROPERTIES_FILE_BEAN);
         SpringUtils.addStringBean
@@ -61,11 +67,25 @@ public class SpringUtilsTest
     }
 
     @Test
+    public void propertiesConfigurerStringUnset()
+    {
+        GenericApplicationContext context=
+            new GenericApplicationContext
+            (new FileSystemXmlApplicationContext(TEST_SPRING_FILE));
+        SpringUtils.addPropertiesConfigurer
+            (context,TEST_CONFIGURER_BEAN,TEST_PROPERTIES_FILE_BEAN);
+        SpringUtils.addStringBean
+            (context,TEST_NAME_BEAN,"${"+TEST_NAME_PROP_BLANK+"}");
+        context.refresh();
+        assertEquals(StringUtils.EMPTY,context.getBean(TEST_NAME_BEAN));
+    }
+
+    @Test
     public void propertiesConfigurerList()
     {
         GenericApplicationContext context=
             new GenericApplicationContext
-            (new ClassPathXmlApplicationContext(TEST_SPRING_FILE));
+            (new FileSystemXmlApplicationContext(TEST_SPRING_FILE));
         SpringUtils.addPropertiesConfigurer
             (context,TEST_CONFIGURER_BEAN,TEST_PROPERTIES_FILES_BEAN);
         SpringUtils.addStringBean
