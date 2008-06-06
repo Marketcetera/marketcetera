2f1aa09bcca42a44b8db330a0cb78d2b79ac8e80
Ken Lim <klim@marketcetera.com>
2008-06-06 20:58:56 +0000

    Remove irrelevant tag due to found bugs and fixes.
    git-svn-id: file:///space/source.marketcetera.org/root/tags/releases@9317 501a4a96-914b-0410-8454-f5988a4ca596

--- 0.5.0/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/marketdata/IMarketDataConstants.java
+++
@@ -1,8 +0,0 @@
-package org.marketcetera.photon.marketdata;
-
-public interface IMarketDataConstants {
-
-	String EXTENSION_POINT_ID = "org.marketcetera.photon.marketdatafeed";
-	String FEED_FACTORY_CLASS_ATTRIBUTE = "feedFactoryClass";
-	String CREDENTIALS_FACTORY_CLASS_ATTRIBUTE = "credentialsFactoryClass";
-}
