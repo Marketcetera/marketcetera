f6e1160e6f8f438aec78b88ecad4079a617c9122
Toli Lerios <tlerios@marketcetera.com>
2008-07-23 09:35:47 +0000

    Author: tlerios
    Reviewer: ajaiswal
    
    Added message container (class or properties file) comparator. Initial
    intended use is to simplify unit tests for proper externalization of
    i18n strings. Migrated unit tests of all util packages. Also made
    other minor edits to existing classes in order to support the new
    comparator. Added reflection utility class for class introspection.
    
    
    
    git-svn-id: file:///space/source.marketcetera.org@9402 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/util/src/test/java/org/marketcetera/util/misc/MessagesTest.java
+++ root/trunk/source/util/src/test/java/org/marketcetera/util/misc/MessagesTest.java
@@ -1,9 +1,10 @@
 package org.marketcetera.util.misc;
 
-import java.util.Locale;
 import org.junit.Test;
-import org.marketcetera.util.log.I18NMessageProvider;
+import org.marketcetera.util.l10n.MessageComparator;
 import org.marketcetera.util.test.TestCaseBase;
+
+import static org.junit.Assert.*;
 
 /**
  * @author tlerios@marketcetera.com
@@ -17,8 +18,10 @@ public class MessagesTest
     extends TestCaseBase
 {
     @Test
-    public void messagesExist()
+    public void messagesMatch()
+        throws Exception
     {
-        I18NMessageProvider.setLocale(Locale.US);
+        MessageComparator comparator=new MessageComparator(Messages.class);
+        assertTrue(comparator.getDifferences(),comparator.isMatch());
     }
 }
