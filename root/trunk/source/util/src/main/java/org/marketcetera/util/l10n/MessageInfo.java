15ad53f595d040cdb44ba6b458f5f400e2686a4d
Toli Lerios <tlerios@marketcetera.com>
2008-07-23 09:31:57 +0000

    Author: tlerios
    Reviewer: ajaiswal
    
    Added message container (class or properties file) comparator. Initial
    intended use is to simplify unit tests for proper externalization of
    i18n strings. Migrated unit tests of all util packages. Also made
    other minor edits to existing classes in order to support the new
    comparator. Added reflection utility class for class introspection.
    
    
    
    git-svn-id: file:///space/source.marketcetera.org@9397 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/util/src/main/java/org/marketcetera/util/l10n/MessageInfo.java
@@ -0,0 +1,104 @@
+package org.marketcetera.util.l10n;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.marketcetera.util.misc.ClassVersion;
+
+/**
+ * Holder of meta-information about a generic message.
+ *
+ * @author tlerios@marketcetera.com
+ * @since 0.6.0
+ * @version $Id$
+ */
+
+/* $License$ */
+
+@ClassVersion("$Id$")
+public abstract class MessageInfo
+{
+
+    // CLASS DATA.
+
+    /**
+     * An empty array of class instances.
+     */
+
+    public static final MessageInfo[] EMPTY_ARRAY=
+        new MessageInfo[0];
+
+
+    // INSTANCE DATA.
+
+    private String mKey;
+    private int mParamCount;
+
+
+    // CONSTRUCTORS.
+
+    /**
+     * Creates a new meta-information holder with the given message
+     * key and parameter count.
+     *
+     * @param key The key.
+     * @param paramCount The parameter count; -1 signifies an unknown
+     * count.
+     */
+
+    public MessageInfo
+        (String key,
+         int paramCount)
+    {
+        mKey=key;
+        mParamCount=paramCount;
+    }
+
+
+    // INSTANCE METHODS.
+
+    /**
+     * Returns the receiver's message key.
+     *
+     * @return The key.
+     */
+
+    public String getKey()
+    {
+        return mKey;
+    }
+
+    /**
+     * Returns the receiver's parameter count.
+     *
+     * @return The count; -1 signifies an unknown count.
+     */
+    
+    public int getParamCount()
+    {
+        return mParamCount;
+    }
+
+
+    // Object.
+
+    @Override
+    public int hashCode()
+    {
+        return (ObjectUtils.hashCode(getKey())+
+                getParamCount());
+    }
+
+    @Override
+    public boolean equals
+        (Object other)
+    {
+        if (this==other) {
+            return true;
+        }
+        if (!(other instanceof MessageInfo)) {
+            return false;
+        }
+        MessageInfo o=(MessageInfo)other;
+        return (ObjectUtils.equals(getKey(),o.getKey()) &&
+                (getParamCount()==o.getParamCount()));
+    }
+}
