6833cf9dba4a353e84d20643f8cbdb71ae3c5925
Colin DuPlantis <colind@marketcetera.com>
2008-08-13 18:06:47 +0000

    Partial implementation of the List Search Filter feature, to wit, the multiple view portion
    git-svn-id: file:///space/source.marketcetera.org@9489 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/AveragePriceView.java
+++ root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/AveragePriceView.java
@@ -1,17 +1,24 @@
 package org.marketcetera.photon.views;
 
 import org.eclipse.jface.action.IToolBarManager;
+import org.marketcetera.core.ClassVersion;
 import org.marketcetera.messagehistory.FIXMessageHistory;
 import org.marketcetera.messagehistory.MessageHolder;
+import org.marketcetera.photon.actions.OpenAdditionalViewAction;
 
 import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.FilterList;
 
+/* $License$ */
+
 /**
+ * View which displays the average price of a series of trades.
+ * 
  * @author gmiller
  * @author michael.lossos@softwaregoodness.com
- *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
  */
+@ClassVersion("$Id$") //$NON-NLS-1$
 public class AveragePriceView extends AbstractFIXMessagesView {
 
 	public static final String ID = "org.marketcetera.photon.views.AveragePriceView"; //$NON-NLS-1$
@@ -23,11 +30,13 @@ public class AveragePriceView extends AbstractFIXMessagesView {
 	}
 
 	@Override
-	protected void initializeToolBar(IToolBarManager theToolBarManager) {
-
+	protected void initializeToolBar(IToolBarManager theToolBarManager)
+	{
+        theToolBarManager.add(new OpenAdditionalViewAction(getViewSite().getWorkbenchWindow(),
+                                                           AVERAGE_PRICE_VIEW_LABEL.getText(),
+                                                           ID));
 	}
 
-	@SuppressWarnings("unchecked") //$NON-NLS-1$
 	protected FilterList<MessageHolder> getFilterList() {
 		return (FilterList<MessageHolder>) getInput();
 	}
@@ -37,9 +46,7 @@ public class AveragePriceView extends AbstractFIXMessagesView {
 	}
 
 	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-
+	public void setFocus()
+	{
 	}
-
 }
--- root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/OpenOrdersView.java
+++ root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/OpenOrdersView.java
@@ -1,17 +1,24 @@
 package org.marketcetera.photon.views;
 
 import org.eclipse.jface.action.IToolBarManager;
+import org.marketcetera.core.ClassVersion;
 import org.marketcetera.messagehistory.FIXMessageHistory;
 import org.marketcetera.messagehistory.MessageHolder;
 import org.marketcetera.photon.actions.CancelAllOpenOrdersAction;
+import org.marketcetera.photon.actions.OpenAdditionalViewAction;
 
 import ca.odell.glazedlists.EventList;
 
+/* $License$ */
+
 /**
+ * View which shows all open orders.
+ * 
  * @author gmiller
  * @author michael.lossos@softwaregoodness.com
- *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
  */
+@ClassVersion("$Id$") //$NON-NLS-1$
 public class OpenOrdersView extends AbstractFIXMessagesView {
 
 	public static final String ID = "org.marketcetera.photon.views.OpenOrdersView"; //$NON-NLS-1$
@@ -24,6 +31,9 @@ public class OpenOrdersView extends AbstractFIXMessagesView {
 	@Override
 	protected void initializeToolBar(IToolBarManager theToolBarManager) {
 		theToolBarManager.add(new CancelAllOpenOrdersAction());
+		theToolBarManager.add(new OpenAdditionalViewAction(getViewSite().getWorkbenchWindow(),
+		                                                   OPEN_ORDERS_VIEW_LABEL.getText(),
+		                                                   ID));
 	}
 
 	public EventList<MessageHolder> extractList(FIXMessageHistory hist) {
@@ -31,9 +41,7 @@ public class OpenOrdersView extends AbstractFIXMessagesView {
 	}
 
 	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-
+	public void setFocus()
+	{
 	}
-
 }
