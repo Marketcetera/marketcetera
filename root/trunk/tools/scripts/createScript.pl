76610881914f90c4ecced390f1e7cc79e2661416
Ken Lim <klim@marketcetera.com>
2008-09-05 18:42:10 +0000

    ENG-26 for EG-146: Create the wrapper for invoking the ORS CLI.
    git-svn-id: file:///space/source.marketcetera.org@9523 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/tools/scripts/createScript.pl
+++ root/trunk/tools/scripts/createScript.pl
@@ -11,83 +11,83 @@
 
 
 use strict;
-use Getopt::Std;
 
-
-# Extract arguments.
-
-use vars qw($opt_j);
-getopts('j');
 
 # Error checking.
 
-if (@ARGV!=3) {
-  warn "\n";
-  warn "Usage: ",__FILE__," [-j(mx)] [-c(onf)] <root> <name> <class>\n";
-  die "\n";
+if (@ARGV%3) {
+	warn "\n";
+	warn "Usage: ",__FILE__," [<root> <name> <class>]+\n";
+	die "\n";
 }
 
 # Configuration.
 
-my($root)=$ARGV[0];
-my($scriptBase)=$ARGV[1];
-my($class)=$ARGV[2];
-my($artifact)=($root=~m/[\/\\]([^\/\\]+)$/io);
-
-my($lib)=$root.'/lib';
-my($bin)=$root.'/bin';
-my($logs)=$root.'/logs';
 my($commonArgs)='-Xms384m -Xmx600m';
-if ($opt_j) {
-	$commonArgs.=' -Dcom.sun.management.jmxremote';
+
+# Generate scripts.
+
+while (@ARGV) {
+	my($root)=shift(@ARGV);
+	my($scriptBase)=shift(@ARGV);
+	my($class)=shift(@ARGV);
+
+	# Shorthands.
+
+	my($artifact)=($root=~m/[\/\\]([^\/\\]+)$/io);
+	my($lib)=$root.'/lib';
+	my($bin)=$root.'/bin';
+	my($logs)=$root.'/logs';
+
+    # Create output directory.
+
+	mkdir($bin) if (!(-e $bin));
+	mkdir($logs) if (!(-e $logs));
+
+    # Fetch jar files.
+
+	opendir(DIR,$lib);
+	my(@jars);
+	my($jar);
+	while ($jar=readdir(DIR)) {
+		next if ($jar!~/\.jar$/io);
+		push(@jars,$jar);
+	}
+	closedir(DIR);
+	@jars=sort(@jars);
+
+    # Create the Windows script.
+
+	my($sep)="\r\n";
+	my($script)=$bin.'/'.$scriptBase.'.bat';
+	open(OUT,'>'.$script);
+	binmode(OUT);
+	print OUT '@ECHO OFF'.$sep.$sep;
+	print OUT 'CALL "%~dp0..\\..\\setEnv.bat"'.$sep;
+	print OUT 'java.exe '.$commonArgs.
+		' -Dorg.marketcetera.appDir="%METC_HOME%\\'.$artifact.'"^'.$sep;
+	print OUT ' -cp "%METC_HOME%\\'.$artifact.'\\conf"^'.$sep;
+	foreach $jar (@jars) {
+		print OUT ';"%METC_HOME%\\'.$artifact.'\\lib\\'.$jar.'"^'.$sep;
+	}
+	print OUT ' '.$class.' %*'.$sep;
+	close(OUT);
+
+    # Create the Unix script.
+
+	my($sep)="\n";
+	my($script)=$bin.'/'.$scriptBase.'.sh';
+	open(OUT,'>'.$script);
+	binmode(OUT);
+	print OUT '#!/bin/sh'.$sep.$sep;
+	print OUT '. "$(dirname $0)/../../setEnv.sh"'.$sep;
+	print OUT 'exec java '.$commonArgs.
+		' -Dorg.marketcetera.appDir="${METC_HOME}/'.$artifact.'"\\'.$sep;
+	print OUT ' -cp "${METC_HOME}/'.$artifact.'/conf"\\'.$sep;
+	foreach $jar (@jars) {
+		print OUT ':"${METC_HOME}/'.$artifact.'/lib/'.$jar.'"\\'.$sep;
+	}
+	print OUT ' '.$class.' $*'.$sep;
+	close(OUT);
+	chmod(0755,$script);
 }
-
-# Create output directory.
-
-mkdir($bin) if (!(-e $bin));
-mkdir($logs) if (!(-e $logs));
-
-# Fetch jar files.
-
-opendir(DIR,$lib);
-my(@jars);
-my($jar);
-while ($jar=readdir(DIR)) {
-	next if ($jar!~/\.jar$/io);
-	push(@jars,$jar);
-}
-closedir(DIR);
-@jars=sort(@jars);
-
-# Create the Windows script.
-
-my($sep)="\r\n";
-my($script)=$bin.'/'.$scriptBase.'.bat';
-open(OUT,'>'.$script);
-binmode(OUT);
-print OUT '@ECHO OFF'.$sep.$sep;
-print OUT 'CALL "%~dp0..\\..\\setEnv.bat"'.$sep;
-print OUT 'java.exe '.$commonArgs.' -Dorg.marketcetera.appDir="%METC_HOME%\\'.$artifact.'"^'.$sep;
-print OUT ' -cp "%METC_HOME%\\'.$artifact.'\\conf"^'.$sep;
-foreach $jar (@jars) {
-	print OUT ';"%METC_HOME%\\'.$artifact.'\\lib\\'.$jar.'"^'.$sep;
-}
-print OUT ' '.$class.' %*'.$sep;
-close(OUT);
-
-# Create the Unix script.
-
-my($sep)="\n";
-my($script)=$bin.'/'.$scriptBase.'.sh';
-open(OUT,'>'.$script);
-binmode(OUT);
-print OUT '#!/bin/sh'.$sep.$sep;
-print OUT '. "$(dirname $0)/../../setEnv.sh"'.$sep;
-print OUT 'exec java '.$commonArgs.' -Dorg.marketcetera.appDir="${METC_HOME}/'.$artifact.'"\\'.$sep;
-print OUT ' -cp "${METC_HOME}/'.$artifact.'/conf"\\'.$sep;
-foreach $jar (@jars) {
-	print OUT ':"${METC_HOME}/'.$artifact.'/lib/'.$jar.'"\\'.$sep;
-}
-print OUT ' '.$class.' $*'.$sep;
-close(OUT);
-chmod(0755,$script);
