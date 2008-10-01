f208a4fd6f38d7dda11a1230b38f7db74a13f4dd
Ken Lim <klim@marketcetera.com>
2008-10-01 06:22:12 +0000

    Update version string to 0.9.0 (review ENG-49)
    git-svn-id: file:///space/source.marketcetera.org@9570 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/tools/scripts/setSvnProperties.pl
+++ root/trunk/tools/scripts/setSvnProperties.pl
@@ -63,11 +63,11 @@ my(%patternsCustom)=(
 	'public/source/photon/plugins/com.swtworkbench.community.xswt'
 	  => ('bin'),
 	'public/source/photon/plugins/org.marketcetera.core'
-	  => ('core-0.8.0.jar'),
+	  => ('core-0.9.0.jar'),
 	'public/source/photon/plugins/org.marketcetera.core.tests'
-	  => ('core-0.8.0-tests.jar'),
+	  => ('core-0.9.0-tests.jar'),
 	'public/source/photon/plugins/org.marketcetera.util'
-	  => ('util-0.8.0.jar'),
+	  => ('util-0.9.0.jar'),
 	'public/source/photon/plugins/org.marketcetera.bogusfeed'
 	  => ('bin'),
 	'public/source/photon/plugins/org.slf4j'
