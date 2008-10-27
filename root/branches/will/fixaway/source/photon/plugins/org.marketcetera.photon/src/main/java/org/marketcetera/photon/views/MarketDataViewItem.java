f13c4d7005c6c313ffad1807bd81d56fc20af6e0
Will Horn <will@marketcetera.com>
2008-10-27 20:28:54 +0000

    javadocs for MarketDataViewItem
    git-svn-id: file:///space/source.marketcetera.org@9917 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/will/fixaway/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/MarketDataViewItem.java
+++ root/branches/will/fixaway/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/MarketDataViewItem.java
@@ -14,7 +14,18 @@ import org.marketcetera.event.SymbolExchangeEvent;
 import org.marketcetera.event.TradeEvent;
 import org.marketcetera.photon.Messages;
 import org.marketcetera.photon.ui.ISymbolProvider;
+import org.marketcetera.util.misc.ClassVersion;
 
+/* $License$ */
+
+/**
+ * Market data view model object.
+ * 
+ * @author <a href="mailto:will@marketcetera.com">Will Horn</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@ClassVersion("$Id")//$NON-NLS-1$
 public class MarketDataViewItem implements ISymbolProvider {
 
 	private static final MessageFormat FORMAT = new MessageFormat(
@@ -28,6 +39,12 @@ public class MarketDataViewItem implements ISymbolProvider {
 	private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(
 			this);
 
+	/**
+	 * Constructor. Will throw an exception if symbol is null.
+	 * 
+	 * @param symbol
+	 *            symbol for item, cannot be null
+	 */
 	public MarketDataViewItem(MSymbol symbol) {
 		Assert.isNotNull(symbol);
 		mSymbol = symbol;
@@ -38,30 +55,55 @@ public class MarketDataViewItem implements ISymbolProvider {
 		return mSymbol;
 	}
 
+	/**
+	 * @return the last trade price, or null if none available
+	 */
 	public BigDecimal getLastPx() {
 		return mTradeEvent == null ? null : mTradeEvent.getPrice();
 	}
 
+	/**
+	 * @return the last trade quantity, or null if none available
+	 */
 	public BigDecimal getLastQty() {
 		return mTradeEvent == null ? null : mTradeEvent.getSize();
 	}
 
+	/**
+	 * @return the last bid size, or null if none available
+	 */
 	public BigDecimal getBidSize() {
 		return mBidEvent == null ? null : mBidEvent.getSize();
 	}
 
+	/**
+	 * @return the last bid price, or null if none available
+	 */
 	public BigDecimal getBidPx() {
 		return mBidEvent == null ? null : mBidEvent.getPrice();
 	}
 
+	/**
+	 * @return the last offer price, or null if none available
+	 */
 	public BigDecimal getOfferPx() {
 		return mAskEvent == null ? null : mAskEvent.getPrice();
 	}
 
+	/**
+	 * @return the last offer size, or null if none available
+	 */
 	public BigDecimal getOfferSize() {
 		return mAskEvent == null ? null : mAskEvent.getSize();
 	}
 
+	/**
+	 * Changes the underlying symbol of this item. All data will be reset if the
+	 * symbol changes. An exception will be thrown if symbol is null.
+	 * 
+	 * @param symbol
+	 *            the new symbol
+	 */
 	public void setSymbol(MSymbol symbol) {
 		Assert.isNotNull(symbol);
 		MSymbol oldSymbol = getSymbol();
@@ -75,6 +117,12 @@ public class MarketDataViewItem implements ISymbolProvider {
 		}
 	}
 
+	/**
+	 * Update cached bid event.
+	 * 
+	 * @param bidEvent
+	 *            the bid event
+	 */
 	public void setBidEvent(BidEvent bidEvent) {
 		if (!ObjectUtils.equals(mBidEvent, bidEvent)) {
 			if (!validateSymbol(bidEvent)) {
@@ -90,6 +138,12 @@ public class MarketDataViewItem implements ISymbolProvider {
 		}
 	}
 
+	/**
+	 * Update cached ask event.
+	 * 
+	 * @param askEvent
+	 *            the ask event
+	 */
 	public void setAskEvent(AskEvent askEvent) {
 		if (!ObjectUtils.equals(mAskEvent, askEvent)) {
 			if (!validateSymbol(askEvent)) {
@@ -105,6 +159,12 @@ public class MarketDataViewItem implements ISymbolProvider {
 		}
 	}
 
+	/**
+	 * Update cached trade event.
+	 * 
+	 * @param tradeEvent
+	 *            the trade event
+	 */
 	public void setTradeEvent(TradeEvent tradeEvent) {
 		if (!ObjectUtils.equals(mTradeEvent, tradeEvent)) {
 			if (!validateSymbol(tradeEvent)) {
@@ -119,32 +179,48 @@ public class MarketDataViewItem implements ISymbolProvider {
 					"lastQty", oldSizeValue, getLastQty()); //$NON-NLS-1$
 		}
 	}
-	
+
 	private boolean validateSymbol(SymbolExchangeEvent event) {
 		final MSymbol newSymbol = new MSymbol(event.getSymbol());
 		if (!getSymbol().equals(newSymbol)) {
-			Messages.MARKET_DATA_EVENT_SYMBOL_MISMATCH.warn(this, getSymbol(), newSymbol);
+			Messages.MARKET_DATA_EVENT_SYMBOL_MISMATCH.warn(this, getSymbol(),
+					newSymbol);
 			return false;
 		}
 		return true;
 	}
 
+	/**
+	 * @see PropertyChangeSupport#addPropertyChangeListener(String,
+	 *      PropertyChangeListener)
+	 */
 	public void addPropertyChangeListener(String propertyName,
 			PropertyChangeListener listener) {
 		propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
 	}
 
+	/**
+	 * @see PropertyChangeSupport#getPropertyChangeListeners(String)
+	 */
 	public PropertyChangeListener[] getPropertyChangeListeners(
 			String propertyName) {
 		return propertyChangeSupport.getPropertyChangeListeners(propertyName);
 	}
 
+	/**
+	 * @see PropertyChangeSupport#removePropertyChangeListener(String,
+	 *      PropertyChangeListener)
+	 */
 	public void removePropertyChangeListener(String propertyName,
 			PropertyChangeListener listener) {
 		propertyChangeSupport.removePropertyChangeListener(propertyName,
 				listener);
 	}
 
+	/**
+	 * This implementation returns a string representation of the item's
+	 * properties separated by tabs.
+	 */
 	@Override
 	public String toString() {
 		return FORMAT.format(new Object[] { getSymbol(), getLastPx(),
