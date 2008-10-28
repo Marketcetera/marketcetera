6ada859206bcdf1415f868b315f9ee2d26905e4b
Will Horn <will@marketcetera.com>
2008-10-28 20:01:50 +0000

    Merged trunk to branches/colin/marketdata_refactoring rev 9915 - 9921
    -converted marketdata-marketcetera and marketdata-bogus to plug-ins
    -added marketdata data flow tests
    
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@9923 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ marketdata_refactoring/source/photon/plugins/org.marketcetera.photon.module.tests/src/test/java/org/marketcetera/photon/module/preferences/ModulePropertiesPreferencePageTest.java
@@ -0,0 +1,37 @@
+package org.marketcetera.photon.module.preferences;
+
+
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/* $License$ */
+
+/**
+ * Test {@link ModulePropertiesPreferencePage}.
+ *
+ * @author <a href="mailto:will@marketcetera.com">Will Horn</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class ModulePropertiesPreferencePageTest extends SWTTestBase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	@Test
+	public void open() {
+		ModulePropertiesPreferencePage fixture = new ModulePropertiesPreferencePage();
+		GridLayoutFactory.fillDefaults().applyTo(shell);
+		fixture.createContents(shell);
+	}
+
+}
