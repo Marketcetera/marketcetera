2669b44cc8e21993e6fb52fa53d86dbe1dc10467
Will Horn <will@marketcetera.com>
2008-10-28 20:01:50 +0000

    Merged trunk to branches/colin/marketdata_refactoring rev 9915 - 9921
    -converted marketdata-marketcetera and marketdata-bogus to plug-ins
    -added marketdata data flow tests
    
    git-svn-id: file:///space/source.marketcetera.org@9923 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/branches/colin/marketdata_refactoring/source/photon/plugins/org.marketcetera.photon.module.tests/src/test/java/org/marketcetera/photon/module/tests/UISuite.java
@@ -0,0 +1,26 @@
+package org.marketcetera.photon.module.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.marketcetera.photon.module.SinkConsoleControllerTest;
+
+/* $License$ */
+
+/**
+ * Test suite that must be run as a Plug-in JUnit suite. 
+ * 
+ * Note: {@link HeadlessSuite Headless tests} are also run.
+ *
+ * @author <a href="mailto:will@marketcetera.com">Will Horn</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	HeadlessSuite.class,
+	SinkConsoleControllerTest.class
+})
+public class UISuite {
+
+}
+
