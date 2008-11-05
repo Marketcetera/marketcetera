8717ee4f19e25cc7c0d32dc3562f27d1e57a3061
Will Horn <will@marketcetera.com>
2008-11-05 21:30:32 +0000

    rev 10004 was an accident, reversing that change
    git-svn-id: file:///space/source.marketcetera.org@10005 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/MarketDataView.java
+++ root/trunk/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/MarketDataView.java
@@ -435,7 +435,6 @@ public final class MarketDataView extends ViewPart implements IMSymbolListener,
 					}
 				}
 			});
-			PhotonPlugin.getDefault().getMarketDataLogger().debug(inData);
 		}
 		
 	}
