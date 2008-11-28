8ae6519af3d0196de1117271df5410ca93f9ff29
Toli Lerios <tlerios@marketcetera.com>
2008-11-28 06:02:53 +0000

    Author: tlerios
    Reviewer: anshul, ytalmor
    
    Crucible: ENG-139
    
    + Added Web Services to ORS client.
    + Changed getPositionAsOf signature to BigDecimal from BigInteger.
    + Removed extraneous imports.
    + Minor pom file edits.
    
    
    
    git-svn-id: file:///space/source.marketcetera.org@10110 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/client/src/test/java/org/marketcetera/client/MockServiceImpl.java
@@ -0,0 +1,168 @@
+package org.marketcetera.client;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedList;
+import org.marketcetera.client.dest.DestinationStatus;
+import org.marketcetera.client.dest.DestinationsStatus;
+import org.marketcetera.core.MSymbol;
+import org.marketcetera.trade.DestinationID;
+import org.marketcetera.trade.ExecutionReportImpl;
+import org.marketcetera.trade.Factory;
+import org.marketcetera.trade.MessageCreationException;
+import org.marketcetera.trade.OrderCancelRejectImpl;
+import org.marketcetera.trade.Originator;
+import org.marketcetera.trade.ReportBaseImpl;
+import org.marketcetera.util.misc.ClassVersion;
+import org.marketcetera.util.ws.stateful.ClientContext;
+import org.marketcetera.util.ws.stateful.RemoteCaller;
+import org.marketcetera.util.ws.stateful.ServiceBaseImpl;
+import org.marketcetera.util.ws.stateful.SessionHolder;
+import org.marketcetera.util.ws.stateful.SessionManager;
+import org.marketcetera.util.ws.wrappers.RemoteException;
+import quickfix.field.OrigClOrdID;
+import quickfix.fix44.ExecutionReport;
+import quickfix.fix44.OrderCancelReject;
+
+/**
+ * A test service implementation to aid testing of client via {@link
+ * MockServer}.
+ * 
+ * @author tlerios@marketcetera.com
+ * @since $Release$
+ * @version $Id$
+ */
+
+/* $License$ */
+
+@ClassVersion("$Id$") //$NON-NLS-1$
+public class MockServiceImpl
+    extends ServiceBaseImpl<Object>
+    implements Service
+{
+
+    // INSTANCE DATA.
+
+    private int mNextOrderID;
+
+
+    // CONSTRUCTORS.
+
+    public MockServiceImpl
+        (SessionManager<Object> sessionManager)
+    {
+        super(sessionManager);
+    }
+
+
+    // INSTANCE METHODS.
+
+    private DestinationsStatus getDestinationsStatusImpl()
+    {
+        LinkedList<DestinationStatus> list=new LinkedList<DestinationStatus>();
+        list.add(new DestinationStatus("N1",new DestinationID("ID1"),true));
+        list.add(new DestinationStatus("N2",new DestinationID("ID2"),false));
+        return new DestinationsStatus(list);
+    }
+
+    private ReportBaseImpl[] getReportsSinceImpl
+        (Date date)
+        throws MessageCreationException
+    {
+        Factory f=Factory.getInstance();
+        DestinationID dID=new DestinationID("me");
+        ExecutionReport er=new ExecutionReport();
+        er.set(new OrigClOrdID("42"));
+        OrderCancelReject ocr=new OrderCancelReject();
+        ocr.set(new OrigClOrdID("43"));
+        return new ReportBaseImpl[] {
+            (ExecutionReportImpl)
+            f.createExecutionReport(er,dID,Originator.Server),
+            (OrderCancelRejectImpl)
+            f.createOrderCancelReject(ocr,dID)
+        };
+    }
+
+    private BigDecimal getPositionAsOfImpl
+        (Date date,
+         MSymbol symbol)
+    {
+        return new BigDecimal(date.getTime());
+    }
+
+    private String getNextOrderIDImpl()
+    {
+        return "MyID"+(mNextOrderID++);
+    }
+
+
+    // Service.
+
+    @Override
+    public DestinationsStatus getDestinationsStatus
+        (ClientContext context)
+        throws RemoteException
+    {
+        return (new RemoteCaller<Object,DestinationsStatus>
+                (getSessionManager()) {
+            @Override
+            protected DestinationsStatus call
+                (ClientContext context,
+                 SessionHolder<Object> sessionHolder)
+            {
+                return getDestinationsStatusImpl();
+            }}).execute(context);
+    }
+
+    @Override
+    public ReportBaseImpl[] getReportsSince
+        (ClientContext context,
+         final Date date)
+        throws RemoteException
+    {
+        return (new RemoteCaller<Object,ReportBaseImpl[]>
+                (getSessionManager()) {
+            @Override
+            protected ReportBaseImpl[] call
+                (ClientContext context,
+                 SessionHolder<Object> sessionHolder)
+                throws MessageCreationException
+            {
+                return getReportsSinceImpl(date);
+            }}).execute(context);
+    }
+
+    @Override
+    public BigDecimal getPositionAsOf
+        (ClientContext context,
+         final Date date,
+         final MSymbol symbol)
+        throws RemoteException
+    {
+        return (new RemoteCaller<Object,BigDecimal>
+                (getSessionManager()) {
+            @Override
+            protected BigDecimal call
+                (ClientContext context,
+                 SessionHolder<Object> sessionHolder)
+            {
+                return getPositionAsOfImpl(date,symbol);
+            }}).execute(context);
+    }
+
+    @Override
+    public String getNextOrderID
+        (ClientContext context)
+        throws RemoteException
+    {
+        return (new RemoteCaller<Object,String>
+                (getSessionManager()) {
+            @Override
+            protected String call
+                (ClientContext context,
+                 SessionHolder<Object> sessionHolder)
+            {
+                return getNextOrderIDImpl();
+            }}).execute(context);
+    }
+}
