88cd08c2358375d96052a53a59ac8635ebdbcde8
Will Horn <will@marketcetera.com>
2008-12-08 22:53:18 +0000

    EG-379: Improve ruby strategy template for New Ruby Strategy Wizard
    Reviewed by Colin, ENG-206
    
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@10214 501a4a96-914b-0410-8454-f5988a4ca596

--- source/photon/plugins/org.marketcetera.photon.strategy/src/main/resources/org/marketcetera/photon/internal/strategy/ruby/RubyStrategyTemplate.rb
+++ source/photon/plugins/org.marketcetera.photon.strategy/src/main/resources/org/marketcetera/photon/internal/strategy/ruby/RubyStrategyTemplate.rb
@@ -1,47 +1,76 @@
-require 'java'
 include_class "org.marketcetera.strategy.ruby.Strategy"
-include_class "org.marketcetera.core.MSymbol"
-include_class "java.lang.Long"
-include_class "java.lang.System"
-include_class "java.util.Arrays"
 
+###############################
+# Sample strategy template    #
+###############################
 class __TEMPLATE_CLASS_NAME__ < Strategy
-  def on_ask(ask)
-        set_property("onAsk",
-                     Long.toString(System.nanoTime()));
-  end  
-  def on_bid(bid)
-        set_property("onBid",
-                     Long.toString(System.nanoTime()));
-  end  
-  def on_callback(data)
-        set_property("onCallback",
-                     Long.toString(System.nanoTime()));
-        # execute all services
-        set_property("getCurrentTime",
-                     get_current_time().toString());
-        set_property("getExecutionReport",
-                     Arrays.toString(get_execution_report(nil).toArray()));
-        set_property("getCurrentPositionAtOpen",
-                     get_current_position_at_open(MSymbol.new("symbol")).toString());
-        set_property("getCurrentPosition",
-                     get_current_position(MSymbol.new("symbol")).toString());
-  end  
-  def on_cancel(cancel)
-        set_property("onCancel",
-                     Long.toString(System.nanoTime()));
-  end  
-  def on_execution_report(execution_report)
-        set_property("onExecutionReport",
-                     Long.toString(System.nanoTime()));
-  end  
-  def on_news(symbol,
-              news)
-        set_property("onNews",
-                     Long.toString(System.nanoTime()));
-  end  
-  def on_trade(trade)
-        set_property("onTrade",
-                     Long.toString(System.nanoTime()));
-  end  
+  
+    ##########################################
+    # Executed when the strategy is started. #
+    #                                        #
+    # Use this method to set up data flows   #
+    #  and other initialization tasks.       #
+    ##########################################
+    def on_start
+      
+    end
+    
+    ############################################
+    # Executed when the strategy is stopped.   #
+    ############################################
+    def on_stop
+      
+    end
+    
+    ####################################################
+    # Executed when the strategy receives an ask event #
+    ####################################################
+    def on_ask(ask)
+      
+    end
+    
+    ###################################################
+    # Executed when the strategy receives a bid event #
+    ###################################################
+    def on_bid(bid)
+      
+    end
+    
+    #####################################################
+    # Executed when the strategy receives a trade event #
+    #####################################################
+    def on_trade(trade)
+      
+    end
+    
+    ###########################################################
+    # Executed when the strategy receives an execution report #
+    ###########################################################
+    def on_execution_report(executionReport)
+      
+    end
+    
+    ############################################################
+    # Executed when the strategy receives data of a type other #
+    #  than the other callbacks                                #
+    ############################################################
+    def on_other(data)
+      
+    end
+
+    ############################################################
+    # Executed when the strategy receives a callback requested #
+    #  via request_callback_at or request_callback_after       #
+    ############################################################
+    def on_callback(data)
+      
+    end
+    
+    ##############################################################
+    # Executed when the strategy receives an order cancel reject #
+    #  event                                                     #
+    ##############################################################
+    def on_cancel(reject)
+      
+    end
 end
