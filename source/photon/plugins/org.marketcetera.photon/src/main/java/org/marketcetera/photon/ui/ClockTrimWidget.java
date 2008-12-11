9af93ecda945a4ce03bfaeaf3434e42b8619dc5f
Will Horn <will@marketcetera.com>
2008-12-11 01:46:21 +0000

    EG-263 FIX reviewed by Yoram.
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@10240 501a4a96-914b-0410-8454-f5988a4ca596

--- source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/ClockTrimWidget.java
+++ source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/ClockTrimWidget.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -12,48 +13,52 @@ import org.marketcetera.core.ClassVersion;
 import org.marketcetera.core.ThreadLocalSimpleDateFormat;
 
 /**
- * Places the clock widget in the bottom status bar of the Photon client.
- * Sets up the clock to be updated every second.
+ * Places the clock widget in the bottom status bar of the Photon client. Sets
+ * up the clock to be updated every second.
  * 
  * @author toli
  * @version $Id$
  */
 
-@ClassVersion("$Id$") //$NON-NLS-1$
+@ClassVersion("$Id$")
 public class ClockTrimWidget extends Workaround253082ContributionItem {
-    private Label clockValue;
-    private static final ThreadLocalSimpleDateFormat DATE_FORMAT_LOCAL = new ThreadLocalSimpleDateFormat("MMM d HH:mm:ss z"); //$NON-NLS-1$
-    private static final Timer timer = new Timer("ClockUpdateTimer"); //$NON-NLS-1$
+	private Label clockValue;
+	private static final ThreadLocalSimpleDateFormat DATE_FORMAT_LOCAL = new ThreadLocalSimpleDateFormat(
+			"MMM d HH:mm:ss z"); //$NON-NLS-1$
+	private static final Timer timer = new Timer("ClockUpdateTimer"); //$NON-NLS-1$
 	private TimerTask task;
-    
+
 	@Override
-    public void doDispose() {
-        if (task != null) {
-            task.cancel();
-            task = null;
-        }
+	public void doDispose() {
+		if (task != null) {
+			task.cancel();
+			task = null;
+		}
 		super.doDispose();
-    }
+	}
 
 	@Override
 	protected Control createControl(Composite parent) {
-		clockValue = new Label(parent, SWT.NONE);
-        clockValue.setText(DATE_FORMAT_LOCAL.get().format(new Date()));
+		Composite composite = new Composite(parent, SWT.NONE);
+		clockValue = new Label(composite, SWT.NONE);
+		clockValue.setText(DATE_FORMAT_LOCAL.get().format(new Date()));
+		GridLayoutFactory.swtDefaults().generateLayout(composite);
 
-        task = new TimerTask() {
-            public void run() {
-                if (!clockValue.isDisposed()) {
-                    clockValue.getDisplay().asyncExec(new Runnable() {
-                        public void run() {
-                            if (!clockValue.isDisposed()) {
-                            	clockValue.setText(DATE_FORMAT_LOCAL.get().format(new Date()));
-                            }
-                        }
-                    });
-                }
-            }
-        };
+		task = new TimerTask() {
+			public void run() {
+				if (!clockValue.isDisposed()) {
+					clockValue.getDisplay().asyncExec(new Runnable() {
+						public void run() {
+							if (!clockValue.isDisposed()) {
+								clockValue.setText(DATE_FORMAT_LOCAL.get()
+										.format(new Date()));
+							}
+						}
+					});
+				}
+			}
+		};
 		timer.schedule(task, 0, 1000);
-		return clockValue;
+		return composite;
 	}
 }
