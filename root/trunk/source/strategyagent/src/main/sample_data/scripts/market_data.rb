ad66d5089dff6f942fc5cac55b35043f5af81c76
Anshul Jaiswal <anshul@marketcetera.com>
2008-12-11 21:31:32 +0000

    - Reviewed by Yoram per ENG-228 - Added sample strategy scripts and commands to run them to the strategy agent installation - Added properties files for marketcetera feed and client module so that they connect out-of-the-box - Updated assembly to copy them to the appropriate  subdirectories in strategyagent install - Updated sampleCommands.txt to refer to real module names
    git-svn-id: file:///space/source.marketcetera.org@10241 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/strategyagent/src/main/sample_data/scripts/market_data.rb
@@ -0,0 +1,47 @@
+#
+# author:anshul@marketcetera.com
+# since $Release$
+# version: $Id$
+#
+#   
+include_class "org.marketcetera.strategy.ruby.Strategy"
+
+#######################################
+# Strategy that receives marketdata   #
+#######################################
+class MarketData < Strategy
+    SYMBOLS = "AMZN,JAVA" # Depends on MD - can be other symbols
+    MARKET_DATA_PROVIDER = "marketcetera" # Can also be activ, bogus, opentick
+
+    ##########################################
+    # Executed when the strategy is started. #
+    #                                        #
+    # Use this method to set up data flows   #
+    #  and other initialization tasks.       #
+    ##########################################
+    def on_start
+      request_market_data(SYMBOLS,MARKET_DATA_PROVIDER)
+    end
+
+    ####################################################
+    # Executed when the strategy receives an ask event #
+    ####################################################
+    def on_ask(ask)
+      puts "Ask " + ask.to_s
+    end
+
+    ###################################################
+    # Executed when the strategy receives a bid event #
+    ###################################################
+    def on_bid(bid)
+      puts "Bid " + bid.to_s
+    end
+
+    #####################################################
+    # Executed when the strategy receives a trade event #
+    #####################################################
+    def on_trade(trade)
+      puts "Trade " + trade.to_s
+    end
+
+end
