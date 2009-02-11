a264021664109f867c2dab6ee08627b0a1f60c55
Ken Lim <klim@marketcetera.com>
2009-02-11 23:16:39 +0000

    Remove bad tag.
    git-svn-id: file:///space/source.marketcetera.org@10311 501a4a96-914b-0410-8454-f5988a4ca596

--- root/tags/1.1.0/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/validation/IMessageDisplayer.java
+++
@@ -1,6 +0,0 @@
-package org.marketcetera.photon.ui.validation;
-
-public interface IMessageDisplayer {
-	public void showErrorMessage(String errorString, int severity);
-	public void clearMessage();
-}
