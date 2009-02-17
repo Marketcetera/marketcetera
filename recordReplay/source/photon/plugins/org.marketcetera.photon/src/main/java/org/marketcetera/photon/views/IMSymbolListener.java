aaa9c38a5ef68ae29b136bcf27d8db5b92bb4334
Anshul Jaiswal <anshul@marketcetera.com>
2009-02-17 22:48:31 +0000

    - Removing the branch as it's not needed
    git-svn-id: file:///space/source.marketcetera.org/root/branches/anshul@10330 501a4a96-914b-0410-8454-f5988a4ca596

--- recordReplay/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IMSymbolListener.java
+++
@@ -1,9 +0,0 @@
-package org.marketcetera.photon.views;
-
-import org.marketcetera.trade.MSymbol;
-
-public interface IMSymbolListener {
-	void onAssertSymbol(MSymbol symbol);
-	
-	boolean isListeningSymbol(MSymbol symbol);
-}
--- recordReplay/source/tradebase/vendor/plugins/activemessaging/test/all_tests.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require_gem 'coverage'
-
-require 'test/unit'
-
-require 'test/config_test'
-require 'test/filter_test'
-require 'test/tracer_test'
-require 'test/jms_test'
