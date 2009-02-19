f4ce8f645bfff0621d0988d385281afd5e6b51e5
Will Horn <will@marketcetera.com>
2009-02-19 04:06:36 +0000

    removing old branch
    git-svn-id: file:///space/source.marketcetera.org/root/branches/will@10333 501a4a96-914b-0410-8454-f5988a4ca596

--- custom-mdata/source/photon/plugins/com.swtworkbench.community.xswt/src/com/swtworkbench/community/xswt/dataparser/IDataParserContext.java
+++
@@ -1,7 +0,0 @@
-package com.swtworkbench.community.xswt.dataparser;
-
-import com.swtworkbench.community.xswt.XSWTException;
-
-public interface IDataParserContext {
-	public Object parse(String value, Class klass) throws XSWTException;
-}
