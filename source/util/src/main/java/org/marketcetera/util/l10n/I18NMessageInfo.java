4d891c0ce28ab226aaa23bd2f5614f0cd067cbcd
Toli Lerios <tlerios@marketcetera.com>
2009-03-06 17:21:43 +0000

    Author: tlerios
    Reviewer: ajaiswal
    
    Crucible: ENG-292
    
    Comments:
    
    M1 for multiple traders.
    
    * Removed //NON-NLS comments from annotations.
    
    
    
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@10380 501a4a96-914b-0410-8454-f5988a4ca596

--- source/util/src/main/java/org/marketcetera/util/l10n/I18NMessageInfo.java
+++ source/util/src/main/java/org/marketcetera/util/l10n/I18NMessageInfo.java
@@ -14,7 +14,7 @@ import org.marketcetera.util.misc.ClassVersion;
 
 /* $License$ */
 
-@ClassVersion("$Id$") //$NON-NLS-1$
+@ClassVersion("$Id$")
 public class I18NMessageInfo
     extends MessageInfo
 {
--- source/util/src/main/java/org/marketcetera/util/l10n/MessageComparator.java
+++ source/util/src/main/java/org/marketcetera/util/l10n/MessageComparator.java
@@ -17,7 +17,7 @@ import org.marketcetera.util.misc.ClassVersion;
 
 /* $License$ */
 
-@ClassVersion("$Id$") //$NON-NLS-1$
+@ClassVersion("$Id$")
 public class MessageComparator
 {
 
