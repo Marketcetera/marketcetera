9b230b7e4210b78663bf7be6bde652286d206a15
Anshul Jaiswal <anshul@marketcetera.com>
2009-05-14 19:52:51 +0000

    - Reviewed by Colin per ENG-418 - Added boiler plate package documentation to all the packages that don't have them.
    git-svn-id: file:///space/source.marketcetera.org@10573 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/modules/marketdata/marketcetera/src/main/java/org/marketcetera/marketdata/marketcetera/package-info.java
@@ -0,0 +1,10 @@
+/* $License$ */
+/**
+ * A market data feed that provides market data from the marketcetera
+ * exchange simulator.
+ *
+ * @author anshul@marketcetera.com
+ * @version $Id$
+ * @since 1.0.0
+ */
+package org.marketcetera.marketdata.marketcetera;
