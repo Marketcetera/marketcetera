4d546467ceab1ebf781197a3d791c48f5f9734dc
Toli Lerios <tlerios@marketcetera.com>
2009-08-12 02:21:20 +0000

    Author: tlerios
    Reviewer: colin
    
    Crucible: ENG-462
    
    JIRA: EG-747
    
    Comments:
    
    * Added information stores.
    
    
    
    git-svn-id: file:///space/source.marketcetera.org@10662 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/ors/src/main/java/org/marketcetera/ors/info/NestedInfoImpl.java
@@ -0,0 +1,81 @@
+package org.marketcetera.ors.info;
+
+import org.marketcetera.util.misc.ClassVersion;
+
+/**
+ * A store for key-value pairs, which also has a parent store.
+ *
+ * <p>This class is not intended to be thread-safe.</p>
+ *
+ * @author tlerios@marketcetera.com
+ * @since $Release$
+ * @version $Id$
+ */
+
+/* $License$ */
+
+@ClassVersion("$Id$")
+class NestedInfoImpl<T extends ReadInfo>
+    extends ReadWriteInfoImpl
+{
+
+    // CLASS DATA.
+
+    /**
+     * The name separator in paths.
+     */
+
+    static final String NAME_SEPARATOR=
+        ":"; //$NON-NLS-1$
+
+
+    // INSTANCE DATA.
+
+    private final T mParentInfo;
+
+
+    // CONSTRUCTORS.
+
+    /**
+     * Creates a new store with the given name and the given parent
+     * store.
+     *
+     * @param name The store name.
+     * @param parentInfo The parent store.
+     */
+
+    NestedInfoImpl
+        (String name,
+         T parentInfo)
+    {
+        super(name);
+        mParentInfo=parentInfo;
+    }
+
+
+    // INSTANCE METHODS.
+
+    /**
+     * Returns the receiver's parent store.
+     *
+     * @return The parent store.
+     */
+
+    T getParentInfo()
+    {
+        return mParentInfo;
+    }
+
+
+    // ReadWriteInfoImpl.
+
+    @Override
+    public String getPath()
+    {
+        StringBuilder builder=new StringBuilder();
+        builder.append(getParentInfo().getPath());
+        builder.append(NAME_SEPARATOR);
+        builder.append(super.getPath());
+        return builder.toString();
+    }
+}
