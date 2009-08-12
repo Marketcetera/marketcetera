145ada06c8c2cad32d8dc2953267098f1d2b7453
Will Horn <will@marketcetera.com>
2009-08-12 16:30:50 +0000

    Core Model
    git-svn-id: file:///space/source.marketcetera.org@10676 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/branches/will/strategy-engine-review/source/photon/plugins/org.marketcetera.photon.strategy.engine/src/model/java/org/marketcetera/photon/strategy/engine/model/core/impl/StrategyEngineImpl.java
@@ -0,0 +1,537 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.marketcetera.photon.strategy.engine.model.core.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.eclipse.emf.ecore.util.EObjectWithInverseResolvingEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+import org.marketcetera.photon.strategy.engine.model.core.ConnectionState;
+import org.marketcetera.photon.strategy.engine.model.core.DeployedStrategy;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngine;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngineConnection;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngineCorePackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Strategy Engine</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#getName <em>Name</em>}</li>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#getDescription <em>Description</em>}</li>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#getConnectionState <em>Connection State</em>}</li>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#getConnection <em>Connection</em>}</li>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#getDeployedStrategies <em>Deployed Strategies</em>}</li>
+ *   <li>{@link org.marketcetera.photon.strategy.engine.model.core.impl.StrategyEngineImpl#isReadOnly <em>Read Only</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class StrategyEngineImpl extends EObjectImpl implements StrategyEngine {
+    /**
+     * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getName()
+     * @generated
+     * @ordered
+     */
+    protected static final String NAME_EDEFAULT = null;
+
+    /**
+     * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getName()
+     * @generated
+     * @ordered
+     */
+    protected String name = NAME_EDEFAULT;
+
+    /**
+     * The default value of the '{@link #getDescription() <em>Description</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getDescription()
+     * @generated
+     * @ordered
+     */
+    protected static final String DESCRIPTION_EDEFAULT = null;
+
+    /**
+     * The cached value of the '{@link #getDescription() <em>Description</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getDescription()
+     * @generated
+     * @ordered
+     */
+    protected String description = DESCRIPTION_EDEFAULT;
+
+    /**
+     * The default value of the '{@link #getConnectionState() <em>Connection State</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getConnectionState()
+     * @generated
+     * @ordered
+     */
+    protected static final ConnectionState CONNECTION_STATE_EDEFAULT = ConnectionState.DISCONNECTED;
+
+    /**
+     * The cached value of the '{@link #getConnectionState() <em>Connection State</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getConnectionState()
+     * @generated
+     * @ordered
+     */
+    protected ConnectionState connectionState = CONNECTION_STATE_EDEFAULT;
+
+    /**
+     * The cached value of the '{@link #getConnection() <em>Connection</em>}' reference.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getConnection()
+     * @generated
+     * @ordered
+     */
+    protected StrategyEngineConnection connection;
+
+    /**
+     * The cached value of the '{@link #getDeployedStrategies() <em>Deployed Strategies</em>}' reference list.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getDeployedStrategies()
+     * @generated
+     * @ordered
+     */
+    protected EList<DeployedStrategy> deployedStrategies;
+
+    /**
+     * The default value of the '{@link #isReadOnly() <em>Read Only</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isReadOnly()
+     * @generated
+     * @ordered
+     */
+    protected static final boolean READ_ONLY_EDEFAULT = false;
+
+    /**
+     * The cached value of the '{@link #isReadOnly() <em>Read Only</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isReadOnly()
+     * @generated
+     * @ordered
+     */
+    protected boolean readOnly = READ_ONLY_EDEFAULT;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected StrategyEngineImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return StrategyEngineCorePackage.Literals.STRATEGY_ENGINE;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setName(String newName) {
+        String oldName = name;
+        name = newName;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__NAME, oldName,
+                    name));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setDescription(String newDescription) {
+        String oldDescription = description;
+        description = newDescription;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__DESCRIPTION,
+                    oldDescription, description));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public ConnectionState getConnectionState() {
+        return connectionState;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setConnectionState(ConnectionState newConnectionState) {
+        ConnectionState oldConnectionState = connectionState;
+        connectionState = newConnectionState == null ? CONNECTION_STATE_EDEFAULT
+                : newConnectionState;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(
+                    this,
+                    Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION_STATE,
+                    oldConnectionState, connectionState));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public StrategyEngineConnection getConnection() {
+        if (connection != null && connection.eIsProxy()) {
+            InternalEObject oldConnection = (InternalEObject) connection;
+            connection = (StrategyEngineConnection) eResolveProxy(oldConnection);
+            if (connection != oldConnection) {
+                if (eNotificationRequired())
+                    eNotify(new ENotificationImpl(
+                            this,
+                            Notification.RESOLVE,
+                            StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION,
+                            oldConnection, connection));
+            }
+        }
+        return connection;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public StrategyEngineConnection basicGetConnection() {
+        return connection;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public NotificationChain basicSetConnection(
+            StrategyEngineConnection newConnection, NotificationChain msgs) {
+        StrategyEngineConnection oldConnection = connection;
+        connection = newConnection;
+        if (eNotificationRequired()) {
+            ENotificationImpl notification = new ENotificationImpl(this,
+                    Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION,
+                    oldConnection, newConnection);
+            if (msgs == null)
+                msgs = notification;
+            else
+                msgs.add(notification);
+        }
+        return msgs;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setConnection(StrategyEngineConnection newConnection) {
+        if (newConnection != connection) {
+            NotificationChain msgs = null;
+            if (connection != null)
+                msgs = ((InternalEObject) connection)
+                        .eInverseRemove(
+                                this,
+                                StrategyEngineCorePackage.STRATEGY_ENGINE_CONNECTION__ENGINE,
+                                StrategyEngineConnection.class, msgs);
+            if (newConnection != null)
+                msgs = ((InternalEObject) newConnection)
+                        .eInverseAdd(
+                                this,
+                                StrategyEngineCorePackage.STRATEGY_ENGINE_CONNECTION__ENGINE,
+                                StrategyEngineConnection.class, msgs);
+            msgs = basicSetConnection(newConnection, msgs);
+            if (msgs != null)
+                msgs.dispatch();
+        } else if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION,
+                    newConnection, newConnection));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EList<DeployedStrategy> getDeployedStrategies() {
+        if (deployedStrategies == null) {
+            deployedStrategies = new EObjectWithInverseResolvingEList<DeployedStrategy>(
+                    DeployedStrategy.class,
+                    this,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES,
+                    StrategyEngineCorePackage.DEPLOYED_STRATEGY__ENGINE);
+        }
+        return deployedStrategies;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setReadOnly(boolean newReadOnly) {
+        boolean oldReadOnly = readOnly;
+        readOnly = newReadOnly;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET,
+                    StrategyEngineCorePackage.STRATEGY_ENGINE__READ_ONLY,
+                    oldReadOnly, readOnly));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public NotificationChain eInverseAdd(InternalEObject otherEnd,
+            int featureID, NotificationChain msgs) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            if (connection != null)
+                msgs = ((InternalEObject) connection)
+                        .eInverseRemove(
+                                this,
+                                StrategyEngineCorePackage.STRATEGY_ENGINE_CONNECTION__ENGINE,
+                                StrategyEngineConnection.class, msgs);
+            return basicSetConnection((StrategyEngineConnection) otherEnd, msgs);
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            return ((InternalEList<InternalEObject>) (InternalEList<?>) getDeployedStrategies())
+                    .basicAdd(otherEnd, msgs);
+        }
+        return super.eInverseAdd(otherEnd, featureID, msgs);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public NotificationChain eInverseRemove(InternalEObject otherEnd,
+            int featureID, NotificationChain msgs) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            return basicSetConnection(null, msgs);
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            return ((InternalEList<?>) getDeployedStrategies()).basicRemove(
+                    otherEnd, msgs);
+        }
+        return super.eInverseRemove(otherEnd, featureID, msgs);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Object eGet(int featureID, boolean resolve, boolean coreType) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__NAME:
+            return getName();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DESCRIPTION:
+            return getDescription();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION_STATE:
+            return getConnectionState();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            if (resolve)
+                return getConnection();
+            return basicGetConnection();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            return getDeployedStrategies();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__READ_ONLY:
+            return isReadOnly();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void eSet(int featureID, Object newValue) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__NAME:
+            setName((String) newValue);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DESCRIPTION:
+            setDescription((String) newValue);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION_STATE:
+            setConnectionState((ConnectionState) newValue);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            setConnection((StrategyEngineConnection) newValue);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            getDeployedStrategies().clear();
+            getDeployedStrategies().addAll(
+                    (Collection<? extends DeployedStrategy>) newValue);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__READ_ONLY:
+            setReadOnly((Boolean) newValue);
+            return;
+        }
+        super.eSet(featureID, newValue);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void eUnset(int featureID) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__NAME:
+            setName(NAME_EDEFAULT);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DESCRIPTION:
+            setDescription(DESCRIPTION_EDEFAULT);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION_STATE:
+            setConnectionState(CONNECTION_STATE_EDEFAULT);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            setConnection((StrategyEngineConnection) null);
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            getDeployedStrategies().clear();
+            return;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__READ_ONLY:
+            setReadOnly(READ_ONLY_EDEFAULT);
+            return;
+        }
+        super.eUnset(featureID);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public boolean eIsSet(int featureID) {
+        switch (featureID) {
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__NAME:
+            return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT
+                    .equals(name);
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DESCRIPTION:
+            return DESCRIPTION_EDEFAULT == null ? description != null
+                    : !DESCRIPTION_EDEFAULT.equals(description);
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION_STATE:
+            return connectionState != CONNECTION_STATE_EDEFAULT;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__CONNECTION:
+            return connection != null;
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__DEPLOYED_STRATEGIES:
+            return deployedStrategies != null && !deployedStrategies.isEmpty();
+        case StrategyEngineCorePackage.STRATEGY_ENGINE__READ_ONLY:
+            return readOnly != READ_ONLY_EDEFAULT;
+        }
+        return super.eIsSet(featureID);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public String toString() {
+        if (eIsProxy())
+            return super.toString();
+
+        StringBuffer result = new StringBuffer(super.toString());
+        result.append(" (name: ");
+        result.append(name);
+        result.append(", description: ");
+        result.append(description);
+        result.append(", connectionState: ");
+        result.append(connectionState);
+        result.append(", readOnly: ");
+        result.append(readOnly);
+        result.append(')');
+        return result.toString();
+    }
+
+} //StrategyEngineImpl
---
+++ root/branches/will/strategy-engine-review/source/photon/plugins/org.marketcetera.photon.strategy.engine/src/model/java/org/marketcetera/photon/strategy/engine/model/core/util/StrategyEngineCoreAdapterFactory.java
@@ -0,0 +1,204 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.marketcetera.photon.strategy.engine.model.core.util;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+import org.eclipse.emf.ecore.EObject;
+import org.marketcetera.photon.strategy.engine.model.core.DeployedStrategy;
+import org.marketcetera.photon.strategy.engine.model.core.Strategy;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngine;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngineConnection;
+import org.marketcetera.photon.strategy.engine.model.core.StrategyEngineCorePackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Adapter Factory</b> for the model.
+ * It provides an adapter <code>createXXX</code> method for each class of the model.
+ * <!-- end-user-doc -->
+ * @see org.marketcetera.photon.strategy.engine.model.core.StrategyEngineCorePackage
+ * @generated
+ */
+public class StrategyEngineCoreAdapterFactory extends AdapterFactoryImpl {
+    /**
+     * The cached model package.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected static StrategyEngineCorePackage modelPackage;
+
+    /**
+     * Creates an instance of the adapter factory.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public StrategyEngineCoreAdapterFactory() {
+        if (modelPackage == null) {
+            modelPackage = StrategyEngineCorePackage.eINSTANCE;
+        }
+    }
+
+    /**
+     * Returns whether this factory is applicable for the type of the object.
+     * <!-- begin-user-doc -->
+     * This implementation returns <code>true</code> if the object is either the model's package or is an instance object of the model.
+     * <!-- end-user-doc -->
+     * @return whether this factory is applicable for the type of the object.
+     * @generated
+     */
+    @Override
+    public boolean isFactoryForType(Object object) {
+        if (object == modelPackage) {
+            return true;
+        }
+        if (object instanceof EObject) {
+            return ((EObject) object).eClass().getEPackage() == modelPackage;
+        }
+        return false;
+    }
+
+    /**
+     * The switch that delegates to the <code>createXXX</code> methods.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected StrategyEngineCoreSwitch<Adapter> modelSwitch = new StrategyEngineCoreSwitch<Adapter>() {
+        @Override
+        public Adapter caseStrategyEngine(StrategyEngine object) {
+            return createStrategyEngineAdapter();
+        }
+
+        @Override
+        public Adapter caseStrategyEngineConnection(
+                StrategyEngineConnection object) {
+            return createStrategyEngineConnectionAdapter();
+        }
+
+        @Override
+        public Adapter caseStrategy(Strategy object) {
+            return createStrategyAdapter();
+        }
+
+        @Override
+        public Adapter caseDeployedStrategy(DeployedStrategy object) {
+            return createDeployedStrategyAdapter();
+        }
+
+        @Override
+        public Adapter caseStringToStringMapEntry(
+                Map.Entry<String, String> object) {
+            return createStringToStringMapEntryAdapter();
+        }
+
+        @Override
+        public Adapter defaultCase(EObject object) {
+            return createEObjectAdapter();
+        }
+    };
+
+    /**
+     * Creates an adapter for the <code>target</code>.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @param target the object to adapt.
+     * @return the adapter for the <code>target</code>.
+     * @generated
+     */
+    @Override
+    public Adapter createAdapter(Notifier target) {
+        return modelSwitch.doSwitch((EObject) target);
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link org.marketcetera.photon.strategy.engine.model.core.StrategyEngine <em>Strategy Engine</em>}'.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null so that we can easily ignore cases;
+     * it's useful to ignore a case when inheritance will catch all the cases anyway.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @see org.marketcetera.photon.strategy.engine.model.core.StrategyEngine
+     * @generated
+     */
+    public Adapter createStrategyEngineAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link org.marketcetera.photon.strategy.engine.model.core.StrategyEngineConnection <em>Strategy Engine Connection</em>}'.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null so that we can easily ignore cases;
+     * it's useful to ignore a case when inheritance will catch all the cases anyway.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @see org.marketcetera.photon.strategy.engine.model.core.StrategyEngineConnection
+     * @generated
+     */
+    public Adapter createStrategyEngineConnectionAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link org.marketcetera.photon.strategy.engine.model.core.Strategy <em>Strategy</em>}'.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null so that we can easily ignore cases;
+     * it's useful to ignore a case when inheritance will catch all the cases anyway.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @see org.marketcetera.photon.strategy.engine.model.core.Strategy
+     * @generated
+     */
+    public Adapter createStrategyAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link org.marketcetera.photon.strategy.engine.model.core.DeployedStrategy <em>Deployed Strategy</em>}'.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null so that we can easily ignore cases;
+     * it's useful to ignore a case when inheritance will catch all the cases anyway.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @see org.marketcetera.photon.strategy.engine.model.core.DeployedStrategy
+     * @generated
+     */
+    public Adapter createDeployedStrategyAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link java.util.Map.Entry <em>String To String Map Entry</em>}'.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null so that we can easily ignore cases;
+     * it's useful to ignore a case when inheritance will catch all the cases anyway.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @see java.util.Map.Entry
+     * @generated
+     */
+    public Adapter createStringToStringMapEntryAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for the default case.
+     * <!-- begin-user-doc -->
+     * This default implementation returns null.
+     * <!-- end-user-doc -->
+     * @return the new adapter.
+     * @generated
+     */
+    public Adapter createEObjectAdapter() {
+        return null;
+    }
+
+} //StrategyEngineCoreAdapterFactory
