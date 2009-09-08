7f8ec6414bc9a1c2512f489e94aa0efac4f13d8c
Will Horn <will@marketcetera.com>
2009-09-08 17:25:44 +0000

    remove unused branches
    git-svn-id: file:///space/source.marketcetera.org/root/branches/will@10720 501a4a96-914b-0410-8454-f5988a4ca596

--- eclipse35/source/core/src/main/java/quickfix/spring/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Spring integration classes for quickfix.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package quickfix.spring;
--- strategy-engine-review/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/marketdata/OptionInfoComponent.java
+++
@@ -1,5 +0,0 @@
-package org.marketcetera.photon.ui.marketdata;
-
-public enum OptionInfoComponent {
-	STRIKE_INFO, CALL_MARKET_DATA, PUT_MARKET_DATA, CALL_EXTRA_INFO, PUT_EXTRA_INFO
-}
