21a83fe42b726a647c88f8227805fb5c47c74c0e
Will Horn <will@marketcetera.com>
2009-09-08 19:16:20 +0000

    core model changes
    git-svn-id: file:///space/source.marketcetera.org@10725 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/branches/will/strategy-engine-review/source/photon/plugins/org.marketcetera.photon.strategy.engine.tests/src/test/java/org/marketcetera/photon/strategy/engine/StrategyEnginesCoreTest.java
@@ -0,0 +1,21 @@
+package org.marketcetera.photon.strategy.engine;
+
+import org.junit.Test;
+import org.marketcetera.photon.test.OSGITestUtil;
+
+/* $License$ */
+
+/**
+ * Tests {@link StrategyEnginesCore}.
+ * 
+ * @author <a href="mailto:will@marketcetera.com">Will Horn</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class StrategyEnginesCoreTest {
+
+    @Test
+    public void testBundle() {
+        OSGITestUtil.assertBundle(StrategyEnginesCore.PLUGIN_ID);
+    }
+}
