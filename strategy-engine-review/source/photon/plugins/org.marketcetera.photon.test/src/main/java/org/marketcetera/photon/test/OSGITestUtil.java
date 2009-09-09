8f610cb3125111fecda6d38a42e19019f70d9d3d
Will Horn <will@marketcetera.com>
2009-09-09 19:55:09 +0000

    test plugin changes
    git-svn-id: file:///space/source.marketcetera.org/root/branches/will@10736 501a4a96-914b-0410-8454-f5988a4ca596

--- strategy-engine-review/source/photon/plugins/org.marketcetera.photon.test/src/main/java/org/marketcetera/photon/test/OSGITestUtil.java
+++ strategy-engine-review/source/photon/plugins/org.marketcetera.photon.test/src/main/java/org/marketcetera/photon/test/OSGITestUtil.java
@@ -1,8 +1,13 @@
 package org.marketcetera.photon.test;
 
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.Hashtable;
 
 import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
@@ -20,23 +25,59 @@ import com.google.common.collect.ImmutableMap;
 public class OSGITestUtil {
 
     /**
-     * Register the provided service with maxiumum priority.
+     * Register the provided service with maximum rank.
      * 
      * @param clazz
-     *            The class name under which the service can be located.
+     *            the class name under which the service can be located
      * @param service
-     *            The service object or a ServiceFactory object.
+     *            the service object or a ServiceFactory object
      * @return the ServiceRegistration that can be used to unregister the
      *         service
      */
-    public static ServiceRegistration registerMockService(String clazz,
+    public static ServiceRegistration registerMockService(Class<?> clazz,
             Object service) {
+        return registerMockService(clazz, service, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Register the provided service with the specified rank.
+     * 
+     * @param clazz
+     *            the class name under which the service can be located
+     * @param service
+     *            the service object or a ServiceFactory object
+     * @param rank
+     *            the rank for the service
+     * @return the ServiceRegistration that can be used to unregister the
+     *         service
+     */
+    public static ServiceRegistration registerMockService(Class<?> clazz,
+            Object service, int rank) {
+        return getTestBundleContext().registerService(
+                clazz.getName(),
+                service,
+                new Hashtable<String, Integer>(ImmutableMap.of(
+                        Constants.SERVICE_RANKING, rank)));
+    }
+
+    /**
+     * Returns the bundle context for the test bundle.
+     * 
+     * @return the bundle context for the test bundle
+     */
+    public static BundleContext getTestBundleContext() {
         return Platform.getBundle("org.marketcetera.photon.test")
-                .getBundleContext().registerService(
-                        clazz,
-                        service,
-                        new Hashtable<String, Integer>(ImmutableMap.of(
-                                Constants.SERVICE_RANKING, Integer.MAX_VALUE)));
+                .getBundleContext();
+    }
+
+    /**
+     * Asserts that the bundle exists.
+     * 
+     * @param id
+     *            the bundle id
+     */
+    public static void assertBundle(String id) {
+        assertThat(Platform.getBundle(id), not(nullValue()));
     }
 
     private OSGITestUtil() {
