5005063664ba707448665f7755598d60bb5802d5
Colin DuPlantis <colind@marketcetera.com>
2009-10-28 17:14:22 +0000

    Adds OSI-option from string utility - reviewed as ENG-509 by Anshul
    git-svn-id: file:///space/source.marketcetera.org@10837 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/core/src/main/java/org/marketcetera/options/OptionUtils.java
+++ root/trunk/source/core/src/main/java/org/marketcetera/options/OptionUtils.java
@@ -1,8 +1,13 @@
 package org.marketcetera.options;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.regex.Pattern;
+
+import org.marketcetera.trade.Option;
+import org.marketcetera.trade.OptionType;
 
 public class OptionUtils {
 
@@ -31,4 +36,102 @@ public class OptionUtils {
 		} 
 		return candidate;
 	}
+	/**
+	 * Gets the <code>OptionType</code> value for the given character
+	 * interpreted as an OSI-compliant symbol value.
+	 *
+	 * @param inOsiChar a <code>char</code> value
+	 * @return an <code>OptionType</code> value corresponding with the given
+	 *  <code>character</code> or {@link OptionType#Unknown}
+	 */
+	public static OptionType getOptionTypeForOSICharacter(char inOsiChar)
+	{
+	    switch(inOsiChar) {
+	        case 'P':
+	            return OptionType.Put;
+	        case 'C':
+	            return OptionType.Call;
+	        default:
+	            return OptionType.Unknown;
+	    }
+	}
+    /**
+     * Gets an <code>Option</code> from the given full symbol if the given symbol
+     * complies with the option format set out in the
+     * <a href="http://www.theocc.com/initiatives/symbology/default.jsp">Option Symbology Initiative</a>.
+     *
+     * @param inFullSymbol a <code>String</code> value
+     * @return an <code>Option</code> value compliant with the OSI
+     * @throws IllegalArgumentException if the given full symbol is not OSI-compliant
+     */
+    public static Option getOsiOptionFromString(String inFullSymbol)
+    {
+        // this is basic check for the symbol to see if syntactically conforms to the OSI standard - it does not check
+        //  for valid dates, for example, just that the right type of character is at the right place in the symbol
+        if(OSI_SYMBOL_PATTERN.matcher(inFullSymbol).matches()) {
+            // we now know that the symbol is 21 characters long and can be split into sensical pieces
+            String symbol = inFullSymbol.substring(0,
+                                                   6);
+            // note that the Option object expects a four-character year.  the OSI standard doesn't specify how two-character
+            //  years are to be interpreted.
+            String expiryYear = String.valueOf(getFullYear(Integer.parseInt(inFullSymbol.substring(6,
+                                                                                                   8))));
+            String expiryMonth = inFullSymbol.substring(8,
+                                                        10);
+            String expiryDay = inFullSymbol.substring(10,
+                                                      12);
+            OptionType type = getOptionTypeForOSICharacter(inFullSymbol.charAt(12));
+            BigDecimal strikeWhole = new BigDecimal(inFullSymbol.substring(13,
+                                                                           18));
+            BigDecimal strikeDecimal = new BigDecimal(inFullSymbol.substring(18,
+                                                                             21));
+            BigDecimal strike = new BigDecimal(String.format("%s.%s", //$NON-NLS-1$
+                                                             strikeWhole,
+                                                             strikeDecimal));
+            Option osiOption = new Option(symbol,
+                                          String.format("%s%s%s", //$NON-NLS-1$
+                                                        expiryYear,
+                                                        expiryMonth,
+                                                        expiryDay),
+                                          strike,
+                                          type);
+            return osiOption;
+        }
+        throw new IllegalArgumentException();
+    }
+    /**
+     * Returns a full year complete with century from the given
+     * year-only value.
+     *
+     * <p>This method assumes that the given value is a number in the
+     * interval [0,99].  The returned value will be the given year plus
+     * the current century.  If the resulting value < today, the returned
+     * value will be in the next century instead.
+     *   
+     * @param inYear an <code>int</code> containing a number between 0 and 99 inclusive
+     * @return an <code>int</code> value containing a full year representation (century and year)
+     * @throws IllegalArgumentException if the given year is outside of the interval [0,99] 
+     */
+    private static int getFullYear(int inYear)
+    {
+        if(inYear < 0 ||
+           inYear > 99) {
+            throw new IllegalArgumentException();
+        }
+        // determine current century as an int
+        Calendar today = GregorianCalendar.getInstance();
+        int currentCentury = (today.get(Calendar.YEAR) / 100) * 100;
+        int extrapolatedYear = currentCentury + inYear;
+        // completeYear contains an int representing a specific year
+        int currentYear = GregorianCalendar.getInstance().get(Calendar.YEAR);
+        if(extrapolatedYear < currentYear) {
+            extrapolatedYear += 100;
+        }
+        return extrapolatedYear;
+    }
+    /**
+     * this pattern does a basic syntax check of a symbol to see if it complies with the OSI
+     * note that this pattern does not check the expiry date to see if it is completely valid, for example Feb 31st would be valid
+     */
+    private static final Pattern OSI_SYMBOL_PATTERN = Pattern.compile(".{6}\\d{2}(0\\d|1[0-2])(0[1-9]|[12]\\d|3[01])(C|P)\\d{5}\\d{3}"); //$NON-NLS-1$
 }
--- root/trunk/source/core/src/test/java/org/marketcetera/options/OptionUtilsTest.java
+++ root/trunk/source/core/src/test/java/org/marketcetera/options/OptionUtilsTest.java
@@ -1,10 +1,19 @@
 package org.marketcetera.options;
 
+import static org.marketcetera.trade.OptionType.*;
+import java.math.BigDecimal;
+import java.text.DecimalFormatSymbols;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.marketcetera.module.ExpectedFailure;
+import org.marketcetera.trade.Option;
+import org.marketcetera.trade.OptionType;
 
 public class OptionUtilsTest extends TestCase {
 	public void testGetUSEquityOptionExpiration() throws Exception {
@@ -45,6 +54,241 @@ public class OptionUtilsTest extends TestCase {
 		Date date = OptionUtils.getNextUSEquityOptionExpiration();
 //		assertTrue(currentTimeMillis < date.getTime());
 	}
-	
-	
+    /**
+     * Tests {@link OptionUtils#getOsiOptionFromString(String)}.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    @Test
+    public void testOsiOptionFromString()
+            throws Exception
+    {
+        new ExpectedFailure<NullPointerException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString(null);
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("");
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("this-is-not-a-valid-osi-option");
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("21-characters-not-osi");
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("MSFT  991122p12345123"); // valid except 'Put' indicator is lower-case
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("MSFT  991122c12345123"); // valid except 'Call' indicator is lower-case
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("MSFT  991122 12345123"); // option-type missing
+            }
+        };
+        new ExpectedFailure<IllegalArgumentException>(null) {
+            @Override
+            protected void run()
+                    throws Exception
+            {
+                OptionUtils.getOsiOptionFromString("MSFT  991122 12345123"); // option-type missing
+            }
+        };
+        verifyOption(OptionUtils.getOsiOptionFromString("123456301122P12345123"),
+                     "123456",
+                     OptionType.Put,
+                     "20301122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001122P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  000122P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21000122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  000922P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21000922",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001022P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001022",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001222P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001222",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001201P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001201",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  201209P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "20201209",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  201210P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "20201210",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  201219P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "20201219",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001220P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001220",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("123456001220P12345123"),
+                     "123456",
+                     OptionType.Put,
+                     "21001220",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  001229P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "21001229",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  201230P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "20201230",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("MSFT  501231P12345123"),
+                     "MSFT",
+                     OptionType.Put,
+                     "20501231",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("x     301122P12345123"),
+                     "x",
+                     OptionType.Put,
+                     "20301122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("xx    301122P12345123"),
+                     "xx",
+                     OptionType.Put,
+                     "20301122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("ABCDEF301122C12345123"),
+                     "ABCDEF",
+                     OptionType.Call,
+                     "20301122",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("ABCDEF000431C12345123"), // invalid date
+                     "ABCDEF",
+                     OptionType.Call,
+                     "21000431",
+                     new BigDecimal("12345.123"));
+        verifyOption(OptionUtils.getOsiOptionFromString("ABCDEF301122C00000000"),
+                     "ABCDEF",
+                     OptionType.Call,
+                     "20301122",
+                     new BigDecimal("0.0"));
+        verifyOption(OptionUtils.getOsiOptionFromString("ABCDEF301122C99999999"),
+                     "ABCDEF",
+                     OptionType.Call,
+                     "20301122",
+                     new BigDecimal("99999.999"));
+        Locale currentLocale = Locale.getDefault();
+        try {
+            Locale.setDefault(Locale.FRANCE);
+            assertEquals(',',
+                         DecimalFormatSymbols.getInstance().getDecimalSeparator());
+            verifyOption(OptionUtils.getOsiOptionFromString("ABCDEF301122C12345123"),
+                         "ABCDEF",
+                         OptionType.Call,
+                         "20301122",
+                         new BigDecimal("12345.123"));
+        } finally {
+            Locale.setDefault(currentLocale);
+        }
+    }
+    /**
+     * Tests {@link OptionType#getInstanceForOSIValue(char)}.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    @Test
+    public void instanceForOSIValue()
+            throws Exception
+    {
+        for(char badOptionType : new char[] { ' ', 'x', 'c', 'p' }) {
+            assertEquals(Unknown,
+                         OptionUtils.getOptionTypeForOSICharacter(badOptionType));
+        }
+        assertEquals(Put,
+                     OptionUtils.getOptionTypeForOSICharacter('P'));
+        assertEquals(Call,
+                     OptionUtils.getOptionTypeForOSICharacter('C'));
+    }
+    /**
+     * Verifies that the given <code>Option</code> matches the given expected attributes.
+     *
+     * @param inOption inOption an <code>Option</code> value
+     * @param inExpectedSymbol inSymbol a <code>String</code> value
+     * @param inExpectedType inOptionType an <code>OptionType</code> value
+     * @param inExpectedExpiry inExpiry a <code>String</code> value
+     * @param inExpectedStrike inStrike a <code>BigDecimal</code> value
+     * @throws Exception if an unexpected error occurs
+     */
+    private static void verifyOption(Option inOption,
+                                     String inExpectedSymbol,
+                                     OptionType inExpectedType,
+                                     String inExpectedExpiry,
+                                     BigDecimal inExpectedStrike)
+            throws Exception
+    {
+        assertNotNull(inOption);
+        assertEquals(inExpectedSymbol,
+                     inOption.getSymbol());
+        assertEquals(inExpectedType,
+                     inOption.getType());
+        assertEquals(inExpectedExpiry,
+                     inOption.getExpiry());
+        assertEquals(inExpectedStrike,
+                     inOption.getStrikePrice());
+    }
 }
