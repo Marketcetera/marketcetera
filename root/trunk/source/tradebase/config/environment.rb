4b15793d414ea7d97c13c06fca790a3dad82f693
Ken Lim <klim@marketcetera.com>
2009-11-25 23:44:18 +0000

    Update version from 2.0.0 to 2.1.0
    git-svn-id: file:///space/source.marketcetera.org@10903 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/tradebase/config/environment.rb
+++ root/trunk/source/tradebase/config/environment.rb
@@ -32,7 +32,7 @@ RUN_ON_APPLIANCE = false
 JAVA_JMX_CONNECTOR_LIB='lib/marketcetera-jmx-connector-1.0.jar'
 
 # Version of the application
-RELEASE_VERSION = "2.0.0"
+RELEASE_VERSION = "2.1.0"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
