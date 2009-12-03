ce8123197a1f15f8eb7da41b9998c4f685a294d4
Colin DuPlantis <colind@marketcetera.com>
2009-12-03 16:56:17 +0000

    removing obsolete branch
    git-svn-id: file:///space/source.marketcetera.org@10925 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/colin/event_refactoring/source/core/src/main/java/org/marketcetera/event/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Various types of events used by the system.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.event;
--- root/branches/colin/event_refactoring/source/core/src/main/java/org/marketcetera/jms/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Classes used for JMS communications.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.jms;
