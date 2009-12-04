76c4524c97bc3a691533c9ab11a8ab4f842d1bf2
Ken Lim <klim@marketcetera.com>
2009-12-04 20:08:11 +0000

    set svn properties
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@10931 501a4a96-914b-0410-8454-f5988a4ca596

--- source/core/src/main/java/org/marketcetera/marketdata/AssetClass.java
+++ source/core/src/main/java/org/marketcetera/marketdata/AssetClass.java
@@ -6,7 +6,7 @@ import org.marketcetera.util.misc.ClassVersion;
  * The asset class for market data requests.
  *
  * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
- * @version $Id: MarketDataRequest.java 10885 2009-11-17 19:22:56Z klim $
+ * @version $Id$
  * @since 2.0.0
  */
 @ClassVersion("$Id$")
