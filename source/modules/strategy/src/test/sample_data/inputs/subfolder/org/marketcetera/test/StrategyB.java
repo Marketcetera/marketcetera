8a2372414d7c01697a8367f3185dfdddc91b39ef
Colin DuPlantis <colind@marketcetera.com>
2010-05-13 19:47:59 +0000

    Fixes 2 problems with Java strategies: 1) No debug symbols in compiled strategies - this prevented debuggers from working on stategies (EG-921) 2) Java compiler wasn't looking at Strategy Agent strategy classpath (EG-920)
    Affected unit tests pass
    
    
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@10942 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ source/modules/strategy/src/test/sample_data/inputs/subfolder/org/marketcetera/test/StrategyB.java
@@ -0,0 +1,14 @@
+package org.marketcetera.test;
+
+/* $License$ */
+
+/**
+ * Strategy used by another.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class StrategyB
+{
+}
