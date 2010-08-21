918a723f0c1bc9a55bdee397867a2f35362e7b2f
Colin DuPlantis <colind@marketcetera.com>
2010-08-21 23:54:46 +0000

    Adding ExecutionReport tracker for strategies, also modding Bogus to return OptionEvents with OSI-compliant symbols
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@11024 501a4a96-914b-0410-8454-f5988a4ca596

--- source/modules/marketdata/bogus/src/main/java/org/marketcetera/marketdata/bogus/BogusFeedEventTranslator.java
+++ source/modules/marketdata/bogus/src/main/java/org/marketcetera/marketdata/bogus/BogusFeedEventTranslator.java
@@ -1,5 +1,6 @@
 package org.marketcetera.marketdata.bogus;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,7 +9,10 @@ import org.marketcetera.core.ClassVersion;
 import org.marketcetera.core.CoreException;
 import org.marketcetera.event.Event;
 import org.marketcetera.event.EventTranslator;
+import org.marketcetera.event.HasOption;
 import org.marketcetera.event.UnsupportedEventException;
+import org.marketcetera.options.OptionUtils;
+import org.marketcetera.trade.Option;
 import org.marketcetera.util.log.I18NBoundMessage1P;
 
 /* $License$ */
@@ -20,7 +24,7 @@ import org.marketcetera.util.log.I18NBoundMessage1P;
  * @version $Id$
  * @since 0.5.0
  */
-@ClassVersion("$Id$") //$NON-NLS-1$
+@ClassVersion("$Id$")
 public class BogusFeedEventTranslator
         implements EventTranslator, Messages
 {
@@ -37,6 +41,33 @@ public class BogusFeedEventTranslator
                                                                                             null)));
         }
         Event event = (Event)inData;
+        // if the event type is an option, then the symbol has to be specified in OSI format.  this is only
+        //  fair as we are requiring that on the incoming side.  it's not the most efficient place to do this,
+        //  but it needs to be done.
+        if(event instanceof HasOption) {
+            Option option = ((HasOption)event).getInstrument();
+            try {
+                OptionUtils.getOsiOptionFromString(option.getSymbol());
+                // the symbol is already in OSI format, nothing to do 
+            } catch (IllegalArgumentException e) {
+                // the symbol is not in OSI format - this needs to be fixed
+                try {
+                    String symbol = OptionUtils.getOsiSymbolFromOption(option);
+                    // need to rebuild the event with this symbol instead of the one that's there
+                    // note this is awful and needs to be done only until the underlying architecture is fixed for
+                    //  market data requests.  anyway, kids, don't try this at home
+                    Field f = option.getClass().getDeclaredField("mSymbol"); //$NON-NLS-1$
+                    f.setAccessible(true);
+                    f.set(option,
+                          symbol);
+                } catch (IllegalArgumentException e2) {
+                    // bah, give up, not sure what to do here, just let it fall through and return the event as is
+                } catch (Exception e3) {
+                    // something worse has happened
+                    throw new CoreException(e3);
+                }
+            }
+        }
         return Arrays.asList(new Event[] { event } );
     }
     /* (non-Javadoc)
