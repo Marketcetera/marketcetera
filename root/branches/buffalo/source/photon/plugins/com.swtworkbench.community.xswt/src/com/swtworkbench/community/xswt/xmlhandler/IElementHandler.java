04e73de15c9ed729397edd9089120dff88980da6
Colin DuPlantis <colind@marketcetera.com>
2011-01-01 20:54:43 +0000

    Creating server API, removing Photon
    git-svn-id: file:///space/source.marketcetera.org@15799 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/buffalo/source/photon/plugins/com.swtworkbench.community.xswt/src/com/swtworkbench/community/xswt/xmlhandler/IElementHandler.java
+++
@@ -1,8 +0,0 @@
-package com.swtworkbench.community.xswt.xmlhandler;
-
-import com.swtworkbench.community.xswt.XSWTException;
-
-public interface IElementHandler {
-	public boolean handlesChildElements(Object element, IHandlerContext context);
-	public Object handleElement(Object element, Object parent, IHandlerContext context) throws XSWTException;
-}
