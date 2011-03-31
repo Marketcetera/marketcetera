20d844ff9ac056c83a02468fab7585c0e5697ba2
Colin DuPlantis <colind@marketcetera.com>
2011-03-31 19:10:04 +0000

    Adding no-args constructors to ExecutionReport implementation classes to allow their use space objects
    git-svn-id: file:///space/source.marketcetera.org/root/branches/buffalo@15885 501a4a96-914b-0410-8454-f5988a4ca596

--- source/server/src/main/java/org/marketcetera/systemmodel/impl/ReportSummaryImpl.java
+++ source/server/src/main/java/org/marketcetera/systemmodel/impl/ReportSummaryImpl.java
@@ -3,7 +3,6 @@ package org.marketcetera.systemmodel.impl;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import org.marketcetera.systemmodel.Report;
 import org.marketcetera.systemmodel.ReportSummary;
 import org.marketcetera.systemmodel.User;
 import org.marketcetera.trade.*;
@@ -12,7 +11,7 @@ import org.marketcetera.util.misc.ClassVersion;
 /* $License$ */
 
 /**
- *
+ * {@link ReportSummary} implementation.
  *
  * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
  * @version $Id$
@@ -158,25 +157,50 @@ class ReportSummaryImpl
     {
         return owner;
     }
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("ReportSummary [rootID=%s, orderID=%s, origOrderID=%s, securityType=%s, symbol=%s, expiry=%s, strikePrice=%s, optionType=%s, account=%s, side=%s, cumQuantity=%s, avgPrice=%s, lastQuantity=%s, lastPrice=%s, orderStatus=%s, sendingTime=%s, owner=%s]",
+                             rootID,
+                             orderID,
+                             origOrderID,
+                             securityType,
+                             symbol,
+                             expiry,
+                             strikePrice,
+                             optionType,
+                             account,
+                             side,
+                             cumQuantity,
+                             avgPrice,
+                             lastQuantity,
+                             lastPrice,
+                             orderStatus,
+                             sendingTime,
+                             owner);
+    }
     /**
      * Create a new ReportSummaryImpl instance.
      *
-     * @param inOrderId
-     * @param inOrigOrderId
-     * @param inSecurityType
-     * @param inSymbol
-     * @param inExpiry
-     * @param inStrikePrice
-     * @param inOptionType
-     * @param inAccount
-     * @param inSide
-     * @param inCumQuantity
-     * @param inAvgPrice
-     * @param inLastQuantity
-     * @param inLastPrice
-     * @param inOrderStatus
-     * @param inSendingTime
-     * @param inOwner
+     * @param inOrderId an <code>OrderID</code> value
+     * @param inOrigOrderId an <code>OrderID</code> value
+     * @param inSecurityType a <code>SecurityType</code> value
+     * @param inSymbol a <code>String</code> value
+     * @param inExpiry a <code>String</code> value
+     * @param inStrikePrice a <code>BigDecimal</code> value
+     * @param inOptionType an <code>OptionType</code> value
+     * @param inAccount a <code>String</code> value
+     * @param inSide a <code>Side</code> value
+     * @param inCumQuantity a <code>BigDecimal</code> value
+     * @param inAvgPrice a <code>BigDecimal</code> value
+     * @param inLastQuantity a <code>BigDecimal</code> value
+     * @param inLastPrice a <code>BigDecimal</code> value
+     * @param inOrderStatus an <code>OrderStatus</code> value
+     * @param inSendingTime a <code>Date</code> value
+     * @param inOwner a <code>User</code> value
      */
     ReportSummaryImpl(OrderID inOrderId,
                       OrderID inOrigOrderId,
@@ -214,71 +238,95 @@ class ReportSummaryImpl
         owner = inOwner;
     }
     /**
-     * 
+     * Create a new ReportSummaryImpl instance.
+     */
+    @SuppressWarnings("unused")
+    private ReportSummaryImpl()
+    {
+        rootID = null;
+        orderID = null;
+        origOrderID = null;
+        securityType = null;
+        symbol = null;
+        expiry = null;
+        strikePrice = null;
+        optionType = null;
+        account = null;
+        side = null;
+        cumQuantity = null;
+        avgPrice = null;
+        lastQuantity = null;
+        lastPrice = null;
+        orderStatus = null;
+        sendingTime = null;
+        owner = null;
+    }
+    /**
+     * the rootID of the execution report family
      */
     private final OrderID rootID;
     /**
-     * 
+     * the underlying orderID of the execution report
      */
     private final OrderID orderID;
     /**
-     * 
+     * the original orderID of the execution report or <code>null</code>
      */
     private final OrderID origOrderID;
     /**
-     * 
+     * the security type of the execution report
      */
     private final SecurityType securityType;
     /**
-     * 
+     * the symbol of the execution report
      */
     private final String symbol;
     /**
-     * 
+     * the expiry of the execution report
      */
     private final String expiry;
     /**
-     * 
+     * the strike price of the execution report
      */
     private final BigDecimal strikePrice;
     /**
-     * 
+     * the option type of the execution report
      */
     private final OptionType optionType;
     /**
-     * 
+     * the account of the execution report
      */
     private final String account;
     /**
-     * 
+     * the side of the execution report
      */
     private final Side side;
     /**
-     * 
+     * the cumulative quantity of the execution report
      */
     private final BigDecimal cumQuantity;
     /**
-     * 
+     * the average price of the execution report
      */
     private final BigDecimal avgPrice;
     /**
-     * 
+     * the last quantity of the execution report
      */
     private final BigDecimal lastQuantity;
     /**
-     * 
+     * the last price of the execution report
      */
     private final BigDecimal lastPrice;
     /**
-     * 
+     * the order status of the execution report
      */
     private final OrderStatus orderStatus;
     /**
-     * 
+     * the sending time of the execution report
      */
     private final Date sendingTime;
     /**
-     * 
+     * the owner of the execution report
      */
     private final User owner;
 }
