cf985888ee644946a2ccd0e1a6a1f73891d2c535
Colin DuPlantis <colind@marketcetera.com>
2011-07-26 03:17:53 +0000

    Modifying version/build reporting system to cover other components
    git-svn-id: file:///space/source.marketcetera.org@15955 501a4a96-914b-0410-8454-f5988a4ca596

--- root/trunk/source/core/src/test/java/org/marketcetera/core/ApplicationVersionTest.java
+++ root/trunk/source/core/src/test/java/org/marketcetera/core/ApplicationVersionTest.java
@@ -1,6 +1,8 @@
 package org.marketcetera.core;
 
 import org.marketcetera.util.misc.ClassVersion;
+import org.marketcetera.util.test.UnicodeData;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -13,12 +15,45 @@ import static org.junit.Assert.assertEquals;
  * @since 1.5.0
  */
 @ClassVersion("$Id$")
-public class ApplicationVersionTest {
+public class ApplicationVersionTest
+{
+    /**
+     * Run once before all tests.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    @BeforeClass
+    public static void once()
+            throws Exception
+    {
+        LoggerConfiguration.logSetup();
+    }
+    /**
+     * Basic version tests.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
     @Test
-    public void basic() throws Exception {
+    public void basic()
+            throws Exception
+    {
         assertEquals(ApplicationVersion.DEFAULT_VERSION,
-                ApplicationVersion.getVersion());
+                     ApplicationVersion.getVersion());
         assertEquals(ApplicationVersion.DEFAULT_BUILD,
-                ApplicationVersion.getBuildNumber());
+                     ApplicationVersion.getBuildNumber());
+    }
+    /**
+     * Tests that a class with no embedded resource still returns a value.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    @Test
+    public void testsMissingResource()
+            throws Exception
+    {
+        assertEquals(ApplicationVersion.DEFAULT_VERSION,
+                     ApplicationVersion.getVersion(UnicodeData.class));
+        assertEquals(ApplicationVersion.DEFAULT_BUILD,
+                     ApplicationVersion.getBuildNumber(UnicodeData.class));
     }
 }
