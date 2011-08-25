150ff91fcd5d7d70018a7de3170f3f30c39f6a8c
Colin DuPlantis <colind@marketcetera.com>
2011-08-25 17:39:36 +0000

    - Fix for ORS-81 High volume of messages can cause QFX session disconnect
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@16004 501a4a96-914b-0410-8454-f5988a4ca596

--- source/ors/src/main/java/org/marketcetera/ors/QuickFIXApplication.java
+++ source/ors/src/main/java/org/marketcetera/ors/QuickFIXApplication.java
@@ -1,13 +1,13 @@
 package org.marketcetera.ors;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.marketcetera.ors.brokers.Broker;
 import org.marketcetera.ors.brokers.Brokers;
 import org.marketcetera.ors.filters.MessageFilter;
-import org.marketcetera.ors.info.RequestInfo;
-import org.marketcetera.ors.info.RequestInfoImpl;
-import org.marketcetera.ors.info.SessionInfo;
-import org.marketcetera.ors.info.SessionInfoImpl;
-import org.marketcetera.ors.info.SystemInfo;
+import org.marketcetera.ors.info.*;
 import org.marketcetera.quickfix.FIXMessageUtil;
 import org.marketcetera.quickfix.IQuickFIXSender;
 import org.marketcetera.trade.FIXConverter;
@@ -15,22 +15,12 @@ import org.marketcetera.trade.MessageCreationException;
 import org.marketcetera.trade.Originator;
 import org.marketcetera.trade.TradeMessage;
 import org.marketcetera.util.except.I18NException;
+import org.marketcetera.util.log.SLF4JLoggerProxy;
 import org.marketcetera.util.misc.ClassVersion;
 import org.springframework.jms.core.JmsOperations;
-import quickfix.Application;
-import quickfix.DoNotSend;
-import quickfix.FieldNotFound;
-import quickfix.Message;
-import quickfix.SessionID;
-import quickfix.SessionNotFound;
-import quickfix.UnsupportedMessageType;
-import quickfix.field.DeliverToCompID;
-import quickfix.field.MsgType;
-import quickfix.field.OrdStatus;
-import quickfix.field.RefMsgType;
-import quickfix.field.SessionRejectReason;
-import quickfix.field.Text;
-import quickfix.field.TradSesStatus;
+
+import quickfix.*;
+import quickfix.field.*;
 
 /**
  * The QuickFIX/J intermediary, intercepting messages from/to the
@@ -64,6 +54,7 @@ public class QuickFIXApplication
     private final UserManager mUserManager;
     private final JmsOperations mToClientStatus;
     private final JmsOperations mToTradeRecorder;
+    private final BlockingDeque<MessagePackage> messagesToProcess = new LinkedBlockingDeque<MessagePackage>();
 
 
     // CONSTRUCTORS.
@@ -86,6 +77,7 @@ public class QuickFIXApplication
         mUserManager=userManager;
         mToClientStatus=toClientStatus;
         mToTradeRecorder=toTradeRecorder;
+        new MessageProcessor();
     }
 
 
@@ -317,13 +309,9 @@ public class QuickFIXApplication
         (Message msg,
          SessionID session)
     {
-        Broker b=getBrokers().getBroker(session);
-        Messages.QF_FROM_ADMIN.info(getCategory(msg),msg,b);
-        b.logMessage(msg);
-
-        // Send message to client.
-
-        sendToClientTrades(true,b,msg,Originator.Broker);
+        messagesToProcess.add(new MessagePackage(msg,
+                                                 MessageType.FROM_ADMIN,
+                                                 session));
     }
 
     @Override
@@ -338,11 +326,9 @@ public class QuickFIXApplication
     }
 
     @Override
-	public void fromApp
-        (Message msg,
-         SessionID session)
-        throws UnsupportedMessageType,
-               FieldNotFound
+	public void fromApp(Message msg,
+	                    SessionID session)
+            throws UnsupportedMessageType, FieldNotFound
     {
         Broker b=getBrokers().getBroker(session);
         Messages.QF_FROM_APP.info(getCategory(msg),msg,b);
@@ -354,45 +340,225 @@ public class QuickFIXApplication
             Messages.QF_DISALLOWED_MESSAGE.info(getCategory(msg));
             throw new UnsupportedMessageType();
         }
-
-        // Report trading session status in a human-readable format.
-
-        if (FIXMessageUtil.isTradingSessionStatus(msg)) {
-            Messages.QF_TRADE_SESSION_STATUS.info
-                (getCategory(msg),b.getFIXDataDictionary().getHumanFieldValue
-                 (TradSesStatus.FIELD,msg.getString(TradSesStatus.FIELD)));
+        messagesToProcess.add(new MessagePackage(msg,
+                                                 MessageType.FROM_APP,
+                                                 session));
+    }
+    /**
+     * Indicates the type of message.
+     *
+     * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+     * @version $Id$
+     * @since $Release$
+     */
+    @ClassVersion("$Id$")
+    private enum MessageType
+    {
+        FROM_ADMIN,
+        FROM_APP
+    }
+    /**
+     * Encapsulates a message to be processed.
+     *
+     * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+     * @version $Id$
+     * @since $Release$
+     */
+    @ClassVersion("$Id$")
+    private static class MessagePackage
+    {
+        /* (non-Javadoc)
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + (int) (id ^ (id >>> 32));
+            return result;
         }
-
-        // Send message to client.
-
-        sendToClientTrades(false,b,msg,Originator.Broker);
-
-        // OpenFIX certification: we reject all DeliverToCompID since
-        // we don't redeliver.
-
-        if (msg.getHeader().isSetField(DeliverToCompID.FIELD)) {
+        /* (non-Javadoc)
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof MessagePackage)) {
+                return false;
+            }
+            MessagePackage other = (MessagePackage) obj;
+            if (id != other.id) {
+                return false;
+            }
+            return true;
+        }
+      /**
+         * Create a new MessagePackage instance.
+         *
+         * @param inMessage a <code>Message</code> value
+         * @param inMessageType a <code>MessageType</code> value
+         * @param inSessionId a <code>SessionID</code> value
+         */
+        private MessagePackage(Message inMessage,
+                               MessageType inMessageType,
+                               SessionID inSessionId)
+        {
+            message = inMessage;
+            messageType = inMessageType;
+            sessionId = inSessionId;
+        }
+        /**
+         * Gets the <code>Message</code> value.
+         *
+         * @return a <code>Message</code> value
+         */
+        private Message getMessage()
+        {
+            return message;
+        }
+        /**
+         * Gets the <code>MessageType</code> value.
+         *
+         * @return a <code>MessageType</code> value
+         */
+        private MessageType getMessageType()
+        {
+            return messageType;
+        }
+        /**
+         * Gets the <code>SessionID</code> value.
+         *
+         * @return a <code>SessionID</code> value
+         */
+        private SessionID getSessionId()
+        {
+            return sessionId;
+        }
+        /**
+         * message value
+         */
+        private final Message message;
+        /**
+         * message type value
+         */
+        private final MessageType messageType;
+        /**
+         * session ID value
+         */
+        private final SessionID sessionId;
+        /**
+         * message counter
+         */
+        private final long id = counter.incrementAndGet();
+        /**
+         * counter used to uniquely and sequentially identify messages
+         */
+        private static final AtomicLong counter = new AtomicLong(0);
+    }
+    /**
+     * Processes incoming messages.
+     *
+     * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+     * @version $Id$
+     * @since $Release$
+     */
+    @ClassVersion("$Id$")
+    private class MessageProcessor
+            implements Runnable
+    {
+        /* (non-Javadoc)
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run()
+        {
             try {
-                Message reject=b.getFIXMessageFactory().createSessionReject
-                    (msg,SessionRejectReason.COMPID_PROBLEM);
-                reject.setString
-                    (Text.FIELD,Messages.QF_COMP_ID_REJECT.getText
-                     (msg.getHeader().getString(DeliverToCompID.FIELD)));
-                getSender().sendToTarget(reject,session);
-            } catch (SessionNotFound ex) {
-                Messages.QF_COMP_ID_REJECT_FAILED.error
-                    (getCategory(msg),ex,b.toString());
-            }
-            return;
+                while(true) {
+                    MessagePackage message = messagesToProcess.take();
+                    switch(message.getMessageType()) {
+                        case FROM_ADMIN: {
+                            SessionID session = message.getSessionId();
+                            Message msg = message.getMessage();
+                            Broker b = getBrokers().getBroker(session);
+                            Messages.QF_FROM_ADMIN.info(getCategory(msg),
+                                                        msg,
+                                                        b);
+                            b.logMessage(msg);
+                            // Send message to client.
+                            sendToClientTrades(true,
+                                               b,
+                                               msg,
+                                               Originator.Broker);
+                            break;
+                        }
+                        case FROM_APP: {
+                            SessionID session = message.getSessionId();
+                            Message msg = message.getMessage();
+                            Broker b = getBrokers().getBroker(session);
+                            try {
+                                // Report trading session status in a human-readable format.
+                                if (FIXMessageUtil.isTradingSessionStatus(msg)) {
+                                    Messages.QF_TRADE_SESSION_STATUS.info(getCategory(msg),
+                                                                          b.getFIXDataDictionary().getHumanFieldValue(TradSesStatus.FIELD,
+                                                                                                                      msg.getString(TradSesStatus.FIELD)));
+                                }
+                                // Send message to client.
+                                sendToClientTrades(false,b,msg,Originator.Broker);
+                                // OpenFIX certification: we reject all DeliverToCompID since we don't redeliver.
+                                if (msg.getHeader().isSetField(DeliverToCompID.FIELD)) {
+                                    try {
+                                        Message reject = b.getFIXMessageFactory().createSessionReject(msg,
+                                                                                                      SessionRejectReason.COMPID_PROBLEM);
+                                        reject.setString(Text.FIELD,
+                                                         Messages.QF_COMP_ID_REJECT.getText(msg.getHeader().getString(DeliverToCompID.FIELD)));
+                                        getSender().sendToTarget(reject,
+                                                                 session);
+                                    } catch (SessionNotFound ex) {
+                                        Messages.QF_COMP_ID_REJECT_FAILED.error(getCategory(msg),
+                                                                                ex,
+                                                                                b.toString());
+                                    }
+                                    break;
+                                }
+                                // Record filled (partially or totally) execution reports.
+                                if (FIXMessageUtil.isExecutionReport(msg)) {
+                                    char ordStatus=msg.getChar(OrdStatus.FIELD);
+                                    if ((ordStatus==OrdStatus.FILLED) ||
+                                        (ordStatus==OrdStatus.PARTIALLY_FILLED)) {
+                                        sendTradeRecord(msg);
+                                    }
+                                }
+                            } catch (FieldNotFound e) {
+                                SLF4JLoggerProxy.error(QuickFIXApplication.class,
+                                                       e);
+                            }
+                            break;
+                        }
+                        default:
+                            throw new UnsupportedOperationException();
+                    }
+                }
+            } catch (InterruptedException ignored) {}
         }
-
-        // Record filled (partially or totally) execution reports.
-
-        if (FIXMessageUtil.isExecutionReport(msg)) {
-            char ordStatus=msg.getChar(OrdStatus.FIELD);
-            if ((ordStatus==OrdStatus.FILLED) ||
-                (ordStatus==OrdStatus.PARTIALLY_FILLED)) {
-                sendTradeRecord(msg);
-            }
+        /**
+         * Create a new MessageProcessor instance.
+         */
+        private MessageProcessor()
+        {
+            thread = new Thread(this,
+                                "QuickFIXApplication Message Processing Tread"); //$NON-NLS-1$
+            thread.start();
         }
+        /**
+         * thread on which the messages are processed
+         */
+        private final Thread thread;
     }
 }
