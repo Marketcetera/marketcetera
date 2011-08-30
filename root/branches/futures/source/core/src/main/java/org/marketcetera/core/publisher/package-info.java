9b42682baf166040b1742fba53b3b8f5e5bc75ca
Colin DuPlantis <colind@marketcetera.com>
2011-08-30 15:25:54 +0000

    Removing unused branch
    git-svn-id: file:///space/source.marketcetera.org@16013 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/futures/source/core/src/main/java/org/marketcetera/core/publisher/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Infrastructure classes for publishing and subscribing to events.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.core.publisher;
