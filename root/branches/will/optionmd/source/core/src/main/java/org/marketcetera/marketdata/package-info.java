dcff5f9973a95791ac123215f6cc5679dd6e26c7
Colin DuPlantis <colind@marketcetera.com>
2011-08-30 15:54:10 +0000

    Removing unused branch
    git-svn-id: file:///space/source.marketcetera.org@16015 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/will/optionmd/source/core/src/main/java/org/marketcetera/marketdata/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Infrastructure classes for market data feeds.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.marketdata;
--- root/branches/will/optionmd/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/fixmessagedetail/IFIXMessageDetail.java
+++
@@ -1,7 +0,0 @@
-package org.marketcetera.photon.views.fixmessagedetail;
-
-import quickfix.Message;
-
-public interface IFIXMessageDetail {
-	void showMessage(Message fixMessage);
-}
