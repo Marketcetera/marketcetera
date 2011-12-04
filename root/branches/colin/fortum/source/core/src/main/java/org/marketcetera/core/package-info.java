77e4e1d1203f3c4638b268cef1dc8164a1b01ac4
Colin DuPlantis <colind@marketcetera.com>
2011-12-04 03:02:13 +0000

    removing unneeded branch
    git-svn-id: file:///space/source.marketcetera.org@16045 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/colin/fortum/source/core/src/main/java/org/marketcetera/core/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * General utility classes used by the platform.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.core;
