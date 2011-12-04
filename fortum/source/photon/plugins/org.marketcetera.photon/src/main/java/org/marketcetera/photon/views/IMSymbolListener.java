67ce424e7d9f08b514c39138d15722bd6452acbe
Colin DuPlantis <colind@marketcetera.com>
2011-12-04 03:02:13 +0000

    removing unneeded branch
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16045 501a4a96-914b-0410-8454-f5988a4ca596

--- fortum/source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IMSymbolListener.java
+++
@@ -1,9 +0,0 @@
-package org.marketcetera.photon.views;
-
-import org.marketcetera.trade.Equity;
-
-public interface IMSymbolListener {
-	void onAssertSymbol(Equity symbol);
-	
-	boolean isListeningSymbol(Equity symbol);
-}
