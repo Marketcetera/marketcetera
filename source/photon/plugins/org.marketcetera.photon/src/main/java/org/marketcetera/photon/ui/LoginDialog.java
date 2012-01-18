309c1c622fa2aa3bc0aeeaa046bf9a510557fbc4
Colin DuPlantis <colind@marketcetera.com>
2012-01-18 22:05:45 +0000

    Correct web links for Photon doc
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@16059 501a4a96-914b-0410-8454-f5988a4ca596

--- source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/LoginDialog.java
+++ source/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/ui/LoginDialog.java
@@ -60,7 +60,7 @@ public class LoginDialog
     implements Messages
 {
 
-    private static final String ORS_URL = "href=\"http://www.marketcetera.com/masha/docs?version=2.1.4&qualifier=authentication\""; //$NON-NLS-1$
+    private static final String ORS_URL = "href=\"http://www.marketcetera.org/confluence/display/ORS/Authentication\""; //$NON-NLS-1$
 
     private Combo userIdText;
 
