feff28a6ec1c43f6c652ccd7c008477f35c7c299
Colin DuPlantis <colind@marketcetera.com>
2012-06-25 23:50:05 +0000

    Fix for PN-448 Market Depth View orphans events under load
    git-svn-id: file:///space/source.marketcetera.org@16106 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/trunk/source/photon/plugins/org.marketcetera.photon.marketdata/src/main/java/org/marketcetera/photon/internal/marketdata/LockableEList.java
@@ -0,0 +1,35 @@
+package org.marketcetera.photon.internal.marketdata;
+
+import java.util.concurrent.Callable;
+
+import org.eclipse.emf.common.util.EList;
+import org.marketcetera.util.misc.ClassVersion;
+
+/* $License$ */
+
+/**
+ * Provides an interface to an <code>EList</code> object that permits locked read and write operations.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@ClassVersion("$Id$")
+public interface LockableEList<E>
+        extends EList<E>
+{
+    /**
+     * Executes the given operation as a locked read. 
+     *
+     * @param inOperation a <code>Callable&lt;Tgt;</code> value
+     * @return a <code>T</code> value
+     */
+    public <T> T doReadOperation(Callable<T> inOperation);
+    /**
+     * Executes the given operation as a locked write. 
+     *
+     * @param inOperation a <code>Callable&lt;Tgt;</code> value
+     * @return a <code>T</code> value
+     */
+    public <T> T doWriteOperation(Callable<T> inOperation);
+}
