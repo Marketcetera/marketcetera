85b0537e2c59140ad54f325dcf8f7f78a9f8de01
Brian Topping <brian@mauswerks.com>
2012-06-29 04:42:10 +0000

    Start on openjpa-dao
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.0@16124 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ dao/openjpa-dao/src/main/java/org/marketcetera/dao/openjpa/AuthorityDaoImpl.java
@@ -0,0 +1,50 @@
+package org.marketcetera.dao.openjpa;
+
+import java.util.List;
+
+import org.marketcetera.dao.AuthorityDao;
+import org.marketcetera.systemmodel.Authority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:topping@codehaus.org">Brian Topping</a>
+ * @version $Id$
+ * @date 6/29/12 12:38 AM
+ */
+
+public class AuthorityDaoImpl implements AuthorityDao {
+    @SuppressWarnings("unused")
+    private static final Logger log = LoggerFactory.getLogger(AuthorityDaoImpl.class);
+
+
+    @Override
+    public void add(Authority inData) {
+        log.trace("Entering add");
+
+    }
+
+    @Override
+    public void save(Authority inData) {
+        log.trace("Entering save");
+
+    }
+
+    @Override
+    public Authority getByName(String inName) {
+        log.trace("Entering getByName");
+        return null;
+    }
+
+    @Override
+    public Authority getById(long inId) {
+        log.trace("Entering getById");
+        return null;
+    }
+
+    @Override
+    public List<Authority> getAll() {
+        log.trace("Entering getAll");
+        return null;
+    }
+}
