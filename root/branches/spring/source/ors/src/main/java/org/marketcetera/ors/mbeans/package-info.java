fcd3e8ebe8a40619b8da806d329a70e8d00ef05c
Colin DuPlantis <colind@marketcetera.com>
2012-08-05 23:40:36 +0000

    Removing unneeded branch
    git-svn-id: file:///space/source.marketcetera.org@16186 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/spring/source/ors/src/main/java/org/marketcetera/ors/mbeans/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Beans to enable management of ORS via JMX.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.ors.mbeans;
