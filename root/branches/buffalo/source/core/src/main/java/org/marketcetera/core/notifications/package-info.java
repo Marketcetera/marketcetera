cb77f6f5e81ba9717b2078945dcc6e9524b2b36d
Colin DuPlantis <colind@marketcetera.com>
2012-08-05 23:41:06 +0000

    Removing unneeded branch
    git-svn-id: file:///space/source.marketcetera.org@16187 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/buffalo/source/core/src/main/java/org/marketcetera/core/notifications/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Infrastructure for publishing notifications.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.core.notifications;
--- root/branches/buffalo/source/tradebase/app/models/posting.rb
+++
@@ -1,9 +0,0 @@
-class Posting < ActiveRecord::Base
-  belongs_to :sub_account
-  belongs_to :journal
-  belongs_to :currency
-
-  # pairID values for types of postings
-  NotionalPairID = 1
-  CommissionsPairID = 2
-end
