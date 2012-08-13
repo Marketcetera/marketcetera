ac48d4e114a1b89d0343f46545e71417501ab817
Colin DuPlantis <colind@marketcetera.com>
2012-08-13 19:28:15 +0000

    Adding missing User-related artifacts
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.0@16192 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ core/util/src/main/java/org/marketcetera/core/util/misc/Initializer.java
@@ -0,0 +1,21 @@
+package org.marketcetera.core.util.misc;
+
+import org.marketcetera.core.attributes.ClassVersion;
+
+/* $License$ */
+
+/**
+ * Performs some initialization functions
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@ClassVersion("$Id$")
+public interface Initializer
+{
+    /**
+     * Performs some initialization.
+     */
+    public void initialize();
+}
