851b30da7a33b7fda1fc8d75a6c01ccbb9134afe
Colin DuPlantis <colind@marketcetera.com>
2012-08-15 01:07:03 +0000

    Initial add of dao support for testing
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.0@16193 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ dao/openjpa-dao/src/test/java/org/marketcetera/dao/impl/DaoTestBase.java
@@ -0,0 +1,51 @@
+package org.marketcetera.dao.impl;
+
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.marketcetera.core.LoggerConfiguration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+/* $License$ */
+
+/**
+ *
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public abstract class DaoTestBase
+{
+    /**
+     * Run once before all tests.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    @BeforeClass
+    public static void once()
+            throws Exception
+    {
+        LoggerConfiguration.logSetup();
+    }
+    /**
+     * 
+     *
+     *
+     * @return
+     */
+    @Configuration
+    public Option[] config()
+    {
+        return new Option[] { karafDistributionConfiguration().frameworkUrl(maven().groupId("org.apache.karaf").
+                                                                           artifactId("apache-karaf").type("zip").versionAsInProject()).karafVersion("2.2.7").name("Apache Karaf")};
+    }
+}
