955398e6dd724de5477819b69651a5c560cc8399
Colin DuPlantis <colind@marketcetera.com>
2012-08-15 01:07:03 +0000

    Initial add of dao support for testing
    git-svn-id: file:///space/source.marketcetera.org@16193 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/branches/3.0.0/dao/openjpa-dao/src/test/java/org/marketcetera/dao/impl/AuthorityDaoTest.java
@@ -0,0 +1,22 @@
+package org.marketcetera.dao.impl;
+
+import org.junit.Test;
+
+/* $License$ */
+
+/**
+ *
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class AuthorityDaoTest
+        extends DaoTestBase
+{
+    @Test
+    public void testOne()
+    {
+        
+    }
+}
