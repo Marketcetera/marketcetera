43128b3df5260a2d3ac46611f611ef196b094193
Brian Topping <brian@mauswerks.com>
2012-08-18 14:57:50 +0000

    Minor changes for dependencies
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.0@16194 501a4a96-914b-0410-8454-f5988a4ca596

--- dao/openjpa-dao/src/test/java/org/marketcetera/dao/impl/DaoTestBase.java
+++ dao/openjpa-dao/src/test/java/org/marketcetera/dao/impl/DaoTestBase.java
@@ -1,8 +1,5 @@
 package org.marketcetera.dao.impl;
 
-import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.CoreOptions.maven;
-
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.marketcetera.core.LoggerConfiguration;
@@ -11,6 +8,9 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.maven;
 
 /* $License$ */
 
@@ -46,6 +46,6 @@ public abstract class DaoTestBase
     public Option[] config()
     {
         return new Option[] { karafDistributionConfiguration().frameworkUrl(maven().groupId("org.apache.karaf").
-                                                                           artifactId("apache-karaf").type("zip").versionAsInProject()).karafVersion("2.2.7").name("Apache Karaf")};
+                                                                           artifactId("apache-karaf").type("zip").versionAsInProject()).karafVersion("3.0.0-SNAPSHOT").name("Apache Karaf")};
     }
 }
