33eed343a075683e0496f2b9b56cb19387f04208
Brian Topping <brian@mauswerks.com>
2012-09-03 13:44:56 +0000

    A few last pieces of the previous checkin.
    git-svn-id: file:///space/source.marketcetera.org@16248 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/3.0.0/core/systemmodel/src/main/java/org/marketcetera/core/systemmodel/RoleFactory.java
+++ root/branches/3.0.0/core/systemmodel/src/main/java/org/marketcetera/core/systemmodel/RoleFactory.java
@@ -5,7 +5,7 @@ package org.marketcetera.core.systemmodel;
 import org.marketcetera.api.dao.Role;
 
 /**
- * Creates <code>Group</code> objects.
+ * Creates <code>Role</code> objects.
  *
  * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
  * @version $Id: RoleFactory.java 82315 2012-03-17 01:58:54Z colin $
@@ -14,16 +14,16 @@ import org.marketcetera.api.dao.Role;
 public interface RoleFactory
 {
     /**
-     * Creates a <code>Group</code> object with the given attributes.
+     * Creates a <code>Role</code> object with the given attributes.
      *
-     * @param inGroupname a <code>String</code> value
-     * @return a <code>Group</code> value
+     * @param inRolename a <code>String</code> value
+     * @return a <code>Role</code> value
      */
-    public Role create(String inGroupname);
+    public Role create(String inRolename);
     /**
-     * Creates a <code>Group</code> object.
+     * Creates a <code>Role</code> object.
      *
-     * @return a <code>Group</code> value
+     * @return a <code>Role</code> value
      */
     public Role create();
 }
