45e8ed9e2b9a53c6e0ece610b184bb742a164387
Colin DuPlantis <colind@marketcetera.com>
2012-10-05 00:46:03 +0000

    Cleaning up UserService and AuthenticationService
    git-svn-id: file:///space/source.marketcetera.org@16302 501a4a96-914b-0410-8454-f5988a4ca596

--- root/branches/3.0.0/webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/systemmodel/UserService.java
+++ root/branches/3.0.0/webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/systemmodel/UserService.java
@@ -2,18 +2,9 @@ package org.marketcetera.webservices.systemmodel;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.apache.cxf.jaxrs.ext.search.SearchContext;
 
 /* $License$ */
 
@@ -33,17 +24,8 @@ public interface UserService
      * @return a <code>WebServicesUser</code> value
      */
     @POST
-    @Consumes({MediaType.APPLICATION_JSON})
-    public WebServicesUser addUserJSON(WebServicesUser inUser);
-    /**
-     * Adds the given user.
-     *
-     * @param inUser a <code>WebServicesUser</code> value
-     * @return a <code>WebServicesUser</code> value
-     */
-    @POST
-    @Consumes({MediaType.APPLICATION_XML})
-    public WebServicesUser addUserXML(WebServicesUser inUser);
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public WebServicesUser addUser(WebServicesUser inUser);
     /**
      * Gets the user with the given id.
      *
@@ -52,36 +34,16 @@ public interface UserService
      */
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public WebServicesUser getUserJSON(@PathParam("id")long inId);
-    /**
-     * Gets the user with the given id.
-     *
-     * @param inId a <code>String</code> value
-     * @return a <code>WebServicesUser</code> value
-     */
-    @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML})
-    public WebServicesUser getUserXML(@PathParam("id")long inId);
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public WebServicesUser getUser(@PathParam("id")long inId);
     /**
      * Gets all users.
      *
      * @return a <code>List&lt;WebServicesUser&gt;</code> value
-     * @param context
      */
     @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public List<WebServicesUser> getUsersJSON(@Context SearchContext context);
-    /**
-     * Gets all users.
-     *
-     * @return a <code>List&lt;WebServicesUser&gt;</code> value
-     * @param context
-     */
-    @GET
-    @Produces({MediaType.APPLICATION_XML})
-    public List<WebServicesUser> getUsersXML(@Context SearchContext context);
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public List<WebServicesUser> getUsers();
     /**
      * Deletes the user with the given id.
      *
@@ -98,15 +60,6 @@ public interface UserService
      * @return a <code>Response</code> value
      */
     @PUT
-    @Consumes({MediaType.APPLICATION_JSON})
-    public Response updateUserJSON(WebServicesUser inUser);
-    /**
-     * Updates the given user.
-     *
-     * @param inUser a <code>WebServicesUser</code> value
-     * @return a <code>Response</code> value
-     */
-    @PUT
-    @Consumes({MediaType.APPLICATION_XML})
-    public Response updateUserXML(WebServicesUser inUser);
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response updateUser(WebServicesUser inUser);
 }
--- root/branches/3.0.0/webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/systemmodel/impl/UserServiceImpl.java
+++ root/branches/3.0.0/webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/systemmodel/impl/UserServiceImpl.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
-import org.apache.cxf.jaxrs.ext.search.SearchCondition;
-import org.apache.cxf.jaxrs.ext.search.SearchContext;
+
 import org.marketcetera.api.dao.UserDao;
 import org.marketcetera.api.dao.UserFactory;
 import org.marketcetera.api.security.User;
@@ -25,45 +24,69 @@ public class UserServiceImpl
         implements UserService
 {
     /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#addUser(java.lang.String)
+     * @see org.marketcetera.webservices.systemmodel.UserService#addUser(org.marketcetera.webservices.systemmodel.WebServicesUser)
      */
     @Override
-    public WebServicesUser addUserJSON(WebServicesUser inUser)
+    public WebServicesUser addUser(WebServicesUser inUser)
     {
-        SLF4JLoggerProxy.debug(UserServiceImpl.class,
-                               "UserService addUserJSON invoked with user {}", //$NON-NLS-1$
-                               inUser);
-        return doAddUser(inUser);
+        try {
+            User persistableUser = userFactory.create(inUser);
+            userDao.add(persistableUser);
+            return new WebServicesUser(persistableUser);
+        } catch (RuntimeException e) {
+            SLF4JLoggerProxy.warn(UserServiceImpl.class,
+                                  e);
+            throw e;
+        }
     }
     /* (non-Javadoc)
      * @see org.marketcetera.webservices.systemmodel.UserService#getUser(long)
      */
     @Override
-    public WebServicesUser getUserJSON(long inId)
+    public WebServicesUser getUser(long inId)
     {
-        SLF4JLoggerProxy.debug(UserServiceImpl.class,
-                               "UserService getUserJSON invoked with id {}", //$NON-NLS-1$
-                               inId);
-        return new WebServicesUser(doGetUser(inId));
+        User user = userDao.getById(inId);
+        if(user == null) {
+            return null;
+        }
+        return new WebServicesUser(user);
     }
     /* (non-Javadoc)
      * @see org.marketcetera.webservices.systemmodel.UserService#getUsers()
      */
     @Override
-    public List<WebServicesUser> getUsersJSON(SearchContext context)
+    public List<WebServicesUser> getUsers()
     {
-        SLF4JLoggerProxy.debug(UserServiceImpl.class, "UserService getUsersJSON invoked"); //$NON-NLS-1$
-        return doGetUsers(context);
+        List<WebServicesUser> decoratedUsers = new ArrayList<WebServicesUser>();
+        for(User user : userDao.getAll()) {
+            decoratedUsers.add(new WebServicesUser(user));
+        }
+        return decoratedUsers;
+    }
+    /* (non-Javadoc)
+     * @see org.marketcetera.webservices.systemmodel.UserService#updateUser(org.marketcetera.webservices.systemmodel.WebServicesUser)
+     */
+    @Override
+    public Response updateUser(WebServicesUser inUser)
+    {
+        Response response;
+        try {
+            User persistableUser = userFactory.create(inUser);
+            userDao.save(persistableUser);
+            response = Response.ok().build();
+        } catch (RuntimeException e) {
+            SLF4JLoggerProxy.error(this,
+                                   e);
+            response = Response.serverError().build();
+        }
+        return response;
     }
     /* (non-Javadoc)
      * @see org.marketcetera.webservices.systemmodel.UserService#deleteUser(long)
      */
     @Override
-    public Response deleteUser(final long inId)
+    public Response deleteUser(long inId)
     {
-        SLF4JLoggerProxy.debug(UserServiceImpl.class,
-                               "UserService deleteUser invoked with id {}", //$NON-NLS-1$
-                               inId);
         Response response;
         try {
             User user = userDao.getById(inId);
@@ -72,53 +95,9 @@ public class UserServiceImpl
         } catch (RuntimeException e) {
             SLF4JLoggerProxy.error(this,
                                    e);
-            response = Response.serverError()
-                               .entity(e.getMessage()).build();
+            response = Response.serverError().build();
         }
         return response;
-    }
-    /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#addUserXML(org.marketcetera.api.dao.User)
-     */
-    @Override
-    public WebServicesUser addUserXML(WebServicesUser inUser)
-    {
-        return doAddUser(inUser);
-    }
-    /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#getUserXML(long)
-     */
-    @Override
-    public WebServicesUser getUserXML(long inId)
-    {
-        SLF4JLoggerProxy.debug(UserServiceImpl.class,
-                               "UserService getUserXML invoked with id {}", //$NON-NLS-1$
-                               inId);
-        return doGetUser(inId);
-    }
-    /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#getUsersXML()
-     */
-    @Override
-    public List<WebServicesUser> getUsersXML(SearchContext context)
-    {
-        return doGetUsers(context);
-    }
-    /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#updateUserJSON(org.marketcetera.webservices.systemmodel.WebServicesUser)
-     */
-    @Override
-    public Response updateUserJSON(WebServicesUser inUser)
-    {
-        return updateUser(inUser);
-    }
-    /* (non-Javadoc)
-     * @see org.marketcetera.webservices.systemmodel.UserService#updateUserXML(org.marketcetera.webservices.systemmodel.WebServicesUser)
-     */
-    @Override
-    public Response updateUserXML(WebServicesUser inUser)
-    {
-        return updateUser(inUser);
     }
     /**
      * Sets the userFactory value.
@@ -136,74 +115,6 @@ public class UserServiceImpl
      */
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
-    }
-    /**
-     * Updates the given <code>User</code>.
-     *
-     * @param inUser a <code>WebServicesUser</code> value
-     * @return a <code>Response</code> value
-     */
-    private Response updateUser(WebServicesUser inUser)
-    {
-        Response response;
-        try {
-            User persistableUser = userFactory.create(inUser);
-            userDao.save(persistableUser);
-            response = Response.ok().build();
-        } catch (RuntimeException e) {
-            SLF4JLoggerProxy.error(this,
-                                   e);
-            response = Response.serverError().build();
-        }
-        return response;
-    }
-    /**
-     * Executes the retrieval of the the <code>User</code> object associated with the given id.
-     *
-     * @param inId a <code>long</code> value
-     * @return a <code>WebServicesUser</code> value
-     */
-    private WebServicesUser doGetUser(long inId)
-    {
-        User user = userDao.getById(inId);
-        if(user == null) {
-            return null;
-        }
-        return new WebServicesUser(user);
-    }
-    /**
-     * Executes the retrieval of all existing <code>User</code> objects.
-     *
-     * @return a <code>List&lt;WebServicesUser&gt;</code> value
-     * @param context
-     */
-    private List<WebServicesUser> doGetUsers(SearchContext context)
-    {
-        SearchCondition sc = context.getCondition(WebServicesUser.class);
-
-        List<WebServicesUser> decoratedUsers = new ArrayList<WebServicesUser>();
-        for(User user : userDao.getAll(sc)) {
-            decoratedUsers.add(new WebServicesUser(user));
-        }
-        return decoratedUsers;
-    }
-    /**
-     * Executes the addition of the given <code>User</code> object.
-     *
-     * @param inUser a <code>WebServicesUser</code> value
-     * @return a <code>WebServicesUser</code> value
-     */
-    private WebServicesUser doAddUser(WebServicesUser inUser)
-    {
-        try {
-            User persistableUser = userFactory.create(inUser);
-            userDao.add(persistableUser);
-            return new WebServicesUser(persistableUser);
-        } catch (RuntimeException e) {
-            SLF4JLoggerProxy.warn(UserServiceImpl.class,
-                                  e);
-            throw e;
-        }
     }
     /**
      * allows datastore access for user objects
