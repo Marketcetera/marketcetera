e074947e9b701a9035296252d9e5d6388f2b950b
Sameer Patil <sameer.patil@marketcetera.com>
2012-11-27 11:42:57 +0000

    Currency market data requester class
    git-svn-id: file:///space/source.marketcetera.org@16381 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ root/branches/forex_phase_2/photon/plugins/org.marketcetera.photon.marketdata/src/main/java/org/marketcetera/photon/internal/marketdata/CurrencyRequestAdapter.java
@@ -0,0 +1,34 @@
+package org.marketcetera.photon.internal.marketdata;
+
+import org.marketcetera.marketdata.AssetClass;
+import org.marketcetera.marketdata.MarketDataRequestBuilder;
+import org.marketcetera.trade.Currency;
+import org.marketcetera.util.misc.ClassVersion;
+
+/* $License$ */
+
+/**
+ * Produces market data requests for {@link Currency} instruments.
+ *
+ */
+@ClassVersion("$Id: CurrencyRequestAdapter.java")
+public class CurrencyRequestAdapter
+        extends InstrumentRequestAdapter<Currency>
+{
+    /**
+     * Create a new CurrencyRequestAdapter instance.
+     */
+    public CurrencyRequestAdapter()
+    {
+        super(Currency.class);
+    }
+    /* (non-Javadoc)
+     * @see org.marketcetera.photon.internal.marketdata.InstrumentRequestAdapter#initializeRequest(org.marketcetera.trade.Instrument)
+     */
+    @Override
+    public MarketDataRequestBuilder initializeRequest(Currency inInstrument)
+    {
+        return MarketDataRequestBuilder.newRequest().withAssetClass(AssetClass.CURRENCY)
+                                                    .withSymbols(inInstrument.getSymbol());
+    }
+}
