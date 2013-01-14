c2e0bb094e5fb4e9850a9b65b01505a36fba5c62
Colin DuPlantis <colind@marketcetera.com>
2013-01-14 20:56:25 +0000

    - Removed Shiro authentication for now (doing manual authentication until session issue can be resolved) - Changed URL for WS to something more sane
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.0@16439 501a4a96-914b-0410-8454-f5988a4ca596

--- webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/security/impl/AuthenticationServiceImpl.java
+++ webservices/webservices-systemmodel/src/main/java/org/marketcetera/webservices/security/impl/AuthenticationServiceImpl.java
@@ -3,13 +3,15 @@ package org.marketcetera.webservices.security.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.security.sasl.AuthenticationException;
+
 import org.marketcetera.api.security.SecurityService;
-import org.marketcetera.api.security.Subject;
-import org.marketcetera.api.security.UsernamePasswordToken;
+import org.marketcetera.api.security.User;
 import org.marketcetera.api.systemmodel.Permission;
-import org.marketcetera.core.security.UnknownSessionException;
 import org.marketcetera.core.util.log.SLF4JLoggerProxy;
 import org.marketcetera.dao.PermissionDao;
+import org.marketcetera.dao.UserDao;
 import org.marketcetera.webservices.security.AuthenticationService;
 import org.marketcetera.webservices.systemmodel.WebServicesPermission;
 
@@ -21,58 +23,85 @@ import org.marketcetera.webservices.systemmodel.WebServicesPermission;
  * @version $Id$
  * @since $Release$
  */
-public class AuthenticationServiceImpl implements AuthenticationService {
-// ------------------------------ FIELDS ------------------------------
-
-    private PermissionDao permissionDao;
-
+public class AuthenticationServiceImpl
+        implements AuthenticationService
+{
     /**
-     * authentication manager subject
+     * Sets the permission DAO value.
+     *
+     * @param permissionDao a <code>PermissionDao</code> value
      */
-    private SecurityService securityService;
-
-// --------------------- GETTER / SETTER METHODS ---------------------
-
-    public void setPermissionDao(PermissionDao permissionDao) {
+    public void setPermissionDao(PermissionDao permissionDao)
+    {
         this.permissionDao = permissionDao;
     }
-
     /**
      * Sets the authenticationManagerService value.
      *
      * @param securityService <code>Subject</code> value
      */
-    public void setSecurityService(SecurityService securityService) {
+    public void setSecurityService(SecurityService securityService)
+    {
         this.securityService = securityService;
     }
-
-// ------------------------ INTERFACE METHODS ------------------------
-
-
-// --------------------- Interface AuthenticationService ---------------------
-
+    /**
+     * Sets the userDao value.
+     *
+     * @param a <code>UserDao</code> value
+     */
+    public void setUserDao(UserDao inUserDao)
+    {
+        userDao = inUserDao;
+    }
     /* (non-Javadoc)
     * @see org.marketcetera.webservices.security.AuthenticationService#authenticate(java.lang.String, java.lang.String)
     */
     @Override
-    public List<WebServicesPermission> authenticate(String username, String password) {
+    public List<WebServicesPermission> authenticate(String username,
+                                                    String password)
+    {
         SLF4JLoggerProxy.trace(AuthenticationServiceImpl.class, "AuthenticationService authenticate invoked with {}/********", //$NON-NLS-1$
-                username);
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        Subject subject = securityService.getSubject();
+                               username);
         try {
-            subject.login(token);
-        } catch (UnknownSessionException e) {
-            try {
-                subject.getSession().invalidate();
-            } catch (UnknownSessionException ignored) {}
-            return authenticate(username,
-                                password);
-        } catch (RuntimeException e) {
+            User user = userDao.getByName(username);
+            if(user.getPassword() == null) {
+                if(password != null) {
+                    throw new AuthenticationException("Incorrect username or password");
+                }
+            } else {
+                if(!user.getPassword().equals(password)) {
+                    throw new AuthenticationException("Incorrect username or password");
+                }
+            }
+        } catch (AuthenticationException e) {
             SLF4JLoggerProxy.warn(this,
-                                  e);
-            throw e;
+                                  e,
+                                  "Username: {} failed authentication",
+                                  username);
+            throw new RuntimeException(e);
+        } catch (NoResultException e) {
+            SLF4JLoggerProxy.warn(this,
+                                  e,
+                                  "Username: {} failed authentication",
+                                  username);
+            throw new RuntimeException(new AuthenticationException("Incorrect username or password"));
         }
+        // TODO this seems to be highly session-based, which is not the right idea: https://issues.apache.org/jira/browse/SHIRO-266
+//        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+//        Subject subject = securityService.getSubject();
+//        try {
+//            subject.login(token);
+//        } catch (UnknownSessionException e) {
+//            try {
+//                subject.getSession().invalidate();
+//            } catch (UnknownSessionException ignored) {}
+//            return authenticate(username,
+//                                password);
+//        } catch (RuntimeException e) {
+//            SLF4JLoggerProxy.warn(this,
+//                                  e);
+//            throw e;
+//        }
         List<WebServicesPermission> decoratedPermissions = new ArrayList<WebServicesPermission>();
         for (Permission permission : permissionDao.getAllByUsername(username)) {
             decoratedPermissions.add(new WebServicesPermission(permission));
@@ -80,4 +109,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         return decoratedPermissions;
     }
+    /**
+     * provides access to user data objects
+     */
+    private UserDao userDao;
+    /**
+     * provides access to permission data objects
+     */
+    private PermissionDao permissionDao;
+    /**
+     * authentication manager subject
+     */
+    @SuppressWarnings("unused")
+    private SecurityService securityService;
 }
