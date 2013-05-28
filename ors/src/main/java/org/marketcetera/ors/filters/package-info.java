ebf6f8362c5fdbb2735156727525d524a5e08ffa
Colin DuPlantis <colind@marketcetera.com>
2013-05-28 00:38:13 +0000

    Refactor: remove ORS from public branch
    git-svn-id: file:///space/source.marketcetera.org/root/branches/2.4.0_refactor@16562 501a4a96-914b-0410-8454-f5988a4ca596

--- ors/src/main/java/org/marketcetera/ors/filters/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Classes that provide message filtering and modification facilities in ORS.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.ors.filters;
