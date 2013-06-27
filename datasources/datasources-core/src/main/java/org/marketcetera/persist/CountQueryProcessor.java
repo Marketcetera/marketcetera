596ce99dc24891625e24a54cafe7d06e4c7a1350
Colin DuPlantis <colind@marketcetera.com>
2013-06-27 22:45:29 +0000

    Refactoring to create a datasources-core project
    git-svn-id: file:///space/source.marketcetera.org/root/branches/2.4.0_refactor@16609 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ datasources/datasources-core/src/main/java/org/marketcetera/persist/CountQueryProcessor.java
@@ -0,0 +1,48 @@
+package org.marketcetera.persist;
+
+import static org.marketcetera.persist.JPQLConstants.COUNT_ALL;
+import static org.marketcetera.persist.JPQLConstants.S;
+import static org.marketcetera.persist.JPQLConstants.SELECT;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.marketcetera.util.log.SLF4JLoggerProxy;
+import org.marketcetera.util.misc.ClassVersion;
+
+/* $License$ */
+/**
+ * A query processor that runs the query and returns
+ * the count of number of results that it would return.
+ *
+ * @author anshul@marketcetera.com
+ */
+@ClassVersion("$Id$")
+public class CountQueryProcessor extends QueryProcessor<Long> {
+    private static final long serialVersionUID = 8533947556425281342L;
+
+    /**
+     * Creates an instance.
+     *
+     */
+    protected CountQueryProcessor() {
+        super(false);
+    }
+
+    /**
+     * Over-ridden to prefix the generated query with <code>select count(*)</code>
+     * @param queryString the query string
+     * @param queryBase the query instance
+     */
+    @Override
+    protected void preGenerate(StringBuilder queryString, QueryBase queryBase) {
+        queryString.append(SELECT).append(S).append(COUNT_ALL);
+    }
+
+    public QueryResults<Long> process(EntityManager em, Query q)
+            throws PersistenceException {
+        final Long result = (Long) q.getSingleResult();
+        SLF4JLoggerProxy.debug(this,"Fetched Count is {}",result); //$NON-NLS-1$
+        return new SingleResult<Long>(result);
+    }
+}
