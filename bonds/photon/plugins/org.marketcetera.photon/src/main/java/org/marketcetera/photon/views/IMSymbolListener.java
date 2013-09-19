c176d55e57b1db5cef6a71e80f091aaab97ce2fe
Colin DuPlantis <colind@marketcetera.com>
2013-09-19 03:21:23 +0000

    Removing unneeded branch
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16689 501a4a96-914b-0410-8454-f5988a4ca596

--- bonds/photon/plugins/org.marketcetera.photon/src/main/java/org/marketcetera/photon/views/IMSymbolListener.java
+++
@@ -1,9 +0,0 @@
-package org.marketcetera.photon.views;
-
-import org.marketcetera.trade.Instrument;
-
-public interface IMSymbolListener {
-	void onAssertSymbol(Instrument instrument);
-	
-	boolean isListeningSymbol(Instrument instrument);
-}
