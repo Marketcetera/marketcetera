e20112f067b5499b78e4ba1c44538b2e20ba8078
Colin DuPlantis <colind@marketcetera.com>
2013-10-25 18:15:09 +0000

    Removing reintegrated branch
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16715 501a4a96-914b-0410-8454-f5988a4ca596

--- db/core/src/main/java/org/marketcetera/options/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Utility classes for supporting Options.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.options;
--- db/modules/strategy/src/test/sample_data/inputs/EmptyStrategy.rb
+++
@@ -1,9 +0,0 @@
-require 'java'
-java_import org.marketcetera.strategy.ruby.Strategy
-
-class EmptyStrategy < Strategy
-  def on_ask(ask)
-        set_property("onAsk",
-                     ask.toString());
-  end  
-end
