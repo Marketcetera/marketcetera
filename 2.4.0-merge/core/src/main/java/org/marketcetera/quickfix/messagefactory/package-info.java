338e19909ca360643e25c582a8cb7e1001d02ab5
Colin DuPlantis <colind@marketcetera.com>
2014-01-08 14:12:40 +0000

    Going to redo merge
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16809 501a4a96-914b-0410-8454-f5988a4ca596

--- 2.4.0-merge/core/src/main/java/org/marketcetera/quickfix/messagefactory/package-info.java
+++
@@ -1,9 +0,0 @@
-/* $License$ */
-/**
- * Classes for creating and augmenting FIX messages.
- *
- * @author anshul@marketcetera.com
- * @version $Id$
- * @since 1.0.0
- */
-package org.marketcetera.quickfix.messagefactory;
