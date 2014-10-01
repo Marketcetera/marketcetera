949cb0adf4d7cc88f0dc55fac139430d2b27591b
Colin DuPlantis <colind@marketcetera.com>
2014-10-01 19:42:09 +0000

    Merging 2.4.2 changes
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16949 501a4a96-914b-0410-8454-f5988a4ca596

--- 2.5.0/client/src/main/java/org/marketcetera/client/brokers/BrokerStatus.java
+++ 2.5.0/client/src/main/java/org/marketcetera/client/brokers/BrokerStatus.java
@@ -1,6 +1,7 @@
 package org.marketcetera.client.brokers;
 
 import java.io.Serializable;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -13,6 +14,8 @@ import org.marketcetera.algo.BrokerAlgoSpec;
 import org.marketcetera.trade.BrokerID;
 import org.marketcetera.util.misc.ClassVersion;
 
+/* $License$ */
+
 /**
  * The web service representation of a single broker's status.
  *
@@ -20,113 +23,126 @@ import org.marketcetera.util.misc.ClassVersion;
  * @since 1.0.0
  * @version $Id$
  */
-
-/* $License$ */
-
-@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 @ClassVersion("$Id$")
 public class BrokerStatus
-    implements Serializable
+        implements Serializable
 {
-
-    // CLASS DATA.
-
-    private static final long serialVersionUID=1L;
-
-
-    // INSTANCE DATA.
-
-    private final String mName;
-    private final BrokerID mId;
-    private final boolean mLoggedOn;
-    private final Set<BrokerAlgoSpec> mBrokerAlgos;
-
-
-    // CONSTRUCTORS.
-
     /**
-     * Creates a new status representation, given the broker
-     * information.
+     * Create a new BrokerStatus instance.
      *
-     * @param name The broker name.
-     * @param id The broker ID.
-     * @param loggedOn The logon flag.
+     * @param inName a <code>String</code> value
+     * @param inBrokerId a <code>BrokerID</code> value
+     * @param inLoggedOn a <code>boolean</code> value
+     * @param inSettings a <code>Map&lt;String,String&gt;</code> value
      * @param inAlgoSpecs a <code>Set&lt;BrokerAlgoSpec&gt;</code> value
      */
-    public BrokerStatus(String name,
-                        BrokerID id,
-                        boolean loggedOn,
+    public BrokerStatus(String inName,
+                        BrokerID inBrokerId,
+                        boolean inLoggedOn,
+                        Map<String,String> inSettings,
                         Set<BrokerAlgoSpec> inAlgoSpecs)
     {
-        mName=name;
-        mId=id;
-        mLoggedOn=loggedOn;
-        mBrokerAlgos = inAlgoSpecs;
+        name = inName;
+        brokerId = inBrokerId;
+        loggedOn = inLoggedOn;
+        settings = inSettings;
+        brokerAlgos = inAlgoSpecs;
     }
     /**
      * Create a new BrokerStatus instance.
      *
      * @param inName a <code>String</code> value
-     * @param inId a <code>BrokerID</code> value
+     * @param inBrokerId a <code>BrokerID</code> value
+     * @param inLoggedOn a <code>boolean</code> value
+     * @param inSettings a <code>Map&lt;String,String&gt;</code> value
+     */
+    public BrokerStatus(String inName,
+                        BrokerID inBrokerId,
+                        boolean inLoggedOn,
+                        Map<String,String> inSettings)
+    {
+        this(inName,
+             inBrokerId,
+             inLoggedOn,
+             inSettings,
+             null);
+    }
+    /**
+     * Create a new BrokerStatus instance.
+     *
+     * @param inName a <code>String</code> value
+     * @param inBrokerId a <code>BrokerID</code> value
+     * @param inLoggedOn a <code>boolean</code> value
+     * @param inAlgoSpecs a <code>Set&lt;BrokerAlgoSpec&gt;</code> value
+     */
+    public BrokerStatus(String inName,
+                        BrokerID inBrokerId,
+                        boolean inLoggedOn,
+                        Set<BrokerAlgoSpec> inAlgoSpecs)
+    {
+        this(inName,
+             inBrokerId,
+             inLoggedOn,
+             null,
+             inAlgoSpecs);
+    }
+    /**
+     * Create a new BrokerStatus instance.
+     *
+     * @param inName a <code>String</code> value
+     * @param inBrokerId a <code>BrokerID</code> value
      * @param inLoggedOn a <code>boolean</code> value
      */
     public BrokerStatus(String inName,
-                        BrokerID inId,
+                        BrokerID inBrokerId,
                         boolean inLoggedOn)
     {
-        mName = inName;
-        mId = inId;
-        mLoggedOn = inLoggedOn;
-        mBrokerAlgos = null;
+        this(inName,
+             inBrokerId,
+             inLoggedOn,
+             null,
+             null);
     }
     /**
-     * Creates a new status representation. This empty constructor is
-     * intended for use by JAXB.
+     * Create a new BrokerStatus instance.
      */
-
-    protected BrokerStatus()
+    @SuppressWarnings("unused")
+    private BrokerStatus()
     {
-        mName=null;
-        mId=null;
-        mLoggedOn=false;
-        mBrokerAlgos = null;
+        name = null;
+        brokerId = null;
+        loggedOn = false;
+        brokerAlgos = null;
+        settings = null;
     }
-
-
-    // INSTANCE METHODS.
-
     /**
-     * Returns the receiver's name.
+     * Get the name value.
      *
-     * @return The name.
+     * @return a <code>String</code> value
      */
-
     public String getName()
     {
-        return mName;
+        return name;
     }
-
     /**
-     * Returns the receiver's broker ID.
+     * Get the brokerId value.
      *
-     * @return The ID.
+     * @return a <code>BrokerID</code> value
      */
-
     public BrokerID getId()
     {
-        return mId;
+        return brokerId;
     }
-
     /**
-     * Returns the receiver's logon flag.
+     * Get the loggedOn value.
      *
-     * @return The flag.
+     * @return a <code>boolean</code> value
      */
-
     public boolean getLoggedOn()
     {
-        return mLoggedOn;
+        return loggedOn;
     }
     /**
      * Get the brokerAlgos value.
@@ -135,15 +151,24 @@ public class BrokerStatus
      */
     public Set<BrokerAlgoSpec> getBrokerAlgos()
     {
-        return mBrokerAlgos;
+        return brokerAlgos;
+    }
+    /**
+     * Get the settings value.
+     *
+     * @return a <code>Map&lt;String,String&gt;</code> value
+     */
+    public Map<String,String> getSettings()
+    {
+        return settings;
     }
     /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
+     * @see java.lang.Object#toString()
      */
     @Override
     public int hashCode()
     {
-        return new HashCodeBuilder().append(mId).toHashCode();
+        return new HashCodeBuilder().append(brokerId).toHashCode();
     }
     /* (non-Javadoc)
      * @see java.lang.Object#equals(java.lang.Object)
@@ -161,13 +186,35 @@ public class BrokerStatus
             return false;
         }
         BrokerStatus other = (BrokerStatus) obj;
-        return new EqualsBuilder().append(mId,other.mId).isEquals();
+        return new EqualsBuilder().append(brokerId,other.brokerId).isEquals();
     }
     @Override
     public String toString()
     {
-        return String.format
-            ("Broker: %s(%s,%s)", //$NON-NLS-1$
-             String.valueOf(getName()),String.valueOf(getId()),getLoggedOn());
+        return String.format("Broker: %s(%s,%s)", //$NON-NLS-1$
+                             String.valueOf(getName()),
+                             String.valueOf(getId()),
+                             getLoggedOn());
     }
+    /**
+     * name value
+     */
+    private final String name;
+    /**
+     * broker id value
+     */
+    private final BrokerID brokerId;
+    /**
+     * logged on value
+     */
+    private final boolean loggedOn;
+    /**
+     * broker algos value
+     */
+    private final Set<BrokerAlgoSpec> brokerAlgos;
+    /**
+     * broker settings value
+     */
+    private final Map<String,String> settings;
+    private static final long serialVersionUID = -4170685026349637823L;
 }
