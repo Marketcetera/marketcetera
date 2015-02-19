08d12bf20f899ad9355638872b89247770174e9e
Colin DuPlantis <colind@marketcetera.com>
2015-02-19 21:28:26 +0000

    MATP-830
    git-svn-id: file:///space/source.marketcetera.org/root/branches/colin@16984 501a4a96-914b-0410-8454-f5988a4ca596

--- log/core/src/main/java/org/marketcetera/marketdata/ISubscription.java
+++
@@ -1,7 +0,0 @@
-package org.marketcetera.marketdata;
-
-import quickfix.Message;
-@Deprecated
-public interface ISubscription {
-	public boolean isResponse(Message possibleResponse);
-}
--- log/trunk/photon/plugins/com.swtworkbench.community.xswt/src/com/swtworkbench/community/xswt/scripting/ScriptingEngine.java
+++
@@ -1,7 +0,0 @@
-package com.swtworkbench.community.xswt.scripting;
-
-import com.swtworkbench.community.xswt.XSWT;
-
-public interface ScriptingEngine {
-	public EvaluationContext getEvaluationContext(String name, XSWT bindings);
-}
