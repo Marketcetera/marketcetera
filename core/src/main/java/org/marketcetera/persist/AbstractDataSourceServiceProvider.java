ad6369ff94d07834df665cb846115a92d55b8c36
Colin DuPlantis <colind@marketcetera.com>
2015-08-19 15:38:59 +0000

    MATP-848 Add support for direct JDBC invocations
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@17035 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ core/src/main/java/org/marketcetera/persist/AbstractDataSourceServiceProvider.java
@@ -0,0 +1,50 @@
+package org.marketcetera.persist;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.apache.commons.lang.Validate;
+
+/* $License$ */
+
+/**
+ * Provides common datasource provider behavior.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id: AbstractDataSourceServiceProvider.java 84161 2014-10-18 00:39:01Z colin $
+ * @since $Release$
+ */
+public abstract class AbstractDataSourceServiceProvider
+{
+    /**
+     * Validates and starts the object.
+     */
+    @PostConstruct
+    public void start()
+    {
+        Validate.notNull(dataSource,
+                         "Datasource is required");
+    }
+    /**
+     * Get the DataSource value.
+     *
+     * @return a <code>DataSource</code> value
+     */
+    public DataSource getDataSource()
+    {
+        return dataSource;
+    }
+    /**
+     * Sets the DataSource value.
+     *
+     * @param inDataSource a <code>DataSource</code> value
+     */
+    public void setDataSource(DataSource inDataSource)
+    {
+        dataSource = inDataSource;
+    }
+    /**
+     * provides access to JDBC
+     */
+    private DataSource dataSource;
+}
