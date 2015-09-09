92732d675b05904e0869a40f10b1984dd0a54eb3
Colin DuPlantis <colind@marketcetera.com>
2015-09-09 18:56:28 +0000

    MATP-843 Add multi-instance support to clustering
    git-svn-id: file:///space/source.marketcetera.org/root/branches/2.5.0-cluster@17049 501a4a96-914b-0410-8454-f5988a4ca596

--- core/src/main/java/org/marketcetera/metrics/MetricService.java
+++ core/src/main/java/org/marketcetera/metrics/MetricService.java
@@ -5,7 +5,7 @@ import com.codahale.metrics.MetricRegistry;
 /* $License$ */
 
 /**
- *
+ * Provides services related to collecting and calculating metric.
  *
  * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
  * @version $Id$
@@ -21,31 +21,29 @@ public class MetricService
         metrics = new MetricRegistry();
     }
     /**
-     * 
+     * Gets the metric service instance.
      *
-     *
-     * @return
+     * @return a <code>MetricService</code> value
      */
     public static MetricService getInstance()
     {
         return instance;
     }
     /**
-     * 
+     * Gets the metrics object.
      *
-     *
-     * @return
+     * @return a <code>MetricRegistry</code> value
      */
     public MetricRegistry getMetrics()
     {
         return metrics;
     }
     /**
-     * 
+     * metric service instance
      */
     private static MetricService instance = new MetricService();;
     /**
-     * 
+     * metrics core instance
      */
     private final MetricRegistry metrics;
 }
