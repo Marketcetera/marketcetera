26eb6cbec15b94bfca24e87e266f020736589916
Colin DuPlantis <colind@marketcetera.com>
2016-09-21 14:20:55 +0000

    Initial add of new Exsim market data adapter
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.11@17266 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ modules/marketdata/marketdata-marketcetera-new/src/main/java/org/marketcetera/marketdata/exsim/ExsimFeedModuleFactory.java
@@ -0,0 +1,15 @@
+package org.marketcetera.marketdata.exsim;
+
+/* $License$ */
+
+/**
+ *
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public class ExsimFeedModuleFactory
+{
+
+}
