91488a87a874d9d0c8fe166eccdef5dbd597ab64
Colin DuPlantis <colind@marketcetera.com>
2017-04-10 19:55:50 +0000

    TC-616 Integrate TensorFlow
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.0.11@17375 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ modules/tensorflow/src/main/java/org/marketcetera/tensorflow/Messages.java
@@ -0,0 +1,25 @@
+package org.marketcetera.tensorflow;
+
+import org.marketcetera.util.log.I18NLoggerProxy;
+import org.marketcetera.util.log.I18NMessage0P;
+import org.marketcetera.util.log.I18NMessage1P;
+import org.marketcetera.util.log.I18NMessageProvider;
+
+/* $License$ */
+
+/**
+ *
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public interface Messages
+{
+    static final I18NMessageProvider PROVIDER = new I18NMessageProvider("activ", Messages.class.getClassLoader()); //$NON-NLS-1$
+    static final I18NLoggerProxy LOGGER = new I18NLoggerProxy(PROVIDER);
+
+    static final I18NMessage0P PROVIDER_DESCRIPTION = new I18NMessage0P(LOGGER,"provider_description"); //$NON-NLS-1$
+    static final I18NMessage1P FEED_NOT_AVAILABLE = new I18NMessage1P(LOGGER,"feed_not_available"); //$NON-NLS-1$
+    static final I18NMessage1P DATA_PROCESSING_ERROR = new I18NMessage1P(LOGGER,"data_processing_error"); //$NON-NLS-1$
+}
