d326321a047d83ffa6d12fe1cb3bc2b4b5957e22
Colin DuPlantis <colind@marketcetera.com>
2017-08-10 15:12:22 +0000

    MATP-966 Develop Complete Open Source Offering
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.1.x@17473 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ admin/admin-db/src/main/java/org/marketcetera/admin/user/dao/UserDao.java
@@ -0,0 +1,50 @@
+package org.marketcetera.admin.user.dao;
+
+import org.marketcetera.admin.user.PersistentUser;
+import org.marketcetera.util.misc.ClassVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+
+/* $License$ */
+
+/**
+ * Provides datastore access to {@link PersistentUser} objects.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id: UserDao.java 17339 2017-08-10 02:14:34Z colin $
+ * @since 2.4.2
+ */
+@ClassVersion("$Id: UserDao.java 17339 2017-08-10 02:14:34Z colin $")
+public interface UserDao
+        extends JpaRepository<PersistentUser,Long>,QueryDslPredicateExecutor<PersistentUser>
+{
+    /**
+     * Finds the user with the given username.
+     *
+     * @param inUsername a <code>String</code> value
+     * @return a <code>SimpleUser</code> value
+     */
+    PersistentUser findByName(String inUsername);
+    /**
+     * Updates the user data for the user with the given name. 
+     *
+     * @param inUsername a <code>String</code> value
+     * @param inUserData a <code>String</code> value
+     */
+    @Modifying
+    @Query("update user u set u.userData=?2 where u.name=?1")
+    void updateUserByName(String inUsername,
+                          String inUserData);
+    /**
+     * Updates the active status for the user with the given name.
+     *
+     * @param inUsername a <code>String</code> value
+     * @param inIsActive a <code>boolean</code> value
+     */
+    @Modifying	
+    @Query("update user u set u.active=?2 where u.name=?1")
+    void updateUserActiveStatus(String inUsername,
+                                boolean inIsActive);
+}
