e4ffe9f7709fe778cb4ec07c2f8ed2159d122f89
Colin DuPlantis <colind@marketcetera.com>
2017-08-21 21:38:13 +0000

    MATP-966 Develop Complete Open Source Offering
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.1.x@17497 501a4a96-914b-0410-8454-f5988a4ca596

--- packages/server-package/src/main/java/org/marketcetera/server/ServerApplication.java
+++ packages/server-package/src/main/java/org/marketcetera/server/ServerApplication.java
@@ -1,5 +1,8 @@
 package org.marketcetera.server;
 
+import org.marketcetera.core.ApplicationContainer;
+import org.marketcetera.core.ApplicationVersion;
+import org.marketcetera.core.Messages;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -28,5 +31,72 @@ public class ServerApplication
     {
         SpringApplication.run(ServerApplication.class,
                               inArgs);
+        // log application start
+        Messages.APP_COPYRIGHT.info(ApplicationContainer.class);
+        Messages.APP_VERSION_BUILD.info(ApplicationContainer.class,
+                                        ApplicationVersion.getVersion(ApplicationContainer.class),
+                                        ApplicationVersion.getBuildNumber());
+        Messages.APP_START.info(ApplicationContainer.class);
+        // check to see if we're using a different starting context file than the default
+//        String rawValue = StringUtils.trimToNull(System.getProperty(CONTEXT_FILE_PROP));
+//        if(rawValue != null) {
+//            contextFilename = rawValue;
+//        }
+        final ApplicationContainer application;
+        try {
+            application = new ApplicationContainer();
+            application.setArguments(inArgs);
+            application.start();
+        } catch(Exception e) {
+            if(exitCode == 0) {
+                exitCode = -1;
+            }
+            e.printStackTrace();
+            try {
+                Messages.APP_STOP_ERROR.error(ApplicationContainer.class,
+                                              e);
+            } catch(Exception e2) {
+                System.err.println("Reporting failed"); //$NON-NLS-1$
+                System.err.println("Reporting failure"); //$NON-NLS-1$
+                e2.printStackTrace();
+                System.err.println("Original failure"); //$NON-NLS-1$
+                e.printStackTrace();
+            }
+            System.exit(exitCode);
+            return;
+        }
+        Messages.APP_STARTED.info(ApplicationContainer.class);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                application.stop();
+                Messages.APP_STOP.info(ApplicationContainer.class);
+//                if(LogManager.getContext() instanceof LoggerContext) {
+//                    Configurator.shutdown((LoggerContext)LogManager.getContext());
+//                }
+            }
+        });
+        try {
+            application.startWaitingForever();
+        } catch(Exception e) {
+            try {
+                Messages.APP_STOP_ERROR.error(ApplicationContainer.class,
+                                              e);
+            } catch(Exception e2) {
+                System.err.println("Reporting failed"); //$NON-NLS-1$
+                System.err.println("Reporting failure"); //$NON-NLS-1$
+                e2.printStackTrace();
+                System.err.println("Original failure"); //$NON-NLS-1$
+                e.printStackTrace();
+            }
+            System.exit(exitCode);
+            return;
+        }
+        Messages.APP_STOP_SUCCESS.info(ApplicationContainer.class);
+        System.exit(exitCode);
     }
+    /**
+     * exit code to return on exit
+     */
+    protected static int exitCode = 0;
 }
