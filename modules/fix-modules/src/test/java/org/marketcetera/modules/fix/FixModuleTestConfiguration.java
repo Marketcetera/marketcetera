5ce06793f260a434291523d74474c3a4b5a622c6
Colin DuPlantis <colind@marketcetera.com>
2017-08-23 02:16:57 +0000

    MATP-966 Develop Complete Open Source Offering
    git-svn-id: file:///space/source.marketcetera.org/root/branches/3.1.x@17500 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ modules/fix-modules/src/test/java/org/marketcetera/modules/fix/FixModuleTestConfiguration.java
@@ -0,0 +1,45 @@
+package org.marketcetera.modules.fix;
+
+import org.marketcetera.module.ModuleManager;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import quickfix.MessageFactory;
+
+/* $License$ */
+
+/**
+ * Provides Spring Boot test configuration.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+@ComponentScan("org.marketcetera")
+@SpringBootConfiguration
+public class FixModuleTestConfiguration
+{
+    /**
+     * Get the message factory value.
+     *
+     * @return a <code>MessageFactory</code> value
+     */
+    @Bean
+    public MessageFactory getMessageFactory()
+    {
+        return new quickfix.DefaultMessageFactory();
+    }
+    /**
+     * Get the module manager value.
+     *
+     * @return a <code>ModuleManager</code> value
+     */
+    @Bean
+    public ModuleManager getModuleManager()
+    {
+        ModuleManager moduleManager = new ModuleManager();
+        moduleManager.init();
+        return moduleManager;
+    }
+}
