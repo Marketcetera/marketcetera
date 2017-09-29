54bc9be9873a1b0ad2765e61af6c9660e09498de
Colin DuPlantis <colind@marketcetera.com>
2017-09-29 23:02:16 +0000

    MATP-973 Add localhost SE to Photon by default
    git-svn-id: file:///space/source.marketcetera.org/root/trunk@17578 501a4a96-914b-0410-8454-f5988a4ca596

--- photon/plugins/org.marketcetera.photon.strategy/src/main/java/org/marketcetera/photon/internal/strategy/StrategyEnginesSupport.java
+++ photon/plugins/org.marketcetera.photon.strategy/src/main/java/org/marketcetera/photon/internal/strategy/StrategyEnginesSupport.java
@@ -1,9 +1,11 @@
 package org.marketcetera.photon.internal.strategy;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EObject;
 import org.marketcetera.photon.commons.emf.EMFFilePersistence;
@@ -18,6 +20,7 @@ import org.osgi.framework.BundleContext;
 
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 
 /* $License$ */
 
@@ -29,52 +32,65 @@ import com.google.common.collect.Collections2;
  * @since 2.0.0
  */
 @ClassVersion("$Id$")
-class StrategyEnginesSupport extends StrategyAgentEnginesSupport {
-
-    private static final String PERSISTENCE_FILE_NAME = "engines.xml"; //$NON-NLS-1$
-
+class StrategyEnginesSupport
+        extends StrategyAgentEnginesSupport
+{
     /**
      * Persistence service that only saves {@link StrategyAgentEngine}.
+     *
+     * @author <a href="mailto:will@marketcetera.com">Will Horn</a>
+     * @version $Id$
+     * @since 2.0.0
      */
     @ClassVersion("$Id$")
-    private final static class Persistence implements IEMFPersistence {
-
-        private final IEMFPersistence mDelegate = new EMFFilePersistence(
-                Platform.getStateLocation(
-                        Platform.getBundle(StrategyUI.PLUGIN_ID)).append(
-                        PERSISTENCE_FILE_NAME).toFile());
-
+    private final static class Persistence
+            implements IEMFPersistence
+    {
         @Override
         public void save(Collection<? extends EObject> objects)
-                throws IOException {
-            mDelegate.save(Collections2.filter(objects, Predicates
-                    .instanceOf(StrategyAgentEngine.class)));
-
+                throws IOException
+        {
+            persistenceDelegate.save(Collections2.filter(objects,
+                                                         Predicates.instanceOf(StrategyAgentEngine.class)));
         }
-
         @Override
-        public List<? extends EObject> restore() throws IOException {
-            return mDelegate.restore();
+        public List<? extends EObject> restore()
+                throws IOException
+        {
+            if(!persistenceFile.exists()) {
+                FileUtils.writeLines(persistenceFile,
+                                     Lists.newArrayList("<strategyagent:StrategyAgentEngine xmi:version=\"2.0\" xmlns:xmi=\"http://www.omg.org/XMI\" xmlns:strategyagent=\"http://www.marketcetera.org/photon/strategy/engine/strategyagent/1.0\" name=\"Local SE\" jmsUrl=\"tcp://localhost:61617\" webServiceHostname=\"localhost\" webServicePort=\"8998\"/>"));
+            }
+            return persistenceDelegate.restore();
         }
+        /**
+         * file that holds persisted strategy engines
+         */
+        private final File persistenceFile = Platform.getStateLocation(Platform.getBundle(StrategyUI.PLUGIN_ID)).append(PERSISTENCE_FILE_NAME).toFile();
+        /**
+         * persistence delegate object
+         */
+        private final IEMFPersistence persistenceDelegate = new EMFFilePersistence(persistenceFile);
     };
-
     /**
-     * Constructor.
-     * 
-     * @param context
-     *            the context with which to register the service
-     * @throws IllegalArgumentException
-     *             if context is null
-     * @throws IllegalStateException
-     *             if called from a non UI thread
+     * Create a new StrategyEnginesSupport instance.
+     *
+     * @param inContext a <code>BundleContext</code> that contains the context with which to register the service
+     * @throws IllegalArgumentException if context is null
+     * @throws IllegalStateException if called from a non UI thread
      */
-    public StrategyEnginesSupport(BundleContext context) {
-        super(context, new Persistence());
-    }
-
-    @Override
-    protected void initList(List<StrategyEngine> engines)
+    public StrategyEnginesSupport(BundleContext inContext)
     {
-        super.initList(engines);
+        super(inContext,
+              new Persistence());
     }
+    @Override
+    protected void initList(List<StrategyEngine> inEngines)
+    {
+        super.initList(inEngines);
+    }
+    /**
+     * name of file that holds persisted engines
+     */
+    private static final String PERSISTENCE_FILE_NAME = "engines.xml"; //$NON-NLS-1$
 }
