10b0d9a0de724a297229aa386390d505c82f4919
Colin DuPlantis <colind@marketcetera.com>
2018-01-30 14:15:38 +0000

    Add webui package
    git-svn-id: file:///space/source.marketcetera.org/root/branches/4.0.x@17646 501a4a96-914b-0410-8454-f5988a4ca596

---
+++ webui/webui-package/src/main/java/org/marketcetera/webui/MyUI.java
@@ -0,0 +1,47 @@
+package org.marketcetera.webui;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI
+        extends UI
+{
+    @Override
+    protected void init(VaadinRequest vaadinRequest)
+    {
+        final VerticalLayout layout = new VerticalLayout();
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+        Button button = new Button("Click Me");
+        button.addClickListener(e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+        });
+        layout.addComponents(name, button);
+        setContent(layout);
+    }
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet
+            extends VaadinServlet
+    {
+        private static final long serialVersionUID = 6022319740380612064L;
+    }
+    private static final long serialVersionUID = 8857063323565115122L;
+}
