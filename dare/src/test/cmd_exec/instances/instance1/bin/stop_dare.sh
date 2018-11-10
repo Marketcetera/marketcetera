00058a079a1e593237b118da4d3116ccda8de7b2
Colin DuPlantis <colind@marketcetera.com>
2018-11-10 13:54:23 +0000

    Remove DARE instances dir from src control
    git-svn-id: file:///space/source.marketcetera.org/root/branches/merge@17751 501a4a96-914b-0410-8454-f5988a4ca596

--- dare/src/test/cmd_exec/instances/instance1/bin/stop_dare.sh
+++
@@ -1,7 +0,0 @@
-cd /home/colin/marketcetera/workspaces/trunk/code/private/source/ors/src/test/cmd_exec/instances/instance1/bin
-if [ -f dare.pid ]
-then
-    kill `cat dare.pid`
-else
-    kill `ps -ef | grep metc.instance=1 | grep java | awk '{print $2}'`
-fi
