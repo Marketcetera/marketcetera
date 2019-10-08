47a60576aa6592dd5d354bc425e20bb2b68e7855
Colin DuPlantis <colind@marketcetera.com>
2019-10-08 22:23:22 +0000

    Cleanup UI before new add
    git-svn-id: file:///space/source.marketcetera.org/root/branches/4.0.x@17911 501a4a96-914b-0410-8454-f5988a4ca596

--- ui/src/main/java/org/marketcetera/webui/ui/utils/TemplateUtil.java
+++
@@ -1,8 +0,0 @@
-package org.marketcetera.webui.ui.utils;
-
-public class TemplateUtil {
-
-	public static String generateLocation(String basePage, String entityId) {
-		return basePage + (entityId == null || entityId.isEmpty() ? "" : "/" + entityId);
-	}
-}
