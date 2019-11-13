8306edfa4991b8fdef546de680b0c43df0296310
Colin DuPlantis <colind@marketcetera.com>
2019-11-13 23:19:12 +0000

    Update web UI FIX session watcher
    git-svn-id: file:///space/source.marketcetera.org/root/branches/4.0.x@17951 501a4a96-914b-0410-8454-f5988a4ca596

--- ui/webui-admin/src/main/java/org/marketcetera/web/fixadmin/FixSessionWatcher.java
+++ ui/webui-admin/src/main/java/org/marketcetera/web/fixadmin/FixSessionWatcher.java
@@ -1,15 +1,21 @@
 package org.marketcetera.web.fixadmin;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.marketcetera.brokers.BrokerStatusListener;
 import org.marketcetera.fix.ActiveFixSession;
+import org.marketcetera.util.log.SLF4JLoggerProxy;
+import org.marketcetera.web.SessionUser;
+import org.marketcetera.web.events.LoginEvent;
+import org.marketcetera.web.events.LogoutEvent;
 import org.marketcetera.web.service.ServiceManager;
+import org.marketcetera.web.service.WebMessageService;
 import org.marketcetera.web.service.admin.AdminClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 
+import com.google.common.eventbus.Subscribe;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
@@ -24,31 +30,113 @@ import com.vaadin.ui.Notification.Type;
  * @since $Release$
  */
 @SpringComponent
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class FixSessionWatcher
-        implements BrokerStatusListener
 {
-    // TODO when/how to start this, when to stop it?, do all sessions get the notification?
     /**
      * Validate and start the object.
      */
     @PostConstruct
     public void start()
     {
-        serviceManager.getService(AdminClientService.class).addBrokerStatusListener(this);
+        messageService.register(this);
     }
-    /* (non-Javadoc)
-     * @see org.marketcetera.brokers.BrokerStatusListener#receiveBrokerStatus(org.marketcetera.fix.ActiveFixSession)
+    /**
+     * Stop the object.
      */
-    @Override
-    public void receiveBrokerStatus(ActiveFixSession inActiveFixSession)
+    @PreDestroy
+    public void stop()
     {
-        Notification.show(inActiveFixSession.getFixSession().getName() + " " + inActiveFixSession.getStatus(),
-                          Type.TRAY_NOTIFICATION);
+        messageService.unregister(this);
+    }
+    /**
+     * Notify on new login events.
+     *
+     * @param inEvent a <code>LoginEvent</code> value
+     */
+    @Subscribe
+    public void onLogin(LoginEvent inEvent)
+    {
+        SLF4JLoggerProxy.trace(this,
+                               "{} logged in",
+                               inEvent.getSessionUser());
+        SessionUser currentUser = inEvent.getSessionUser();
+        FixSessionWatcherSubscriber subscriber = currentUser.getAttribute(FixSessionWatcherSubscriber.class);
+        if(subscriber == null) {
+            subscriber = new FixSessionWatcherSubscriber(currentUser);
+            currentUser.setAttribute(FixSessionWatcherSubscriber.class,
+                                     subscriber);
+        }
+        serviceManager.getService(AdminClientService.class).addBrokerStatusListener(subscriber);
+    }
+    /**
+     * Notify on logout events.
+     *
+     * @param inEvent a <code>LogoutEvent</code> value
+     */
+    @Subscribe
+    public void onLogout(LogoutEvent inEvent)
+    {
+        SessionUser currentUser = SessionUser.getCurrentUser();
+        SLF4JLoggerProxy.trace(this,
+                               "{} logged out",
+                               currentUser);
+        if(currentUser == null) {
+            return;
+        }
+        FixSessionWatcherSubscriber subscriber = currentUser.getAttribute(FixSessionWatcherSubscriber.class);
+        if(subscriber != null) {
+            serviceManager.getService(AdminClientService.class).removeBrokerStatusListener(subscriber);
+        }
+    }
+    /**
+     * Subscribes to broker status changes and manages updates.
+     *
+     * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+     * @version $Id$
+     * @since $Release$
+     */
+    private class FixSessionWatcherSubscriber
+            implements BrokerStatusListener
+    {
+        /* (non-Javadoc)
+         * @see org.marketcetera.brokers.BrokerStatusListener#receiveBrokerStatus(org.marketcetera.fix.ActiveFixSession)
+         */
+        @Override
+        public void receiveBrokerStatus(ActiveFixSession inActiveFixSession)
+        {
+            SLF4JLoggerProxy.trace(FixSessionWatcher.this,
+                                   "{} notifying {}",
+                                   user,
+                                   inActiveFixSession);
+            StringBuilder prettyStatus = new StringBuilder();
+            for(String statusComponent : inActiveFixSession.getStatus().name().split("_")) {
+                prettyStatus.append(StringUtils.lowerCase(statusComponent)).append(' ');
+            }
+            Notification.show(inActiveFixSession.getFixSession().getName() + " " + StringUtils.trim(prettyStatus.toString()),
+                              Type.TRAY_NOTIFICATION);
+        }
+        /**
+         * Create a new FixSessionWatcherSubscriber instance.
+         *
+         * @param inUser a <code>SessionUser</code> value
+         */
+        private FixSessionWatcherSubscriber(SessionUser inUser)
+        {
+            user = inUser;
+        }
+        /**
+         * session user that owns this listener
+         */
+        private final SessionUser user;
     }
     /**
      * provides access to client services
      */
     @Autowired
     private ServiceManager serviceManager;
+    /**
+     * provides access to web message services
+     */
+    @Autowired
+    private WebMessageService messageService;
 }
