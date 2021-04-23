f52bdeb5cac11dbfc452ed10242327602bee05e8
Colin DuPlantis <colin@marketcetera.com>
2021-04-23 07:27:55 -0700

    #241 Checkpoint Commit

---
+++ eventbus/eventbus-api/src/main/java/org/marketcetera/eventbus/DataEventFactory.java
@@ -0,0 +1,17 @@
+package org.marketcetera.eventbus;
+
+import org.marketcetera.core.Factory;
+
+/* $License$ */
+
+/**
+ *
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public interface DataEventFactory
+        extends Factory<DataEvent>
+{
+}
---
+++ eventbus/eventbus-api/src/main/java/org/marketcetera/eventbus/EventAction.java
@@ -0,0 +1,17 @@
+package org.marketcetera.eventbus;
+
+/* $License$ */
+
+/**
+ * Indicates the type of data event.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public enum EventAction
+{
+    ADD,
+    CHANGE,
+    DELETE;
+}
