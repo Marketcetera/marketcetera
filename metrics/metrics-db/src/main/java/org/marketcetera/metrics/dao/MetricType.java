d5cbbd54dc0e6553238da900752c1921861893ee
Colin DuPlantis <colin@marketcetera.com>
2021-07-07 13:04:22 -0700

    Initial add of metrics

---
+++ metrics/metrics-db/src/main/java/org/marketcetera/metrics/dao/MetricType.java
@@ -0,0 +1,19 @@
+package org.marketcetera.metrics.dao;
+
+/* $License$ */
+
+/**
+ * Describes the type of a {@link PersistentMetric}.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
+public enum MetricType
+{
+    HISTOGRAM,
+    COUNTER,
+    TIMER,
+    METER,
+    GAUGE;
+}
