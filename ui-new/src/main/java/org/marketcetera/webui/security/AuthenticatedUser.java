83237a01fd948bd0ce76c85236fd8af361cc1872
Colin DuPlantis <colin@marketcetera.com>
2021-08-02 07:05:13 -0700

    Work on new UI

--- ui-new/src/main/java/org/marketcetera/webui/security/AuthenticatedUser.java
+++ ui-new/src/main/java/org/marketcetera/webui/security/AuthenticatedUser.java
@@ -2,11 +2,8 @@ package org.marketcetera.webui.security;
 
 import java.util.Optional;
 
+import org.marketcetera.admin.service.UserService;
 import org.marketcetera.webui.data.entity.User;
-import org.marketcetera.webui.data.service.UserRepository;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.server.VaadinServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,34 +11,50 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServletRequest;
+
 @Component
-public class AuthenticatedUser {
+public class AuthenticatedUser
+{
 
-    @Autowired
-    private UserRepository userRepository;
-
-    private UserDetails getAuthenticatedUser() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Object principal = context.getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) context.getAuthentication().getPrincipal();
-            return userDetails;
-        }
-        return null;
-    }
-
-    public Optional<User> get() {
+    public Optional<User> get()
+    {
         UserDetails details = getAuthenticatedUser();
         if (details == null) {
             return Optional.empty();
         }
         return Optional.of(userRepository.findByUsername(details.getUsername()));
     }
-
-    public void logout() {
+    /**
+     * Logs the current user out.
+     */
+    public void logout()
+    {
         UI.getCurrent().getPage().setLocation(SecurityConfiguration.LOGOUT_URL);
         SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-        logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(), null, null);
+        logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(),
+                             null,
+                             null);
     }
-
+    /**
+     * Get the user details for the authenticated user.
+     *
+     * @return a <code>UserDetails</code> value
+     */
+    private UserDetails getAuthenticatedUser()
+    {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Object principal = context.getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails)context.getAuthentication().getPrincipal();
+            return userDetails;
+        }
+        return null;
+    }
+    /**
+     * provides access to user services
+     */
+    @Autowired
+    private UserService userService;
 }
--- ui-new/src/main/java/org/marketcetera/webui/security/UserDetailsServiceImpl.java
+++ ui-new/src/main/java/org/marketcetera/webui/security/UserDetailsServiceImpl.java
@@ -1,40 +1,60 @@
 package org.marketcetera.webui.security;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
-import org.marketcetera.webui.data.entity.User;
-import org.marketcetera.webui.data.service.UserRepository;
-
+import org.apache.commons.compress.utils.Lists;
+import org.marketcetera.admin.Permission;
+import org.marketcetera.admin.User;
+import org.marketcetera.admin.service.AuthorizationService;
+import org.marketcetera.admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/* $License$ */
+
+/**
+ * Provides authentication and authorization services.
+ *
+ * @author <a href="mailto:colin@marketcetera.com">Colin DuPlantis</a>
+ * @version $Id$
+ * @since $Release$
+ */
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-    @Autowired
-    private UserRepository userRepository;
-
+public class UserDetailsServiceImpl
+        implements UserDetailsService
+{
+    /* (non-Javadoc)
+     * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
+     */
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String inUsername)
+            throws UsernameNotFoundException
+    {
+        User user = userService.findByName(inUsername);
         if (user == null) {
-            throw new UsernameNotFoundException("No user present with username: " + username);
+            throw new UsernameNotFoundException("No user present with username: " + inUsername);
         } else {
-            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getHashedPassword(),
-                    getAuthorities(user));
+            Set<Permission> permissions = authService.findAllPermissionsByUsername(inUsername);
+            List<GrantedAuthority> grantedAuthorities = Lists.newArrayList();
+            permissions.forEach(permission -> grantedAuthorities.add(permission));
+            return new org.springframework.security.core.userdetails.User(user.getName(),
+                                                                          user.getHashedPassword(),
+                                                                          permissions);
         }
     }
-
-    private static List<GrantedAuthority> getAuthorities(User user) {
-        return user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
-                .collect(Collectors.toList());
-
-    }
-
+    /**
+     * provides access to user services
+     */
+    @Autowired
+    private UserService userService;
+    /**
+     * provides access to authorization services
+     */
+    @Autowired
+    private AuthorizationService authService;
 }
